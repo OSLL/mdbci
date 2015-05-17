5f4440545aa3f188e7e7abe36a1e9f43170ab1c1
Kirill Krinkin <kirill.krinkin@gmail.com>
2015-05-17 16:29:39 +0300

    #6268 added show and setup commands

--- core/help.rb
+++ core/help.rb
@@ -2,16 +2,21 @@ class Help
 
 def Help.display
   puts <<-EOF
-mdbci [option] <up>
+mdbci [option] <make | prepare | run>
 
 -h, --help:
   Shows this help screen
 
--c, --config [config file]
-  Uses [config file] for running instance. by default 'config.mdbci' will be used.
+-t, --template [config file]:
+  Uses [config file] for running instance. By default 'instance.json' will be used as config template.
 
--p, --platforms
+-p, --platforms:
   Show list of supported platforms
+
+-w, --override
+
+Configname is a directory what will be created for new vagrant configuration. Process will fail if directory already exists. Use key -w to override configuration.
+
   EOF
 
 end
---
+++ core/session.rb
@@ -0,0 +1,57 @@
+require 'json'
+require 'fileutils'
+
+class Session
+
+  attr_accessor :isOverride, :configFile
+  attr  :boxes
+  attr  :versions
+
+=begin
+     Load collections from json files:
+      - boxes.json.json
+      - versions.json
+=end
+
+  def loadCollections
+    puts 'Load boxes.json'
+    @boxes = JSON.parse(IO.read('boxes.json'))
+    puts 'Load Versions'
+  end
+
+  def inspect
+    @boxes.to_json
+  end
+
+  def setup(what)
+    case what
+      when 'boxes'
+        p @boxes.keys
+        puts 'Adding boxes to vagrant'
+        p @boxes
+        @boxes.each do |key,value|
+          shell = 'vagrant box add '+key+' '+value
+          system shell
+        end
+      else
+        puts 'Cannot setup '+what
+    end
+  end
+
+  def checkConfig
+    #TODO #6267
+    puts 'Checking this machine configuration requirments'
+    puts '.....NOT IMPLEMENTED YET'
+  end
+
+  def show(collection)
+    case collection
+      when 'boxes'
+        puts JSON.pretty_generate(@boxes)
+      when 'versions'
+        puts @versions
+      else
+        puts 'Unknown collection: '+collection
+    end
+  end
+end
