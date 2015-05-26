3ed5adb6c46f706e3464331ed495ad76bb655e15
Kirill Krinkin <kirill.krinkin@gmail.com>
2015-05-26 12:51:24 +0300

    Workaround against Bug #6296

--- recipes/cookbooks/mariadb/attributes/default.rb
+++ recipes/cookbooks/mariadb/attributes/default.rb
@@ -1,11 +1,11 @@
 # attributes/default.rb
 
 # mariadb version
-default["maria"]["version"] = "UNDEFINED"
+default["maria"]["version"] = "10.0"
 #default["maria"]["version"] = [ "5.5", "10.0" ]
 
 # mariadb repo ubuntu/debian/mint
-default["maria"]["repo"] = "UNDEFINED"
+default["maria"]["repo"] = "http://mirror.mephi.ru/mariadb/repo"
 
 # mariadb repo key for rhel/fedora/centos/suse
-default["maria"]["repo_key"] = "UNDEFINED"
+default["maria"]["repo_key"] = "http://mirror.mephi.ru/mariadb/yum"
