b22c86b8c14362fb59f9580ea07b64f5b4ff1046
Kirill Yudenok <kirill.yudenok@gmail.com>
2015-06-15 02:25:57 +0300

    #6279 fedora, centos, suse tests

--- templates/examples/master_slave_recipe/recipes/cookbooks/maria_master_slave/recipes/master_slave_setup.rb
+++ templates/examples/master_slave_recipe/recipes/cookbooks/maria_master_slave/recipes/master_slave_setup.rb
@@ -4,6 +4,12 @@ require 'shellwords'
 system 'echo server-id: ' + node['mariadb']['server_id']
 system 'echo database: ' + node['mariadb']['database']
 
+#
+# 1. объединить блоки
+# 2. проверять какая ОСь и 
+#   - отличие только /etc/mysql/my.cnf для дебиана и /etc/my.cnf для остальных
+#   - и сохранение для фаервола
+
 case node[:platform_family]
   when "debian", "ubuntu", "mint"
     
@@ -30,23 +36,6 @@ case node[:platform_family]
       sed -i $line'ibinlog_do_db\t\t= #{Shellwords.escape(node['mariadb']['database'])}' /etc/mysql/my.cnf
       EOF
     end
-
-    # Найти server.cnf
-
-    # Config master server.cnf
-    #bash 'Config mariadb server.cnf...' do
-    #  user 'root'
-    #  code <<-EOF
-    #    sed -i "s/log-basename=mar/#log-basename=mar/g" /etc/mysql/server.cnf  - comment
-    #    sed -i "s/server_id/server_id/g" /etc/mysql/server.cnf  # from attribute
-    #    sed -i "s/binlog-format/binlog-format/g" /etc/mysql/server.cnf  - uncomment
-    #    sed -i "s/#log_bin_index/log_bin_index/g" /etc/mysql/server.cnf  - uncomment
-    #    sed -i "s/#binlog_do_db/binlog_do_db/g" /etc/mysql/server.cnf  - uncomment # DB name from attribute
-    #  EOF
-    #  not_if "echo 'WARNING: server.cnf not configured!!!'"
-    #  action :run
-    #end
-
 
     # !!!
     # ln -s /etc/apparmor.d/usr.sbin.mysqld /etc/apparmor.d/disable/   usr.sbin.mysqld; service apparmor restart'
@@ -78,47 +67,127 @@ case node[:platform_family]
     # Debian/Ubuntu: iptables-save > /etc/iptables/rules.v4
     # deb - iptables-save > /etc/iptables.up.rules
 
-    bash 'Restart mysql service' do
-      code "service mysql restart"
-    end
+  when "rhel", "fedora", "centos"
 
-    #  -uroot -h127.0.0.1 -P3306 
-    bash 'Create mariadb users' do
-      code <<-EOF
-      /usr/bin/mysql -e 'CREATE USER repl@'%'" IDENTIFIED BY 'repl';'
-      /usr/bin/mysql -e 'GRANT replication slave ON *.* TO repl@'%' IDENTIFIED BY 'repl';'
-
-      /usr/bin/mysql -e 'CREATE USER skysql@'%' IDENTIFIED BY 'skysql';'
-      /usr/bin/mysql -e 'CREATE USER skysql@'localhost' IDENTIFIED BY 'skysql';'
-      /usr/bin/mysql -e 'GRANT ALL PRIVILEGES ON *.* TO skysql@'%' WITH GRANT OPTION;'
-      /usr/bin/mysql -e 'GRANT ALL PRIVILEGES ON *.* TO skysql@'localhost' WITH GRANT OPTION;'
-
-      /usr/bin/mysql -e 'CREATE USER maxuser@'%' identified by 'maxpwd';'
-      /usr/bin/mysql -e 'CREATE USER maxuser@'localhost' identified by 'maxpwd';'
-      /usr/bin/mysql -e 'GRANT ALL PRIVILEGES ON *.* TO maxuser@'%' WITH GRANT OPTION;'
-      /usr/bin/mysql -e 'GRANT ALL PRIVILEGES ON *.* TO maxuser@'localhost' WITH GRANT OPTION;'
-
-      /usr/bin/mysql -e 'FLUSH PRIVILEGES;'
+    # Config master etc/my.cnf
+    bash 'Config mariadb my.cnf' do
+    code <<-EOF
+      sed -i 4"i#bind-address= 127.0.0.1" /etc/my.cnf
+      sed -i 5"iserver-id\t\t= #{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf
+      sed -i 6"ilog_bin = FILENAME" /etc/my.cnf
+      sed -i 6"ilog_bin_index = FILENAME" /etc/my.cnf   
+      sed -i 7'ibinlog_do_db\t\t= #{Shellwords.escape(node['mariadb']['database'])}' /etc/my.cnf
       EOF
     end
 
-
-    bash 'Create test database' do
-      code "/usr/bin/mysql -e 'CREATE DATABASE IF NOT EXISTS test;'"
+    # Config master /etc/my.cnf.d/server.cnf
+    bash 'Config mariadb server.cnf...' do
+    code <<-EOF
+      sed -i 4"ilog-basename=mar" /etc/my.cnf.d/server.cnf
+      sed -i 5"iserver_id\t\t= #{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf.d/server.cnf
+      sed -i 6"ibinlog-format" /etc/my.cnf.d/server.cnf
+      sed -i 7"ilog_bin_index=FILENAME" /etc/my.cnf.d/server.cnf
+      sed -i 8"ibinlog_do_db\t\t= #{Shellwords.escape(node['mariadb']['database'])}" /etc/my.cnf.d/server.cnf
+    EOF
     end
 
-  
-  when "rhel", "fedora" # as CentOS
+    # сохраняются каждый раз при выполнении !!!
+    bash 'Config mariadb ports' do
+    code <<-EOF
+      iptables -I INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 3306 -j ACCEPT -m state --state NEW
+      iptables -I INPUT -p tcp -m tcp --dport 4006 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 4006 -j ACCEPT -m state --state NEW
+      iptables -I INPUT -p tcp -m tcp --dport 4008 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 4008 -j ACCEPT -m state --state NEW
+      iptables -I INPUT -p tcp -m tcp --dport 4009 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 4009 -j ACCEPT -m state --state NEW
+      iptables -I INPUT -p tcp -m tcp --dport 4442 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 4442 -j ACCEPT -m state --state NEW
+      iptables -I INPUT -p tcp -m tcp --dport 6444 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 6444 -j ACCEPT -m state --state NEW
+      iptables -I INPUT -p tcp -m tcp --dport 5306 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 5306 -j ACCEPT -m state --state NEW
 
-    # bash 'commands' do
-    # end
-
+      iptables-save > /etc/sysconfig/iptables
+      service iptables save
+      EOF
+    end
     #RHEL/CentOS: iptables-save > /etc/sysconfig/iptables
     #RHEL/CentOS: service iptables save
 
   when "suse"
 
-    # bash 'commands' do
-    # end
+    # Config master etc/my.cnf
+    bash 'Config mariadb my.cnf' do
+    code <<-EOF
+      sed -i 4"i#bind-address= 127.0.0.1" /etc/my.cnf
+      sed -i 5"iserver-id\t\t= #{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf
+      sed -i 6"ilog_bin = FILENAME" /etc/my.cnf
+      sed -i 6"ilog_bin_index = FILENAME" /etc/my.cnf   
+      sed -i 7'ibinlog_do_db\t\t= #{Shellwords.escape(node['mariadb']['database'])}' /etc/my.cnf
+      EOF
+    end
 
+    # Config master /etc/my.cnf.d/server.cnf
+    bash 'Config mariadb server.cnf...' do
+    code <<-EOF
+      sed -i 4"ilog-basename=mar" /etc/my.cnf.d/server.cnf
+      sed -i 5"iserver_id\t\t= #{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf.d/server.cnf
+      sed -i 6"ibinlog-format" /etc/my.cnf.d/server.cnf
+      sed -i 7"ilog_bin_index=FILENAME" /etc/my.cnf.d/server.cnf
+      sed -i 8"ibinlog_do_db\t\t= #{Shellwords.escape(node['mariadb']['database'])}" /etc/my.cnf.d/server.cnf
+    EOF
+    end
+
+    # сохраняются каждый раз при выполнении !!!
+    bash 'Config mariadb ports' do
+    code <<-EOF
+      iptables -I INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 3306 -j ACCEPT -m state --state NEW
+      iptables -I INPUT -p tcp -m tcp --dport 4006 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 4006 -j ACCEPT -m state --state NEW
+      iptables -I INPUT -p tcp -m tcp --dport 4008 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 4008 -j ACCEPT -m state --state NEW
+      iptables -I INPUT -p tcp -m tcp --dport 4009 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 4009 -j ACCEPT -m state --state NEW
+      iptables -I INPUT -p tcp -m tcp --dport 4442 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 4442 -j ACCEPT -m state --state NEW
+      iptables -I INPUT -p tcp -m tcp --dport 6444 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 6444 -j ACCEPT -m state --state NEW
+      iptables -I INPUT -p tcp -m tcp --dport 5306 -j ACCEPT
+      iptables -I INPUT -p tcp --dport 5306 -j ACCEPT -m state --state NEW
+
+      iptables-save > /etc/sysconfig/iptables
+      service iptables save
+      EOF
+    end
+    # SuSeFirewall
+end
+
+bash 'Restart mysql service' do
+  code "service mysql restart"
+end
+
+bash 'Create mariadb users' do
+  code <<-EOF
+  /usr/bin/mysql -e 'CREATE USER repl@'%'" IDENTIFIED BY 'repl';'
+  /usr/bin/mysql -e 'GRANT replication slave ON *.* TO repl@'%' IDENTIFIED BY 'repl';'
+
+  /usr/bin/mysql -e 'CREATE USER skysql@'%' IDENTIFIED BY 'skysql';'
+  /usr/bin/mysql -e 'CREATE USER skysql@'localhost' IDENTIFIED BY 'skysql';'
+  /usr/bin/mysql -e 'GRANT ALL PRIVILEGES ON *.* TO skysql@'%' WITH GRANT OPTION;'
+  /usr/bin/mysql -e 'GRANT ALL PRIVILEGES ON *.* TO skysql@'localhost' WITH GRANT OPTION;'
+
+  /usr/bin/mysql -e 'CREATE USER maxuser@'%' identified by 'maxpwd';'
+  /usr/bin/mysql -e 'CREATE USER maxuser@'localhost' identified by 'maxpwd';'
+  /usr/bin/mysql -e 'GRANT ALL PRIVILEGES ON *.* TO maxuser@'%' WITH GRANT OPTION;'
+  /usr/bin/mysql -e 'GRANT ALL PRIVILEGES ON *.* TO maxuser@'localhost' WITH GRANT OPTION;'
+
+  /usr/bin/mysql -e 'FLUSH PRIVILEGES;'
+  EOF
+end
+
+bash 'Create test database' do
+  code "/usr/bin/mysql -e 'CREATE DATABASE IF NOT EXISTS test;'"
 end
