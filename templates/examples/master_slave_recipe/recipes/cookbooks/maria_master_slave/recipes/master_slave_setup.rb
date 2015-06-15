4e1873fb00f46cd7c383712db2e2e2cc3a39e8b4
Kirill Yudenok <kirill.yudenok@gmail.com>
2015-06-15 16:04:11 +0300

    #6279 centos, fedora, suse tests 2

--- templates/examples/master_slave_recipe/recipes/cookbooks/maria_master_slave/recipes/master_slave_setup.rb
+++ templates/examples/master_slave_recipe/recipes/cookbooks/maria_master_slave/recipes/master_slave_setup.rb
@@ -4,29 +4,15 @@ require 'shellwords'
 system 'echo server-id: ' + node['mariadb']['server_id']
 system 'echo database: ' + node['mariadb']['database']
 
-#
-# 1. объединить блоки
-# 2. проверять какая ОСь и 
-#   - отличие только /etc/mysql/my.cnf для дебиана и /etc/my.cnf для остальных
-#   - и сохранение для фаервола
 
+# MY.CNF block
 case node[:platform_family]
   when "debian", "ubuntu", "mint"
-    
-    #bash 'Install packages' do
-    #code <<-EOF
-    #  sudo apt-get -y install iptables-persistent
-    #  EOF
-    #end
-	# --force-yes
 
     release_name = '$(lsb_release -cs)'
     system 'echo OS: ' + node[:platform_family] +' : ' + release_name
     
-    # Найти my.cnf - местоположение зависит от дистрибутива Линукс
-    # /etc/mysql/my.cnf
-
-    # Config master my.cnf
+    # Config master-slave /etc/mysql/my.cnf
     bash 'Config mariadb my.cnf' do
     code <<-EOF
       sed -i "s/bind-address/#bind-address/g" /etc/mysql/my.cnf
@@ -37,141 +23,129 @@ case node[:platform_family]
       EOF
     end
 
-    # !!!
-    # ln -s /etc/apparmor.d/usr.sbin.mysqld /etc/apparmor.d/disable/   usr.sbin.mysqld; service apparmor restart'
+  when "rhel", "fedora", "centos", "suse"
 
-    # сохраняются каждый раз при выполнении !!!
-    bash 'Config mariadb ports' do
-    code <<-EOF
-      iptables -I INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 3306 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 4006 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 4006 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 4008 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 4008 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 4009 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 4009 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 4442 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 4442 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 6444 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 6444 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 5306 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 5306 -j ACCEPT -m state --state NEW
-
-      iptables-save > /etc/iptables/rules.v4
-      EOF
-    end
-
-    # iptables-save >/etc/firewall.conf
-    # service iptables-persistent save
-    # Debian/Ubuntu: iptables-save > /etc/iptables/rules.v4
-    # deb - iptables-save > /etc/iptables.up.rules
-
-  when "rhel", "fedora", "centos"
-
-    # Config master etc/my.cnf
+    # Config master-slave /etc/my.cnf
     bash 'Config mariadb my.cnf' do
     code <<-EOF
-      sed -i 4"i#bind-address= 127.0.0.1" /etc/my.cnf
-      sed -i 5"iserver-id\t\t= #{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf
-      sed -i 6"ilog_bin = FILENAME" /etc/my.cnf
-      sed -i 6"ilog_bin_index = FILENAME" /etc/my.cnf   
-      sed -i 7'ibinlog_do_db\t\t= #{Shellwords.escape(node['mariadb']['database'])}' /etc/my.cnf
+      sed -i 6"i[mysqld]" /etc/my.cnf
+      sed -i 7"i#bind-address = 127.0.0.1" /etc/my.cnf
+      sed -i 8"iserver-id = #{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf
+      sed -i 9'ibinlog-do-db = #{Shellwords.escape(node['mariadb']['database'])}' /etc/my.cnf
+      sed -i 10"irelay_log = /var/lib/mysql/mysql-relay-bin" /etc/my.cnf
+      sed -i 11"irelay-log-index = /var/lib/mysql/mysql-relay-bin.index" /etc/my.cnf
+      sed -i 12"ilog-error = /var/lib/mysql/mysql.err" /etc/my.cnf
+      sed -i 13"imaster-info-file = /var/lib/mysql/mysql-master.info" /etc/my.cnf
+      sed -i 14"irelay-log-info-file = /var/lib/mysql/mysql-relay-log.info" /etc/my.cnf
+      sed -i 15"ilog-bin = /var/lib/mysql/mysql-bin" /etc/my.cnf
       EOF
     end
+    # slave - replicate-do-db
 
-    # Config master /etc/my.cnf.d/server.cnf
-    bash 'Config mariadb server.cnf...' do
+    # Config master-slave /etc/my.cnf.d/server.cnf
+    bash 'Config mariadb server.cnf' do
     code <<-EOF
-      sed -i 4"ilog-basename=mar" /etc/my.cnf.d/server.cnf
-      sed -i 5"iserver_id\t\t= #{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf.d/server.cnf
-      sed -i 6"ibinlog-format" /etc/my.cnf.d/server.cnf
-      sed -i 7"ilog_bin_index=FILENAME" /etc/my.cnf.d/server.cnf
-      sed -i 8"ibinlog_do_db\t\t= #{Shellwords.escape(node['mariadb']['database'])}" /etc/my.cnf.d/server.cnf
+      sed -i 13"ilog-basename=mar" /etc/my.cnf.d/server.cnf
+      sed -i 14"ilog-bin" /etc/my.cnf.d/server.cnf
+      sed -i 15"ibinlog-format=STATEMENT" /etc/my.cnf.d/server.cnf
+      sed -i 16"iserver_id=#{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf.d/server.cnf
     EOF
     end
+end # MY.CNF block
 
-    # сохраняются каждый раз при выполнении !!!
-    bash 'Config mariadb ports' do
-    code <<-EOF
-      iptables -I INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 3306 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 4006 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 4006 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 4008 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 4008 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 4009 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 4009 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 4442 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 4442 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 6444 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 6444 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 5306 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 5306 -j ACCEPT -m state --state NEW
-
-      iptables-save > /etc/sysconfig/iptables
-      service iptables save
-      EOF
-    end
-    #RHEL/CentOS: iptables-save > /etc/sysconfig/iptables
-    #RHEL/CentOS: service iptables save
+# iptables rules
+case node[:platform_family]
+  when "debian", "ubuntu", "mint", "rhel", "fedora", "centos"
+  
+  bash 'Config mariadb iptables ports' do
+  code <<-EOF
+    iptables -I INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
+    iptables -I INPUT -p tcp --dport 3306 -j ACCEPT -m state --state NEW
+    iptables -I INPUT -p tcp -m tcp --dport 4006 -j ACCEPT
+    iptables -I INPUT -p tcp --dport 4006 -j ACCEPT -m state --state NEW
+    iptables -I INPUT -p tcp -m tcp --dport 4008 -j ACCEPT
+    iptables -I INPUT -p tcp --dport 4008 -j ACCEPT -m state --state NEW
+    iptables -I INPUT -p tcp -m tcp --dport 4009 -j ACCEPT
+    iptables -I INPUT -p tcp --dport 4009 -j ACCEPT -m state --state NEW
+    iptables -I INPUT -p tcp -m tcp --dport 4442 -j ACCEPT
+    iptables -I INPUT -p tcp --dport 4442 -j ACCEPT -m state --state NEW
+    iptables -I INPUT -p tcp -m tcp --dport 6444 -j ACCEPT
+    iptables -I INPUT -p tcp --dport 6444 -j ACCEPT -m state --state NEW
+    iptables -I INPUT -p tcp -m tcp --dport 5306 -j ACCEPT
+    iptables -I INPUT -p tcp --dport 5306 -j ACCEPT -m state --state NEW
+  EOF
+  end
 
   when "suse"
 
-    # Config master etc/my.cnf
-    bash 'Config mariadb my.cnf' do
-    code <<-EOF
-      sed -i 4"i#bind-address= 127.0.0.1" /etc/my.cnf
-      sed -i 5"iserver-id\t\t= #{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf
-      sed -i 6"ilog_bin = FILENAME" /etc/my.cnf
-      sed -i 6"ilog_bin_index = FILENAME" /etc/my.cnf   
-      sed -i 7'ibinlog_do_db\t\t= #{Shellwords.escape(node['mariadb']['database'])}' /etc/my.cnf
-      EOF
-    end
+  bash 'Install iptables and SuSEfirewall2' do
+  code <<-EOF
+    zypper install -y iptables
+    zypper install -y SuSEfirewall2
+  EOF
+  end
 
-    # Config master /etc/my.cnf.d/server.cnf
-    bash 'Config mariadb server.cnf...' do
+  bash 'Config mariadb iptables ports' do
+  code <<-EOF
+    /usr/sbin/iptables -I INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
+    /usr/sbin/iptables -I INPUT -p tcp --dport 3306 -j ACCEPT -m state --state NEW
+    /usr/sbin/iptables -I INPUT -p tcp -m tcp --dport 4006 -j ACCEPT
+    /usr/sbin/iptables -I INPUT -p tcp --dport 4006 -j ACCEPT -m state --state NEW
+    /usr/sbin/iptables -I INPUT -p tcp -m tcp --dport 4008 -j ACCEPT
+    /usr/sbin/iptables -I INPUT -p tcp --dport 4008 -j ACCEPT -m state --state NEW
+    /usr/sbin/iptables -I INPUT -p tcp -m tcp --dport 4009 -j ACCEPT
+    /usr/sbin/iptables -I INPUT -p tcp --dport 4009 -j ACCEPT -m state --state NEW
+    /usr/sbin/iptables -I INPUT -p tcp -m tcp --dport 4442 -j ACCEPT
+    /usr/sbin/iptables -I INPUT -p tcp --dport 4442 -j ACCEPT -m state --state NEW
+    /usr/sbin/iptables -I INPUT -p tcp -m tcp --dport 6444 -j ACCEPT
+    /usr/sbin/iptables -I INPUT -p tcp --dport 6444 -j ACCEPT -m state --state NEW
+    /usr/sbin/iptables -I INPUT -p tcp -m tcp --dport 5306 -j ACCEPT
+    /usr/sbin/iptables -I INPUT -p tcp --dport 5306 -j ACCEPT -m state --state NEW
+  EOF
+  end
+
+end # iptables rules
+
+# Iptables block
+case node[:platform_family]
+
+  when "debian", "ubuntu", "mint"
+    # сохраняются каждый раз при выполнении !!!
+    bash 'Save mariadb iptables ports' do
     code <<-EOF
-      sed -i 4"ilog-basename=mar" /etc/my.cnf.d/server.cnf
-      sed -i 5"iserver_id\t\t= #{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf.d/server.cnf
-      sed -i 6"ibinlog-format" /etc/my.cnf.d/server.cnf
-      sed -i 7"ilog_bin_index=FILENAME" /etc/my.cnf.d/server.cnf
-      sed -i 8"ibinlog_do_db\t\t= #{Shellwords.escape(node['mariadb']['database'])}" /etc/my.cnf.d/server.cnf
+      iptables-save > /etc/iptables/rules.v4
     EOF
     end
+    # service iptables-persistent save
 
+  when "rhel", "fedora", "centos"
     # сохраняются каждый раз при выполнении !!!
-    bash 'Config mariadb ports' do
+    bash 'Save mariadb iptables ports' do
     code <<-EOF
-      iptables -I INPUT -p tcp -m tcp --dport 3306 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 3306 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 4006 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 4006 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 4008 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 4008 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 4009 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 4009 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 4442 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 4442 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 6444 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 6444 -j ACCEPT -m state --state NEW
-      iptables -I INPUT -p tcp -m tcp --dport 5306 -j ACCEPT
-      iptables -I INPUT -p tcp --dport 5306 -j ACCEPT -m state --state NEW
-
       iptables-save > /etc/sysconfig/iptables
-      service iptables save
-      EOF
+    EOF
     end
-    # SuSeFirewall
-end
+    # CHECK - service iptables save
 
-bash 'Restart mysql service' do
+  when "suse"
+    # сохраняются каждый раз при выполнении !!!
+    bash 'Save mariadb iptables ports' do
+    code <<-EOF
+      iptables-save > /etc/sysconfig/iptables
+    EOF
+    end
+    # CHECK - save rules via SuSeFirewall2
+
+end # Iptables block
+
+# 
+bash 'Restart mariadb service' do
   code "service mysql restart"
 end
-
+# 
 bash 'Create mariadb users' do
   code <<-EOF
-  /usr/bin/mysql -e 'CREATE USER repl@'%'" IDENTIFIED BY 'repl';'
+  /usr/bin/mysql -e 'CREATE USER repl@'%' IDENTIFIED BY 'repl';'
   /usr/bin/mysql -e 'GRANT replication slave ON *.* TO repl@'%' IDENTIFIED BY 'repl';'
 
   /usr/bin/mysql -e 'CREATE USER skysql@'%' IDENTIFIED BY 'skysql';'
@@ -187,7 +161,7 @@ bash 'Create mariadb users' do
   /usr/bin/mysql -e 'FLUSH PRIVILEGES;'
   EOF
 end
-
+#
 bash 'Create test database' do
   code "/usr/bin/mysql -e 'CREATE DATABASE IF NOT EXISTS test;'"
 end
