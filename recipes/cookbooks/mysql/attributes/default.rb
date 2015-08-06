4fc87abd9f29039d2e7b1d646e72817e168830ab
Kirill Krinkin <kirill.krinkin@gmail.com>
2015-08-06 17:56:02 +0300

    #6395 added mysql cookbook

---
+++ recipes/cookbooks/mysql/attributes/default.rb
@@ -0,0 +1,10 @@
+# attributes/default.rb
+
+# mysql version
+default["mysql"]["version"] = "10.0"
+
+# mysql repo ubuntu/debian/mint
+default["mysql"]["repo"] = "http://repo.mysql.com/yum/"
+
+# mysql repo key for rhel/fedora/centos/suse
+default["mysql"]["repo_key"] = "http://repo.mysql.com/yum/"
