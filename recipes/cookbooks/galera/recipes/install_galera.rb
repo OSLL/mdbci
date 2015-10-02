2b4cc5f3780a3608f5bf09f3cc89a116d8746dce
Kirill Yudenok <kirill.yudenok@gmail.com>
2015-10-02 15:44:48 +0300

    #6462 server_id for mariadb, galera recipes

--- recipes/cookbooks/galera/recipes/install_galera.rb
+++ recipes/cookbooks/galera/recipes/install_galera.rb
@@ -1,3 +1,5 @@
+require 'shellwords'
+
 include_recipe "galera::galera_repos"
 
 # Turn off SElinux
@@ -48,3 +50,33 @@ case node[:platform_family]
 else
   package 'MariaDB-Galera-server'
 end
+
+# Config /etc/my.cnf and /etc/mysql/my.cnf.d/server.cnf files
+case node[:platform_family]
+  when "debian", "ubuntu"
+
+    bash 'Config mariadb /etc/mysql/my.cnf file' do
+    code <<-EOF
+      sed -i "s/bind-address/#bind-address/g" /etc/mysql/my.cnf
+      sed -i 's/#server-id\t\t= 1/server-id\t\t= #{Shellwords.escape(node['galera']['server_id'])}/g' /etc/mysql/my.cnf
+      EOF
+    end
+
+  when "rhel", "fedora", "centos", "suse"
+
+    bash 'Config MariaDB /etc/my.cnf file' do
+    code <<-EOF
+      sed -i 6"i\n" /etc/my.cnf
+      sed -i 7"i[mysqld]" /etc/my.cnf
+      sed -i 8"i#bind-address = 127.0.0.1" /etc/my.cnf
+      sed -i 9"iserver-id = #{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf
+    EOF
+    end
+
+    bash 'Config MariaDB /etc/my.cnf.d/server.cnf file' do
+    code <<-EOF
+      sed -i 13"iserver-id = #{Shellwords.escape(node['galera']['server_id'])}" /etc/my.cnf.d/server.cnf
+      EOF
+    end
+
+end # server.cnf block
--- recipes/cookbooks/mariadb/recipes/install_enterprise.rb
+++ recipes/cookbooks/mariadb/recipes/install_enterprise.rb
@@ -1,3 +1,5 @@
+require 'shellwords'
+
 include_recipe "mariadb::mdberepos"
 
 # Turn off SElinux
@@ -39,3 +41,33 @@ else
     action :start
   end 
 end
+
+# Config /etc/my.cnf and /etc/mysql/my.cnf.d/server.cnf files
+case node[:platform_family]
+  when "debian", "ubuntu"
+
+    bash 'Config mariadb /etc/mysql/my.cnf file' do
+    code <<-EOF
+      sed -i "s/bind-address/#bind-address/g" /etc/mysql/my.cnf
+      sed -i 's/#server-id\t\t= 1/server-id\t\t= #{Shellwords.escape(node['mariadb']['server_id'])}/g' /etc/mysql/my.cnf
+      EOF
+    end
+
+  when "rhel", "fedora", "centos", "suse"
+
+    bash 'Config MariaDB /etc/my.cnf file' do
+    code <<-EOF
+      sed -i 6"i\n" /etc/my.cnf
+      sed -i 7"i[mysqld]" /etc/my.cnf
+      sed -i 8"i#bind-address = 127.0.0.1" /etc/my.cnf
+      sed -i 9"iserver-id = #{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf
+    EOF
+    end
+
+    bash 'Config MariaDB /etc/my.cnf.d/server.cnf file' do
+    code <<-EOF
+      sed -i 13"iserver-id = #{Shellwords.escape(node['mariadb']['server_id'])}" /etc/my.cnf.d/server.cnf
+      EOF
+    end
+
+end # server.cnf block
