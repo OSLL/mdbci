e2d86a11cfeec0201ec03a8f068122603f45d4d1
Kirill Yudenok <kirill.yudenok@gmail.com>
2016-01-04 19:03:40 +0300

    #6603 fix return for mdbci boxes

--- core/session.rb
+++ core/session.rb
@@ -362,6 +362,7 @@ class Session
     $out.info 'Current provider: ' + @nodesProvider
     if @nodesProvider == 'mdbci'
       $out.warning 'You are using mdbci nodes template. ./mdbci up command doesn\'t supported for this boxes!'
+      return 0 # success for mdbci warning
     else 
       (1..@attempts.to_i).each { |i|
         $out.info 'Bringing up ' + (up_type ? 'node ' : 'configuration ') + 
@@ -384,7 +385,7 @@ class Session
    	        exit_code = wthr.value.exitstatus # error
 	          $out.info 'UP ERROR, exit code '+exit_code.to_s
 	        else
-  	        $out.info 'UP SUCCESS, exit code '+exit_code.to_s
+  	        $out.info 'UP SUCCESS'
             return 0 # success
           end
   	    end
