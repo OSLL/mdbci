d99ee695a45048d400a20573f471ed4ae57fd583
Kirill Yudenok <kirill.yudenok@gmail.com>
2016-01-15 19:10:14 +0300

    #6617 test generate, up and show network commands, run next test if up success

--- spec/template_up_spec.rb
+++ spec/template_up_spec.rb
@@ -3,136 +3,151 @@ require 'spec_helper'
 
 require_relative '../core/session'
 require_relative '../core/network'
+require_relative '../core/node'
 require_relative '../core/boxes_manager'
 require_relative '../core/out'
 require_relative '../core/exception_handler'
 
-describe 'VBoxTemplateCommandsTests' do
+RSpec.describe "VBoxTemplateCommandsTests", :order => :defined do
+  before(:context) {@up_exit_code = 1}
 
-  context '.vmTemplateCommands' do
+  before(:each) do
 
-    it "./mdbci generate and up test" do
-      #pending # useful for Debugging
+    $session = Session.new
+    $session.isSilent = false
+    $out = Out.new
+    $exception_handler = ExceptionHandler.new
 
-      $session = Session.new
-      $session.isSilent = false
-      $out = Out.new
+    boxesPath = "./BOXES"
+    $session.boxes = BoxesManager.new(boxesPath)
 
-      $exception_handler = ExceptionHandler.new
+    @pwd = Dir.pwd
+    $out.info "PWD: " + @pwd.to_s
 
-      $session.configFile='confs/rspec_vm_test.json'
-      $session.nodesProvider = 'virtualbox'
-      $session.isOverride = true
+    @templateDir = @pwd.to_s+"/"+"template_up_test"
+    $out.info "templateDir: " + @templateDir
 
-      path = './repo.d'
-      $session.repos = RepoManager.new(path)
+    $session.mdbciDir = @pwd.to_s
+    $out.info "mdbciDir: " + $session.mdbciDir
 
-      boxesPath = './BOXES'
-      $session.boxes = BoxesManager.new(boxesPath)
-      $session.boxes.boxesManager.size().should_not eq(0)
-      $session.boxes.boxesManager.size().should eq(31)
+  end
 
-      nodes = JSON.parse(IO.read($session.configFile))
-      #nodes.size().should_not eq(0)
-      #nodes.size().should eq(7)
+'''
+  it "./mdbci generate and up test" do
 
-      # Generator.generate(path, config, boxes, override, provider)
-      path = 'template_up_test'
-      #Generator.generate(path,nodes,$session.boxes,$session.isOverride,$session.nodesProvider)
-      $session.generate(path)
+    $session = Session.new
+    $session.isSilent = false
+    $out = Out.new
 
-      # up generated template
-      #exit_code = $session.up(path)
-      #exit_code.should eq(0)      # success
-      #exit_code.should_not eq(1)  # error
+    $exception_handler = ExceptionHandler.new
 
-    end
-    #
-    # mdbci commands
-    #
-    it "./mdbci show network test" do
+    $session.configFile = "confs/rspec_vm_test.json"
+    $session.nodesProvider = "virtualbox"
+    $session.isOverride = true
 
-      #$session = Session.new
-      #$session.isSilent = false
-      #$out = Out.new
+    path = "./repo.d"
+    $session.repos = RepoManager.new(path)
 
-      pwd = Dir.pwd
-      network = Network.new
+    boxesPath = "./BOXES"
+    $session.boxes = BoxesManager.new(boxesPath)
+    #$session.boxes.boxesManager.size().should_not eq(0)
+    #$session.boxes.boxesManager.size().should eq(31)
 
-      path = 'template_up_test/node0'
-      network.loadNodes pwd.to_s
-      network.show(path)
+    nodes = JSON.parse(IO.read($session.configFile))
+    #nodes.size().should_not eq(0)
+    #nodes.size().should eq(7)
 
-    end
+    # Generator.generate(path, config, boxes, override, provider)
+    path = "template_up_test"
+    #Generator.generate(path,nodes,$session.boxes,$session.isOverride,$session.nodesProvider)
+    $session.generate(path)
 
-    it "./mdbci show keyfile test" do
+    # up generated template
+    @up_exit_code = $session.up(path)
+    @up_exit_code.should eq(0)      # success
 
-      pwd = Dir.pwd
-      network = Network.new
+  end
+'''
+  it "nodesStatusTest" do
 
-      path = 'template_up_test/node0'
-      network.loadNodes pwd.to_s
-      network.showKeyFile(path)
+    network = Network.new
+    network.loadNodes @pwd.to_s + "/" + "template_up_test"
 
-    end
+    $out.info "Nodes: " + network.nodes.to_s
+    network.nodes.size().should eq(2)
+  end
 
-    it "./mdbci show private_ip test" do
+  #
+  # mdbci commands
+  #
+  it "./mdbci show network test" do
 
-      pwd = Dir.pwd
-      network = Network.new
+    path = "template_up_test"
+    $out.info "up exit code = " + @up_exit_code.to_s
+    if @up_exit_code == 1
 
-      path = 'template_up_test/node0'
-      network.loadNodes pwd.to_s
-      network.private_ip(path)
-
-    end
-
-    it "./mdbci ssh test" do
-
-      $session = Session.new
-      $session.isSilent = false
-      $session.command = 'whoami'
-      $out = Out.new
-
-      #$network = Network.new
-
-      path = 'template_up_test/node0'
-      $session.ssh(path)
-
-    end
-
-    it "./mdbci sudo test" do
-
-      $session = Session.new
-      $session.isSilent = false
-      $session.command = 'whoami'
-      $out = Out.new
-
-      #$network = Network.new
-
-      path = 'template_up_test/build'
-      $session.sudo(path)
-
-    end
-
-    it "./mdbci public_keys test" do
-
-      # TBD
-
-    end
-
-    it "./mdbci install_repo test" do
-
-      # TBD
-
-    end
-
-    it "./mdbci update_repo test" do
-
-      # TBD
+      $out.info "mdbciDir: " + $session.mdbciDir.to_s
+      Network.show path.to_s
 
     end
 
   end
+'''
+  it "./mdbci show keyfile test" do
 
+    path = "template_up_test"
+    $out.info "up exit code = " + @up_exit_code.to_s
+    if @up_exit_code == 0
+      network.loadNodes pwd.to_s
+      Network.showKeyFile path.to_s
+    end
+
+  end
+
+  it "./mdbci show private_ip test" do
+
+    pwd = Dir.pwd
+    network = Network.new
+
+    path = "template_up_test/node0"
+    network.loadNodes pwd.to_s
+    Network.private_ip path.to_s
+
+  end
+
+  it "./mdbci ssh test" do
+
+    $session = Session.new
+    $session.isSilent = false
+    $session.command = "whoami"
+    $out = Out.new
+
+    path = "template_up_test/node0"
+    $session.ssh path.to_s
+
+  end
+
+  it "./mdbci sudo test" do
+
+    $session = Session.new
+    $session.isSilent = false
+    $session.command = "whoami"
+    $out = Out.new
+
+    path = "template_up_test/build"
+    $session.sudo(path)
+  end
+
+  it "./mdbci public_keys test" do
+    # TBD
+  end
+
+  it "./mdbci install_repo test" do
+    # TBD
+  end
+
+  it "./mdbci update_repo test" do
+    # TBD
+  end
+  '''
 end
