dbb932cbff4fc0e5750854287517ca12374ecf4f
galiaf95 <galiaf1995@gmail.com>
2016-01-19 03:09:17 +0300

    #6589 added unit tests for Generator.nodeDefinition

--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -1,13 +1,127 @@
 require 'rspec'
 require 'spec_helper'
+require 'json'
+require 'fileutils'
 require_relative '../core/generator'
 require_relative '../core/session'
+require_relative '../core/boxes_manager'
+require_relative '../core/out'
+require_relative '../core/exception_handler'
 
 describe "Generator" do
 
-  # TODO
-  it "Check node definition" do
-    # Generator.nodeDefinition(node, boxes, path, cookbook_path)
+  describe '.nodeDefinition' do
+
+    # Next configuration is changing in tests (box is changed and/or aws config file path added)
+    before :each do
+      @testNodeString = <<EOF
+{
+  "testNode" :
+  {
+    "hostname" : "test",
+    "box" : "centos6",
+    "product" :
+    {
+      "name": "galera",
+      "version"  : "10.0",
+      "cnf_template" : "server1.cnf",
+      "cnf_template_path" : "../cnf",
+      "node_name" : "test"
+    }
+  }
+}
+EOF
+    end
+
+    # Initialising all variables that is used in Generator.nodeDefenition
+    before :all do
+      $out = Out.new
+      $session = Session.new
+      $session.isSilent = true
+      $session.mdbciDir = Dir.pwd
+      $exception_handler = ExceptionHandler.new
+      boxesPath = './BOXES'
+      $session.boxes = BoxesManager.new boxesPath
+      reposPath = './repo.d'
+      $session.repos = RepoManager.new reposPath
+    end
+
+    # If role file created then it will be deleted after each test
+    after :each do
+      rolePath = Dir.pwd + '/testNode.json'
+      File.delete rolePath if File.exists? rolePath
+      dockerDir = rolePath = Dir.pwd + '/testNode/'
+      FileUtils.rm_rf dockerDir
+    end
+
+    it 'generates virtual box node definition' do
+      nodes = JSON.parse @testNodeString
+      nodes['testNode']['box'] = 'centos6'
+      nodes .each do |node|
+        roleFile = File.open Dir.pwd + '/spec/data_to_test/6589_test_vbox_role_node_definition.json', 'r'
+        roleFileContent = roleFile.read
+        vagrantfileFile = File.open Dir.pwd + '/spec/data_to_test/6589_test_vbox_vagrantfile_node_definition.txt', 'r'
+        vagrantfileContent = vagrantfileFile.read
+        vagrantfileContent = "\n" + vagrantfileContent
+        Generator.nodeDefinition(node,  $session.boxes, Dir.pwd, '../recipes/cookbooks/')
+            .delete(' ')
+            .should eql vagrantfileContent.delete(' ')
+        testRoleFile = File.open Dir.pwd + '/testNode.json', 'r'
+        testRoleFile.read.should eql roleFileContent
+      end
+    end
+
+    it 'generates aws box node definition' do
+      nodes = JSON.parse @testNodeString
+      nodes['testNode']['box'] = 'centos7'
+      nodes .each do |node|
+        roleFile = File.open Dir.pwd + '/spec/data_to_test/6589_test_aws_role_node_definition.json', 'r'
+        roleFileContent = roleFile.read
+        vagrantfileFile = File.open Dir.pwd + '/spec/data_to_test/6589_test_aws_vagrantfile_node_definition.txt', 'r'
+        vagrantfileContent = vagrantfileFile.read
+        vagrantfileContent = "\n" + vagrantfileContent
+        Generator.nodeDefinition(node,  $session.boxes, Dir.pwd, '../recipes/cookbooks/')
+            .delete(' ')
+            .should eql vagrantfileContent.delete(' ')
+        testRoleFile = File.open Dir.pwd + '/testNode.json', 'r'
+        testRoleFile.read.should eql roleFileContent
+      end
+    end
+
+    it 'generates qemu box node definition' do
+      nodes = JSON.parse @testNodeString
+      nodes['testNode']['box'] = 'centos_7.0_libvirt'
+      nodes .each do |node|
+        roleFile = File.open Dir.pwd + '/spec/data_to_test/6589_test_qemu_role_node_definition.json', 'r'
+        roleFileContent = roleFile.read
+        vagrantfileFile = File.open Dir.pwd + '/spec/data_to_test/6589_test_qemu_vagrantfile_node_definition.txt', 'r'
+        vagrantfileContent = vagrantfileFile.read
+        vagrantfileContent = "\n" + vagrantfileContent
+        Generator.nodeDefinition(node,  $session.boxes, Dir.pwd, '../recipes/cookbooks/')
+            .delete(' ')
+            .should eql vagrantfileContent.delete(' ')
+        testRoleFile = File.open Dir.pwd + '/testNode.json', 'r'
+        testRoleFile.read.should eql roleFileContent
+      end
+    end
+
+    it 'generates docker box node definition' do
+      nodes = JSON.parse @testNodeString
+      nodes['testNode']['box'] = 'centos_7_docker'
+      nodes .each do |node|
+        roleFile = File.open Dir.pwd + '/spec/data_to_test/6589_test_docker_role_node_definition.json', 'r'
+        roleFileContent = roleFile.read
+        vagrantfileFile = File.open Dir.pwd + '/spec/data_to_test/6589_test_docker_vagrantfile_node_definition.txt', 'r'
+        vagrantfileContent = vagrantfileFile.read
+        vagrantfileContent = "\n" + vagrantfileContent
+        Generator.nodeDefinition(node,  $session.boxes, Dir.pwd, '../recipes/cookbooks/')
+            .delete(' ')
+            .should eql vagrantfileContent.delete(' ')
+        testRoleFile = File.open Dir.pwd + '/testNode.json', 'r'
+        testRoleFile.read.should eql roleFileContent
+      end
+    end
+
   end
 
   # TODO
