2169e3b59b35baf931fdea5f4d800711f388bd2b
Kirill Yudenok <kirill.yudenok@gmail.com>
2016-02-24 17:59:38 +0300

    #6816 fix exit_code return statements

--- core/session.rb
+++ core/session.rb
@@ -67,8 +67,7 @@ class Session
   end
 
   def setup(what)
-
-    exit_code = 0
+    exit_code = 1
     possibly_failed_command = ''
 
     case what
@@ -98,7 +97,7 @@ class Session
         end
       else
         $out.warning 'Cannot setup '+what
-        return 1
+        exit_code = 1
     end
 
     if exit_code != 0
@@ -124,13 +123,13 @@ class Session
 
     if args.nil?
       $out.error 'Configuration name is required'
-      return 1
+      exit_code = 1
     end
 
     config = args.split('/')
     unless Dir.exists?(config[0])
       $out.error 'Machine with such name does not exists'
-      return 1
+      exit_code = 1
     end
 
     Dir.chdir config[0]
@@ -179,7 +178,7 @@ class Session
 
     if args.nil?
       $out.error 'Configuration name is required'
-      return 1
+      exit_code = 1
     end
 
     params = args.split('/')
@@ -221,7 +220,7 @@ class Session
     else # aws, vbox nodes
       unless Dir.exist?(params[0])
         $out.error 'Machine with such name does not exist'
-        return 1
+        exit_code = 1
       end
       Dir.chdir params[0]
       cmd = 'vagrant ssh '+params[1].to_s+' -c "'+$session.command+'"'
@@ -256,42 +255,45 @@ class Session
   end
 
   def showBoxes
+    exit_code = 1
     begin
       $out.out JSON.pretty_generate(@boxes.boxesManager)
-      return 0
+      exit_code = 0
     rescue
       $out.error "check boxes configuration and try again"
-      return 1
+      exit_code = 1
     end
+    return exit_code
   end
 
   def showPlatforms
+    exit_code = 1
     begin
       $out.out @boxes.boxesManager.keys
-      return 0
+      exit_code = 0
     rescue
       $out.error "check boxes configuration and try again"
-      return 1
+      exit_code = 1
     end
+    return exit_code
   end
 
   # show boxes with platform and version
   def showBoxes
-
     exit_code = 1
 
     if $session.boxPlatform.nil?
       $out.warning './mdbci show boxes --platform command option is not defined!'
-      return 1
+      exit_code = 1
     elsif $session.boxPlatform.nil? and $session.boxPlatformVersion.nil?
       $out.warning './mdbci show boxes --platform or --platform-version command parameters are not defined!'
-      return 1
+      exit_code = 1
     end
     # check for undefined box
     some_box = $session.boxes.boxesManager.find { |box| box[1]['platform'] == $session.boxPlatform }
     if some_box.nil?
       $out.warning 'Platform '+$session.boxPlatform+' is not supported!'
-      return 1
+      exit_code = 1
     end
 
     if !$session.boxPlatformVersion.nil?
@@ -394,17 +396,17 @@ class Session
       IO.read($session.configFile)
     rescue
       $out.warning 'Instance configuration file not found!'
-      return 1
+      exit_code = 1
     end
     instanceConfigFile = $exception_handler.handle('INSTANCE configuration file not found'){IO.read($session.configFile)}
     if instanceConfigFile.nil?
       $out.warning 'Instance configuration file invalid!'
-      return 1
+      exit_code = 1
     end
     @configs = $exception_handler.handle('INSTANCE configuration file invalid'){JSON.parse(instanceConfigFile)}
     if @configs.nil?
       $out.out 'Template configuration file is empty!'
-      return 1
+      exit_code = 1
     else
       LoadNodesProvider configs
     end
@@ -444,7 +446,7 @@ class Session
     # No arguments provided
     if args.nil?
       $out.info 'Command \'up\' needs one argument, found zero'
-      return
+      exit_code = 1
     end
 
     # No attempts provided
@@ -486,7 +488,7 @@ class Session
     $out.info 'Current provider: ' + @nodesProvider
     if @nodesProvider == 'mdbci'
       $out.warning 'You are using mdbci nodes template. ./mdbci up command doesn\'t supported for this boxes!'
-      return 0
+      exit_code = 0
     else
       (1..@attempts.to_i).each do |i|
         $out.info 'Bringing up ' + (up_type ? 'node ' : 'configuration ') +
@@ -519,7 +521,7 @@ class Session
 	          $out.error 'exit code '+exit_code.to_s
 	        else
             $out.info 'Configuration UP SUCCESS!'
-            return 0
+            exit_code = 0
           end
   	    end
 
@@ -561,7 +563,7 @@ class Session
 
     if args.nil?
       $out.error 'Configuration name is required'
-      return 1
+      exit_code = 1
     end
 
     args = args.split('/')
@@ -572,7 +574,7 @@ class Session
       if args[1].nil?     # read ip for all nodes
         if $session.mdbciNodes.empty?
           $out.error "MDBCI nodes not found in #{args[0]}"
-          return 1
+          exit_code = 1
         end
         $session.mdbciNodes.each do |node|
           box = node[1]['box'].to_s
@@ -597,7 +599,7 @@ class Session
 
         if mdbci_node.nil?
           $out.error "No such node with name #{args[1]} in #{args[0]}"
-          return 1
+          exit_code = 1
         end
 
         box = mdbci_node[1]['box'].to_s
@@ -617,14 +619,14 @@ class Session
           possibly_failed_command = cmd
         else
           $out.error "Wrong box parameter in node: #{args[1]}"
-          return 1
+          exit_code = 1
         end
       end
     else # aws, vbox, libvirt, docker nodes
 
       unless Dir.exists? args[0]
         $out.error "Directory with nodes does not exists: #{args[1]}"
-        return 1
+        exit_code = 1
       end
 
       network = Network.new
@@ -632,7 +634,7 @@ class Session
 
       if network.nodes.empty?
         $out.error "No aws, vbox, libvirt, docker nodes found in #{args[0]}"
-        return 1
+        exit_code = 1
       end
 
       if args[1].nil? # No node argument, copy keys to all nodes
@@ -651,7 +653,7 @@ class Session
 
         if node.nil?
           $out.error "No such node with name #{args[1]} in #{args[0]}"
-          return 1
+          exit_code = 1
         end
 
         #
@@ -678,6 +680,7 @@ class Session
   end
 
   def showProvider(name)
+    exit_code = 1
     if $session.boxes.boxesManager.has_key?(name)
       box_params = $session.boxes.getBox(name)
       provider = box_params["provider"].to_s
@@ -692,17 +695,18 @@ class Session
 
   # print boxes platform versions by platform name
   def boxesPlatformVersions
+    exit_code = 1
 
     if $session.boxPlatform == nil
       $out.error "Specify parameter --platforms and try again"
-      return 1
+      exit_code = 1
     end
 
     # check for supported platforms
     some_platform = $session.boxes.boxesManager.find { |box| box[1]['platform'] == $session.boxPlatform }
     if some_platform.nil?
       $out.error "Platform #{$session.boxPlatform} is not supported!"
-      return 1
+      exit_code = 1
     else
       $out.info "Supported versions for #{$session.boxPlatform}:"
     end
@@ -715,8 +719,10 @@ class Session
       if params.has_value?($session.boxPlatform)
         box_platform_version = params['platform_version']
         boxes_versions.push(box_platform_version)
+        exit_code = 0
       else
         $out.error "#{$session.boxPlatform} has 0 supported versions! Please check box platform!"
+        exit_code = 1
       end
     end
 
@@ -724,7 +730,7 @@ class Session
     boxes_versions = boxes_versions.uniq # delete duplicates values
     boxes_versions.each { |version| $out.out version }
 
-    return 0
+    return exit_code
   end
 
 
