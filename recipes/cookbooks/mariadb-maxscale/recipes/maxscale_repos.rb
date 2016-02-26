1568df46650420ef817c84aa898636e08c2c7c48
Kirill Yudenok <kirill.yudenok@gmail.com>
2016-02-26 19:13:16 +0300

    #6846 try to save chef command exit status to file

--- recipes/cookbooks/mariadb-maxscale/recipes/maxscale_repos.rb
+++ recipes/cookbooks/mariadb-maxscale/recipes/maxscale_repos.rb
@@ -33,9 +33,21 @@ case node[:platform_family]
   template "/etc/yum.repos.d/maxscale.repo" do
     source "mdbci.maxscale.rhel.erb"
     action :create
+    not_if do
+      exit_status = system("rpm -qa | grep 'maxscale'")
+      if exit_status == false
+        chef_log = File.open("/home/vagrant/#{node.name}_chef_up.log", "w")
+        chef_log.puts "#{node.name} : maxscale package not found : exit status = "+ (exit_status.to_s == "true" ? "1" : "0")
+        chef_log.close
+        FileUtils.cp("/home/vagrant/#{node.name}_chef_up.log", "/vagrant/#{node.name}_chef_up.log")
+        #raise "mariadb-maxscale package not found!"
+      end
+    end
   end
-  when "suse"
-  # Add the repo
+
+  when "suse", "opensuse", "sles"
+
+    # Add the repo
   template "/etc/zypp/repos.d/maxscale.repo" do
     source "mdbci.maxscale.suse.erb"
     action :create
