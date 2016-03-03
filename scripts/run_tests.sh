59f5e81a33579156a565e4f59d6c9dc776d3a882
galiaf95 <galiaf1995@gmail.com>
2016-03-03 12:02:19 +0300

    #6848 added silent flag

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,1 +1,16 @@
-rake run
+#! /bin/bash
+
+while getopts ":s:" opt; do
+  case $opt in
+    s) silent="$OPTARG"
+    ;;
+    \?) silent="true" 
+    ;;
+  esac
+done
+
+if [[ $silent != "true" ]] && [[ $silent != "false" ]]; then
+	silent=true
+fi
+
+SILENT=$silent rake run
