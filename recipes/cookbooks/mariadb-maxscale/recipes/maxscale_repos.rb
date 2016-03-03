bf41146604350ca52fd80ae185558e2748a271e2
Kirill Yudenok <kirill.yudenok@gmail.com>
2016-03-03 22:31:17 +0300

    #6846 remove chef not_if checks

--- recipes/cookbooks/mariadb-maxscale/recipes/maxscale_repos.rb
+++ recipes/cookbooks/mariadb-maxscale/recipes/maxscale_repos.rb
@@ -1,52 +1,8 @@
-
-#
-# Chef up log file
-#
-file "/home/vagrant/"+node['maxscale']['node_name']+"_chef_up.log" do
-  owner 'vagrant'
-  group 'vagrant'
-  mode '0664'
-  action :create
-end
 #
 # Install dependencies
 #
 [ "net-tools", "psmisc" ].each do |pkg|
-  package pkg do
-    action :install
-    not_if do
-      case node[:platform_family]
-        when "debian", "ubuntu"
-          status = system("dpkg -s net-tools")
-          if status == false
-            # move to separate file logger.rb or use chef_handler cookbook
-            chef_log = File.open("/home/vagrant/"+node['maxscale']['node_name']+"_chef_up.log", "w")
-            unless chef_log.nil?
-              chef_log.puts node['maxscale']['node_name']+":apt:#{pkg} package not found:exit code:"+(status == true ? "0" : "1")
-            end
-            chef_log.close
-          end
-        when "rhel", "fedora", "centos"
-          status = system("rpm -qa | grep 'net-tools'")
-          if status == false
-            chef_log = File.open("/home/vagrant/"+node['maxscale']['node_name']+"_chef_up.log", "w")
-            unless chef_log.nil?
-              chef_log.puts node['maxscale']['node_name']+":rpm:#{pkg} package not found:exit code:"+(status == true ? "0" : "1")
-            end
-            chef_log.close
-          end
-        when "opensuse", "sles", "suse"
-          status = system("rpm -qa | grep 'net-tools'")
-          if status == false
-            chef_log = File.open("/home/vagrant/"+node['maxscale']['node_name']+"_chef_up.log", "w")
-            unless chef_log.nil?
-              chef_log.puts node['maxscale']['node_name']+":zypper:#{pkg} package not found:exit code:"+(status == true ? "0" : "1")
-            end
-            chef_log.close
-          end
-      end
-    end
-  end
+  package pkg
 end
 #
 # Maxscale package attributes
@@ -75,22 +31,6 @@ case node[:platform_family]
       command "apt-get update"
     end
 
-    # Update maxscale repo
-    execute "update maxscale package" do
-      command "apt-get update maxscale"
-      not_if do
-        status = system("apt-cache search maxscale | grep 'maxscale'")
-        if status == false
-          # move to separate file logger.rb or use chef_handler cookbook
-          chef_log = File.open("/home/vagrant/"+node['maxscale']['node_name']+"_chef_up.log", "w")
-          unless chef_log.nil?
-            chef_log.puts node['maxscale']['node_name']+":apt:maxscale package not found:exit code:"+(status == true ? "0" : "1")
-          end
-          chef_log.close
-        end
-      end
-    end
-
   when "rhel", "fedora", "centos"
 
     # Add the repo
@@ -99,45 +39,12 @@ case node[:platform_family]
       action :create
     end
 
-    # Update maxscale repo
-    execute "update maxscale package" do
-      command "yum update maxscale"
-      not_if do
-        status = system("rpm -qa | grep 'maxscale'")
-        if status == false
-          # move to separate file logger.rb or use chef_handler cookbook
-          chef_log = File.open("/home/vagrant/"+node['maxscale']['node_name']+"_chef_up.log", "w")
-          unless chef_log.nil?
-            chef_log.puts node['maxscale']['node_name']+":rpm:maxscale package not found:exit code:"+(status == true ? "0" : "1")
-          end
-          chef_log.close
-          #raise "mariadb-maxscale package not found! Please check product repo!"
-        end
-      end
-    end
-
   when "suse", "opensuse", "sles"
 
     # Add the repo
     template "/etc/zypp/repos.d/maxscale.repo" do
       source "mdbci.maxscale.suse.erb"
       action :create
-    end
-
-    # Update maxscale repo
-    execute "update maxscale package" do
-      command "zypper update maxscale"
-      not_if do
-        status = system("zypper -is | grep 'maxscale'")
-        if status == false
-          # move to separate file logger.rb or use chef_handler cookbook
-          chef_log = File.open("/home/vagrant/"+node['maxscale']['node_name']+"_chef_up.log", "w")
-          unless chef_log.nil?
-            chef_log.puts node['maxscale']['node_name']+":zypper:maxscale package not found:exit code:"+(status == true ? "0" : "1")
-          end
-          chef_log.close
-        end
-      end
     end
 
 end
