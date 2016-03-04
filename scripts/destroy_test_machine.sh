ddda058390bd4d45e462e23f5c16199ef58e7fb0
galiaf95 <galiaf1995@gmail.com>
2016-03-04 19:49:27 +0300

    #6771 fixes due to comments in pull request

--- scripts/destroy_test_machine.sh
+++ scripts/destroy_test_machine.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 cur_dir=$(pwd)
 cd INNER_TEST_MACHINE
--- scripts/up_test_machine.sh
+++ scripts/up_test_machine.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 ./mdbci --override --template spec/configs/template/centos_6_vbox_mariadb_10.0.json generate INNER_TEST_MACHINE
 ./mdbci up INNER_TEST_MACHINE
