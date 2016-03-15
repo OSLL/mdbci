8f2e095cc294e7ee9404bd43e221b380cb9560e5
galiaf95 <galiaf1995@gmail.com>
2016-03-16 01:19:33 +0300

    #6886 fixes

--- scripts/parse_ctest_log.rb
+++ scripts/parse_ctest_log.rb
@@ -9,6 +9,9 @@ ONLY_FAILED_OPTION = '--only-failed'
 TEST_NUMBER = 'test_number'
 TEST_NAME = 'test_name'
 TEST_SUCCESS = 'test_success'
+TESTS = 'tests'
+TESTS_COUNT = 'tests_count'
+FAILED_TESTS_COUNT = 'failed_tests_count'
 
 FAILED = 'Failed'
 PASSED = 'Passed'
@@ -19,14 +22,11 @@ opts = GetoptLong.new(
 )
 
 $log_file_path = nil
-$only_failed = false
 
 opts.each do |opt, arg|
   case opt
     when LOG_FILE_OPTION
       $log_file_path = arg
-    when ONLY_FAILED_OPTION
-      $only_failed = true
   end
 end
 
@@ -61,11 +61,12 @@ def parseCtestLog()
   end
   ctest_log = ctest_log[0..ctest_end_line]
   tests_quantity = ctest_log[-1].match(ctest_last_line_regex).captures[0]
-  return findTestsInfo(ctest_log, tests_quantity)
+  return {TESTS_COUNT=>tests_quantity}.merge findTestsInfo(ctest_log, tests_quantity)
 end
 
 def findTestsInfo(ctest_log, tests_quantity)
   tests_info = Array.new
+  failed_tests_counter = 0
   test_counter = 1
   real_test_num = nil
   test_start_found = false
@@ -81,18 +82,19 @@ def findTestsInfo(ctest_log, tests_quantity)
       test_end_regex = /#{test_counter}\/#{tests_quantity} Test ##{real_test_num}: #{current_test_name} .+(Failed|Passed)/
       if line =~ test_end_regex
         test_success = line.match(test_end_regex).captures[0]
-        if !$only_failed && test_success.equal?(FAILED) || PASSED
-          tests_info.push({real_test_num=>{TEST_NAME=>current_test_name, TEST_SUCCESS=>test_success}})
+        if test_success == (FAILED)
+          failed_tests_counter += 1
         end
+        tests_info.push(real_test_num=>{TEST_NAME=>current_test_name, TEST_SUCCESS=>test_success})
         current_test_name = nil
         real_test_num = nil
         test_counter += 1
       end
     end
-    break if (test_counter - 1).equal?tests_quantity.to_i
+    break if (test_counter - 1) == tests_quantity
   end
-  return tests_info
+  return {FAILED_TESTS_COUNT=>failed_tests_counter, TESTS=>tests_info}
 end
 
 
-puts parseCtestLog().to_json
+puts JSON.pretty_generate(parseCtestLog())
