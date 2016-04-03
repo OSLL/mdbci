2b71e18362effbd563045788efcfac75a9f19eca
galiaf95 <galiaf1995@gmail.com>
2016-04-03 20:16:23 +0300

    #6927 added ctest parsing to writer

--- scripts/build_parser/write_build_results.rb
+++ scripts/build_parser/write_build_results.rb
@@ -14,7 +14,7 @@ LOGIN = 'test_bot'
 PASSWORD = 'pass'
 DB_NAME = 'test_results_db'
 
-# parse_ctest_log.rb keys definition 
+# parse_ctest_log.rb keys definition
 TEST_NAME = 'test_name'
 TEST_SUCCESS = 'test_success'
 FAILED = 'Failed'
@@ -42,7 +42,7 @@ class BuildResultsWriter
 
   def connect_mdb(host, login, password, db_name)
     @client = Mysql2::Client.new(:host => "#{host}", :username => "#{login}",\
-      :password => "#{password}", :database => "#{db_name}") 
+      :password => "#{password}", :database => "#{db_name}")
     puts "Connection to db (:host => #{host}, :username => #{login}, "\
          ":password => #{password}, :database => #{db_name}) established successfuly"
   end
@@ -53,9 +53,9 @@ class BuildResultsWriter
     test_runs_query = "INSERT INTO test_run (jenkins_id, "\
     "start_time, target, box, product, mariadb_version, "\
     "test_code_commit_id, maxscale_commit_id, job_name) "\
-    "VALUES (#{jenkins_id}, #{start_time}, '#{target}', '#{box}', '#{product}', "\
+    "VALUES ('#{jenkins_id}', '#{start_time}', '#{target}', '#{box}', '#{product}', "\
     "'#{mariadb_version}', '#{test_code_commit_id}', '#{maxscale_commit_id}', '#{job_name}')"
-     
+
     @client.query(test_runs_query)
     id = @client.last_id
     puts "Performed insert (test_run, id = #{id}): #{test_runs_query}"
@@ -63,8 +63,8 @@ class BuildResultsWriter
   end
 
   def write_results_table(id, test, result)
-    results_query = "INSERT INTO results (id, test, result) VALUES (#{id}, "\
-      "'#{test}', #{result})"
+    results_query = "INSERT INTO results (id, test, result) VALUES ('#{id}', "\
+      "'#{test}', '#{result}')"
     @client.query(results_query)
     puts "Performed insert (results): #{results_query}"
   end
@@ -73,17 +73,19 @@ class BuildResultsWriter
     # TODO extract params from @results@
     # Code below is a STUB. Please replace it with actual parameters aquiring from results
     # <STUB>
-    jenkins_id = 1
-    start_time = 'NOW()'
-    target = 'target'
-    box = 'box'
-    product = 'product'
-    mariadb_version = 'mariadb_version'
-    test_code_commit_id = 'test_code_commit_id'
-    maxscale_commit_id = 'maxscale_commit_id'
-    job_name = 'job_name'
-    tests = [ { TEST_NAME => "server_weight", TEST_SUCCESS => "Failed"},\
-              { TEST_NAME => "server_weight2", TEST_SUCCESS => "Success"}]
+    jenkins_id = results['job_build_number']
+    start_time = results['timestamp']
+    target = results['target']
+    box = results['box']
+    product = results['product']
+    mariadb_version = results['version']
+    test_code_commit_id = results['maxscale_system_test_commit'] #? what is that ?
+    maxscale_commit_id = results['maxscale_commit']
+    job_name = results['job_name']
+    tests = Array.new
+    results['tests'].each do |test|
+      tests.push({TEST_NAME => test[TEST_NAME], TEST_SUCCESS => test[TEST_SUCCESS]})
+    end
     #</STUB>
 
 
@@ -125,10 +127,10 @@ def parse_options
     end
   end
   return input_file_path
-end 
+end
 
 def main
-  input_file_path = parse_options 
+  input_file_path = parse_options
 
   writer = BuildResultsWriter.new
   writer.write_results_from_input_file(input_file_path)
