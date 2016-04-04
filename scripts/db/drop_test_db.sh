e4d6655b5758f851cbebd2112e4e46ec77f489e6
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-04-04 14:50:14 +0300

    Update drop_test_db.sh

--- scripts/db/drop_test_db.sh
+++ scripts/db/drop_test_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./scripts/db/drop_db.sh -H localhost -P 3306 -u test_bot -p pass -d test_results_db
+./scripts/db/drop_db.sh -H localhost -P 3306 -u test_bot -p pass -d ./scripts/db/test_results_db
