add1b4a75aaa2c9e2e89e641a30a3d8b6c6ba66e
galiaf95 <galiaf1995@gmail.com>
2016-04-11 13:15:49 +0300

    #6885 6648 test added to tests

--- spec/integration_tasks.rb
+++ spec/integration_tasks.rb
@@ -3,10 +3,12 @@ task :run_integration do
   task :task_shell_command_testing_example do |t| RakeTaskManager.new(t).run_integration end
   task :task_6819_show_box_info do |t| RakeTaskManager.new(t).run_integration end
   task :task_6782_show_commands_exit_code do |t| RakeTaskManager.new(t).run_integration end
+  task :task_6648_generate_exit_code do |t| RakeTaskManager.new(t).run_integration end
 
   Rake::Task[:task_shell_command_testing_example].execute
   Rake::Task[:task_6819_show_box_info].execute
   Rake::Task[:task_6782_show_commands_exit_code].execute
+  Rake::Task[:task_6648_generate_exit_code].execute
 
   RakeTaskManager.get_failed_tests_info
 
