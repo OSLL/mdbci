5e50fd6ef11f07c921ab411586934ffabd28639d
KILFaT <kinyaevilfat@gmail.com>
2016-04-11 13:29:48 +0300

    #6863 tests for 6821_show_box_config_node

---
+++ spec/unit/6863_tests_for_6821_show_box_config_node.rb
@@ -0,0 +1,29 @@
+require 'rspec'
+require_relative '../spec_helper'
+require_relative '../../core/out'
+require_relative '../../core/exception_handler'
+require_relative '../../core/boxes_manager'
+require_relative '../../core/session'
+
+describe 'Session.showBoxNameByPath' do
+
+  before :all do
+    $out = Out.new
+    $session = Session.new
+    $session.isSilent = true
+    $session.mdbciDir = Dir.pwd
+    $exception_handler = ExceptionHandler.new
+    boxesPath = './BOXES'
+    $session.boxes = BoxesManager.new boxesPath
+    reposPath = './repo.d'
+    $session.repos = RepoManager.new reposPath
+  end
+
+  it '#boxesPlatformVersions.ubuntu' do
+    $session.boxPlatform = 'ubuntu'
+    exit_code = $session.boxesPlatformVersions
+    exit_code.should eq(0)
+
+  end
+
+end
