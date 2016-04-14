38a5e04e4fdb654ae8cba5e3b1c8adb9f4af967a
KILFaT <kinyaevilfat@gmail.com>
2016-04-14 08:43:28 +0300

    spec/unit/6755_show_platform_versions_spec.rb

--- spec/unit/6755_show_platform_versions_spec.rb
+++ spec/unit/6755_show_platform_versions_spec.rb
@@ -5,7 +5,7 @@ require_relative '../../core/exception_handler'
 require_relative '../../core/boxes_manager'
 require_relative '../../core/session'
 
-describe 'Session.boxesPlatformVersions' do
+describe 'Session.getBoxesPlatformVersions' do
 
   before :all do
     $out = Out.new
@@ -19,51 +19,51 @@ describe 'Session.boxesPlatformVersions' do
     $session.repos = RepoManager.new reposPath
   end
 
-  it '#boxesPlatformVersions.ubuntu' do
+  it '#getBoxesPlatformVersions.ubuntu' do
     $session.boxPlatform = 'ubuntu'
-    exit_code = $session.boxesPlatformVersions
+    exit_code = $session.getBoxesPlatformVersions
     exit_code.should eq(0)
 
   end
 
-  it '#boxesPlatformVersions.centos' do
+  it '#getBoxesPlatformVersions.centos' do
     $session.boxPlatform = 'centos'
-    exit_code = $session.boxesPlatformVersions
+    exit_code = $session.getBoxesPlatformVersions
     exit_code.should eq(0)
 
   end
 
-  it '#boxesPlatformVersions.debian' do
+  it '#getBoxesPlatformVersions.debian' do
     $session.boxPlatform = 'debian'
-    exit_code = $session.boxesPlatformVersions
+    exit_code = $session.getBoxesPlatformVersions
     exit_code.should eq(0)
 
   end
 
-  it '#boxesPlatformVersions.opesuse' do
+  it '#getBoxesPlatformVersions.opesuse' do
     $session.boxPlatform = 'opensuse'
-    exit_code = $session.boxesPlatformVersions
+    exit_code = $session.getBoxesPlatformVersions
     exit_code.should eq(0)
 
   end
 
-  it '#boxesPlatformVersions.ubuntu_trusty' do
+  it '#getBoxesPlatformVersions.ubuntu_trusty' do
     $session.boxPlatform = 'ubuntu_trusty'
-    exit_code = $session.boxesPlatformVersions
+    exit_code = $session.getBoxesPlatformVersions
     exit_code.should eq(1)
 
   end
 
-  it '#boxesPlatformVersions.debian7' do
+  it '#getBoxesPlatformVersions.debian7' do
     $session.boxPlatform = 'debian7'
-    exit_code = $session.boxesPlatformVersions
+    exit_code = $session.getBoxesPlatformVersions
     exit_code.should eq(1)
 
   end
 
-  it '#boxesPlatformVersions.centos6' do
+  it '#getBoxesPlatformVersions.centos6' do
     $session.boxPlatform = 'centos6'
-    exit_code = $session.boxesPlatformVersions
+    exit_code = $session.getBoxesPlatformVersions
     exit_code.should eq(1)
 
   end
