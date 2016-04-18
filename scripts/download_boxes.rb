261d8cde99eb0b01a4560c2a17311aee85ce3e48
KILFaT <kinyaevilfat@gmail.com>
2016-04-18 08:14:53 +0300

    #6827 divide download_boxes.rb script at functions

--- scripts/download_boxes.rb
+++ scripts/download_boxes.rb
@@ -4,12 +4,19 @@ require 'json'
 require 'getoptlong'
 require 'net/http'
 require 'fileutils'
+class Download_boxes
+
 
 PROVIDER = 'provider'
 PLATFORM = 'platform'
 PLATFORM_VERSION = 'platform_version'
 BOX = 'box'
 BOX_VERSION = 'box_version'
+	puts 'STATIC!!1'
+init()
+
+def init
+	puts 'INIT!!1'
 
 opts = GetoptLong.new(
     [ '--dir', '-d', GetoptLong::REQUIRED_ARGUMENT ],
@@ -22,11 +29,15 @@ dir = nil
 boxes_dir = nil
 
 force = false
+parseOpts()
+end
 
-opts.each do |opt, arg|
-  case opt
-    when '--help'
-      puts <<-EOF
+def parseOpts
+	puts 'PARSING!!1'
+  opts.each do |opt, arg|
+    case opt
+      when '--help'
+        puts <<-EOF
 download_boxes OPTION
 
 -d, --dir:
@@ -42,47 +53,59 @@ download_boxes OPTION
 -f, --force
   if directories already exists - they will be overwritten
       EOF
-      exit 0
-    when '--force'
-      force = true
-    when '--dir'
-      dir = arg
-    when '--boxes-dir'
-      boxes_dir = arg
+        exit 0
+      when '--force'
+        force = true
+      when '--dir'
+        dir = arg
+      when '--boxes-dir'
+        boxes_dir = arg
+    end
   end
 end
 
-# get BOXES/*.json
-boxes = Hash.new
-boxes_files = Dir.glob(boxes_dir + '*.json', File::FNM_DOTMATCH)
-boxes_files.each do |boxes_file|
-  next if ( boxes_file.to_s.include? 'boxes_aws' )
-  next if ( boxes_file.to_s.include? 'boxes_docker' )
-  next if ( boxes_file.to_s.include? 'boxes_mdbci' )
-  boxes_json = JSON.parse(File.read boxes_file)
-  puts 'Box file: ' + boxes_file
-  boxes = boxes.merge boxes_json
-end
-
-boxes_quantity = boxes.length
-puts 'Boxes quantity: ' + boxes_quantity.to_s
-
-boxes_paths = Array.new
-
-boxes_counter = 0
-box_atlas_url = ''
-boxes.each do |box|
-  box_name = box[1][BOX].to_s
-  box_version = box[1][BOX_VERSION].to_s
-  provider = box[1][PROVIDER].to_s
-
-  if box_name =~ /\A#{URI::regexp(['http', 'https'])}\z/
-    box_atlas_url = box_name
-  else
-    box_name = box[1][BOX].to_s.split('/')
-    box_atlas_url = "https://atlas.hashicorp.com/#{box_name[0]}/boxes/#{box_name[1]}/versions/#{box_version}/providers/#{provider}.box"
+def main
+  # get BOXES/*.json
+  boxes = Hash.new
+  boxes_files = Dir.glob(boxes_dir + '*.json', File::FNM_DOTMATCH)
+  boxes_files.each do |boxes_file|
+    next if ( boxes_file.to_s.include? 'boxes_aws' )
+    next if ( boxes_file.to_s.include? 'boxes_docker' )
+    next if ( boxes_file.to_s.include? 'boxes_mdbci' )
+    boxes_json = JSON.parse(File.read boxes_file)
+    puts 'Box file: ' + boxes_file
+    boxes = boxes.merge boxes_json
   end
 
+  boxes_quantity = boxes.length
+  puts 'Boxes quantity: ' + boxes_quantity.to_s
+
+  boxes_paths = Array.new
+end
+
+def parseFiles
+  boxes_counter = 0
+  box_atlas_url = ''
+  boxes.each do |box|
+    box_name = box[1][BOX].to_s
+    box_version = box[1][BOX_VERSION].to_s
+    provider = box[1][PROVIDER].to_s
+
+    if box_name =~ /\A#{URI::regexp(['http', 'https'])}\z/
+      box_atlas_url = box_name
+    else
+      box_name = box[1][BOX].to_s.split('/')
+      box_atlas_url = "https://atlas.hashicorp.com/#{box_name[0]}/boxes/#{box_name[1]}/versions/#{box_version}/providers/#{provider}.box"
+    end
+
+    downloadBoxes
+    
+  end
+  puts "INFO: Boxes loaded #{boxes_counter}/#{boxes_quantity}"
+  boxes_paths_file = File.absolute_path([dir, "boxes_paths"].join('/'))
+end
+
+def downloadBoxes
   if box_atlas_url =~ /\A#{URI::regexp(['http', 'https'])}\z/
     boxes_counter += 1
     puts "INFO: #{boxes_counter}/#{boxes_quantity}, downloading from url: '#{box_atlas_url}'"
@@ -105,7 +128,7 @@ boxes.each do |box|
         at_exit { puts "ERROR: file '#{downloaded_box_path}' already exists" }
         exit 1
       else
-        puts "WARNING: file '#{downloaded_box_path} will be overwritten"
+       puts "WARNING: file '#{downloaded_box_path} will be overwritten"
         File.delete downloaded_box_path
       end
     end
@@ -127,24 +150,24 @@ boxes.each do |box|
     puts "INFO: Box will not be loaded - url #{box_atlas_url} is wrong"
   end
 end
-puts "INFO: Boxes loaded #{boxes_counter}/#{boxes_quantity}"
 
-boxes_paths_file = File.absolute_path([dir, "boxes_paths"].join('/'))
-
-if Dir.exists?(dir) && File.exists?(boxes_paths_file )
-  if !force
-    at_exit { puts "ERROR: file '#{boxes_paths_file }' already exists" }
-    exit 1
-  else
-    puts "WARNING: file '#{boxes_paths_file}' will be overwritten"
-    File.delete boxes_paths_file
+def pathListStoring
+  if Dir.exists?(dir) && File.exists?(boxes_paths_file )
+    if !force
+      at_exit { puts "ERROR: file '#{boxes_paths_file }' already exists" }
+      exit 1
+    else
+      puts "WARNING: file '#{boxes_paths_file}' will be overwritten"
+      File.delete boxes_paths_file
+    end
   end
-end
 
-File.open(boxes_paths_file, 'w') do |f|
-  boxes_paths.each do |path|
-    f.puts path
+  File.open(boxes_paths_file, 'w') do |f|
+    boxes_paths.each do |path|
+      f.puts path
+    end
   end
-end
 
-puts "INFO: File with boxes paths is stored as '#{boxes_paths_file}'"
+  puts "INFO: File with boxes paths is stored as '#{boxes_paths_file}'"
+end
+end
