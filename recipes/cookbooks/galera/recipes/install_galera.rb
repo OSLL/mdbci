f91661760c13d86e1958b1a4959bea2c1d18e7af
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-06-03 14:26:09 +0300

    #7013: possible fix

--- recipes/cookbooks/galera/recipes/install_galera.rb
+++ recipes/cookbooks/galera/recipes/install_galera.rb
@@ -7,7 +7,7 @@ include_recipe "galera::galera_repos"
 [
   "rsync", "sudo", "sed", 
   "coreutils", "util-linux", "curl", "grep", 
-  "findutils", "gawk", "iproute"
+  "findutils", "gawk", "iproute", "initscripts", "debianutils"
 ].each do |pkg|
   package pkg
 end
