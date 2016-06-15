a8d48ec1a170fb5f0fd4b94cee91624f1e071312
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-06-15 15:07:34 +0300

    #7039: fixed

--- scripts/build_parser/parse_ctest_log.rb
+++ scripts/build_parser/parse_ctest_log.rb
@@ -183,7 +183,7 @@ class CTestParser
     @failed_ctest_info = Array.new
     @fail_ctest_counter = 0
     ctest_log.each do |line|
-      test_end_regex = /(\d+)\/(\d+)\s+Test\s+#(\d+):[\s]+([^\s]+)\s+[\.\*]+([^\d]+)\s+([\d\.]+)/
+      test_end_regex = /(\d+)\/(\d+)\s+Test\s+#(\d+):[\s]+([^\s]+)\s+[\.\*]+([^\d]+)([\d\.]+)/
       if line =~ test_end_regex
         test_index_number = line.match(test_end_regex).captures[0]
         test_number = line.match(test_end_regex).captures[2]
