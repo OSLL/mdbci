9d04b5a0b99b1765517ef7e50cb44d023af131ac
galiaf95 <galiaf1995@gmail.com>
2016-06-22 16:05:58 +0300

    #7093 refactored config test

--- spec/full_configs_test.rb
+++ spec/full_configs_test.rb
@@ -1,6 +1,24 @@
-GLOBAL_PREFIX = "full_config_test"
+require 'rspec'
+require 'open3'
+require 'fileutils'
+require 'json'
+
+GLOBAL_PREFIX = 'full_config_test'
+
 TEMPLATE_COOKBOOK_PATH = 'cookbook_path'
 TEMPLATE_AWS_CONFIG = 'aws_config'
+
+NODES = %W(node0 node1 node2 node3 galera0 galera1 galera2 galera3 maxscale)
+NODES_LITE = %W(node0 galera0 maxscale)
+
+CONFIGS_DIRECTORY = 'confs'
+CONFIGS = %W(aws aws_lite docker docker_lite libvirt libvirt_lite mdbci mdbci_lite)
+
+AWS = 'aws'
+DOCKER = 'docker'
+VBOX = 'vbox'
+LIBVIRT = 'libvirt'
+MDBCI = 'mdbci'
 
 def execute_bash(cmd)
   puts "Executing: [#{cmd}]"
@@ -15,56 +33,74 @@ def execute_bash(cmd)
   return exit_code
 end
 
-def find_nodes(config)
-  nodes = Array.new
-  template = JSON.parse(File.read(File.read("#{config}/template")))
-  template.each do |possible_node|
-    if possible_node[0] != TEMPLATE_AWS_CONFIG and possible_node[0] != TEMPLATE_COOKBOOK_PATH
-      nodes.push possible_node[0]
+def validate_template(template)
+  return execute_bash("./mdbci validate_template --template #{template}")
+end
+
+def generate(template, config_name)
+  return execute_bash("./mdbci --template #{template} generate #{config_name}")
+end
+
+def up(config_name)
+  return execute_bash("./mdbci up #{config_name}")
+end
+
+def ssh(config_name)
+  return execute_bash("./mdbci ssh --command ls #{config_name}")
+end
+
+def clean_environment
+  CONFIGS.each do |config|
+    config_directory = "#{GLOBAL_PREFIX}_#{config}"
+    if !config_directory.include?(MDBCI) and Dir.exists?(config_directory)
+      root_dir = Dir.pwd
+      Dir.chdir config_directory
+      execute_bash 'vagrant destroy -f'
+      Dir.chdir root_dir
     end
+    FileUtils.rm_rf config_directory
   end
-  return nodes
 end
 
-def validate(tempate)
-  return execute_bash("./mdbci validate_template #{template}")
-end
+describe nil do
+  CONFIGS.each do |config|
 
-def generate(template, conf_name)
-  return execute_bash("./mdbci --template #{template} generate #{conf_name}")
-end
+    next unless config.to_s.include? LIBVIRT
 
-def up(conf_name)
-  return execute_bash("./mdbci up #{conf_name}")
-end
-
-def ssh(config)
-  return execute_bash("./mdbci ssh --command ls #{conf_name}")
-end
-
-Dir.glob('confs/*.json') do |file|
-
-  it "Validates template: #{file}" do
-    validate(file).should eql 0
-  end
-
-  it "Generates template: #{file} to config: #{File.basename(file, File.extname(file))}" do
-    generate(file).should eql 0
-  end
-
-  unless(file.include? "aws" or file.include? "vbox")
-
-    it "Starts config: #{File.basename(file, File.extname(file))}" do
-      up(file).should eql 0
+    before :all do
+      clean_environment
     end
 
-    it "Runs ssh command on nodes for config: #{File.basename(file, File.extname(file))}" do
-      nodes_namesfind_nodes(File.basename(file, File.extname(file)))
-      nodes_names.each_char do |node_name|
-        ssh(node_name).should eql 0
+    after :all do
+      clean_environment
+    end
+
+    template_path = "#{CONFIGS_DIRECTORY}/#{config}.json"
+    generated_config_name = "#{GLOBAL_PREFIX}_#{config}"
+
+    it "Validating template: #{template_path}" do
+      validate_template(template_path).should eql 0
+    end
+    it "Generating template: #{template_path} to config: #{generated_config_name}" do
+      generate(template_path, generated_config_name).should eql 0
+    end
+    unless config.to_s.include? MDBCI # MDBCI nodes do not need to be started
+      it "Starting config: #{generated_config_name}" do
+        up(generated_config_name).should eql 0
       end
     end
-
+    if generated_config_name.to_s.include? 'lite'
+      NODES_LITE.each do |node_name|
+        it "Running ssh command on nodes for config: #{generated_config_name}" do
+          ssh("#{generated_config_name}/#{node_name}").should eql 0
+        end
+      end
+    else
+      NODES.each do |node_name|
+        it "Running ssh command on nodes for config: #{generated_config_name}" do
+          ssh("#{generated_config_name}/#{node_name}").should eql 0
+        end
+      end
+    end
   end
-
 end
