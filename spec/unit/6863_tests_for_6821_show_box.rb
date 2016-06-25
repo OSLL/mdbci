6a650b10ea43a327ea7c44ab24721b27a1c76ea5
KILFaT <kinyaevilfat@gmail.com>
2016-06-25 10:35:47 +0300

    #6863 base test added

---
+++ spec/unit/6863_tests_for_6821_show_box.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require_relative '../spec_helper'
+require_relative '../../core/out'
+require_relative '../../core/exception_handler'
+require_relative '../../core/boxes_manager'
+require_relative '../../core/session'
+
+describe 'Session' do
+
+  before :all do
+    $out = Out.new
+    $session = Session.new
+    $session.isSilent = true
+    $session.mdbciDir = Dir.pwd
+    $exception_handler = ExceptionHandler.new
+    boxesPath = './BOXES'
+    $session.boxes = BoxesManager.new boxesPath
+    reposPath = './repo.d'
+    $session.repos = RepoManager.new reposPath
+  end
+
+  it '#show box should exit with zero code for aws/vbox node' do
+    $session.showBoxNameByPath(ENV['pathToConfigToVBOXNode'].to_s).should(eql(0))
+  end
+
+end
