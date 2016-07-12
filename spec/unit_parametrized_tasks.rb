885d6451f84f281ff7a368f8549e4c46f5fa3425
TankBerlin <tank.berlin@gmail.com>
2016-07-12 10:19:17 +0200

    7144_copying_old_config_to_new

--- spec/unit_parametrized_tasks.rb
+++ spec/unit_parametrized_tasks.rb
@@ -11,6 +11,7 @@ task :run_unit_parametrized do
   task :task_6821_show_box_config_node, [:pathToConfigNode, :pathToConfig] do |t, args| RakeTaskManager.new(t).run_parametrized(args) end
   task :task_6818_search_box_name_by_config, [:configPath, :nodeName] do |t, args| RakeTaskManager.new(t).run_parametrized(args) end
   task :task_7159_main_cloning_func_check_provider, [:pathToConfigToMDBCILibvirtProviderNode, :pathToConfigToMDBCIDockerProviderNode, :pathToConfigToMDBCIBadNode] do |t, args| RakeTaskManager.new(t).run_parametrized(args) end
+  task :task_7144_copying_old_config_to_new, [:pathToOldNotExistingConfig, :pathToOldValidConfigWithoutValidNode, :pathToOldValidConfig, :pathToNotValidNewConfig, :pathToValidNewConfig] do |t, args| RakeTaskManager.new(t).run_parametrized(args) end
 
 
   Rake::Task[:task_6639_ssh_exit_code].execute({ :pathToConfigToVBOXNode=>'TEST/vboxnode', :pathToConfigToMDBCINode=>'TEST1/mdbcinode', :pathToConfigToMDBCIBadNode=>'TEST2/mdbcinodebad', :pathToConfigToMDBCIFolder=>'TEST1' })
@@ -24,6 +25,7 @@ task :run_unit_parametrized do
   Rake::Task[:task_6821_show_box_config_node].execute({ :pathToConfigNode=>'TEST/vboxnode', :pathToConfig=>'TEST' })
   Rake::Task[:task_6818_search_box_name_by_config].execute({ :configPath=>'confs/mdbci_up_aws_test_config.json', :nodeName=>'galera0' })
   Rake::Task[:task_7159_main_cloning_func_check_provider].execute({ :pathToConfigToMDBCILibvirtProviderNode=>'TEST0/node0', :pathToConfigToMDBCIDockerProviderNode=>'TEST1/node0', :pathToConfigToMDBCIBadNode=>'TEST2/node0' })
+  Rake::Task[:task_7144_copying_old_config_to_new].execute({ :pathToOldNotExistingConfig=>'not_valid_catalog', :pathToOldValidConfigWithoutValidNode=>'TEST', :pathToOldValidConfig=>'TEST0', :pathToNotValidNewConfig=>'TEST1', :pathToValidNewConfig=>'TEST2' })
 
   RakeTaskManager.get_failed_tests_info
 end
