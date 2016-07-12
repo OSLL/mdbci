ce21067f614730eeba0b25a21b44bc2574b768f9
TankBerlin <tank.berlin@gmail.com>
2016-07-12 13:10:36 +0200

    7144_copying_old_config_to_new

--- spec/unit/7144_copying_old_config_to_new_spec.rb
+++ spec/unit/7144_copying_old_config_to_new_spec.rb
@@ -8,19 +8,19 @@ require_relative '../../core/clone'
 describe 'Clone' do
 
   it '#clone should exit with non-zero code if path to old config is not existing' do
-    $clone.clone(ENV['pathToOldNotExistingConfig'].to_s, ENV['pathToValidNewConfig']).should(eql(1))
+    $clone.copyOldConfigDirectoryToNew(ENV['pathToOldNotExistingConfig'].to_s, ENV['pathToValidNewConfig']).should(eql(1))
   end
 
   it '#clone should exit with non-zero code if nodes in the old config are not valid' do
-    $clone.clone(ENV['pathToOldValidConfigWithoutValidNode'].to_s, ENV['pathToValidNewConfig']).should(eql(1))
+    $clone.copyOldConfigDirectoryToNew(ENV['pathToOldValidConfigWithoutValidNode'].to_s, ENV['pathToValidNewConfig']).should(eql(1))
   end
 
   it '#clone should exit with non-zero code if new path exists' do
-    $clone.clone(ENV['pathToOldValidConfig'], ENV['pathToNotValidNewConfig']).should(eql(1))
+    $clone.copyOldConfigDirectoryToNew(ENV['pathToOldValidConfig'], ENV['pathToNotValidNewConfig']).should(eql(1))
   end
 
   it '#clone should exit with zero code if path to old config is valid, nodes in it are valid and new config path is not exists' do
-    $clone.clone(ENV['pathToOldValidConfig'], ENV['pathToValidNewConfig']).should(eql(1))
+    $clone.copyOldConfigDirectoryToNew(ENV['pathToOldValidConfig'], ENV['pathToValidNewConfig']).should(eql(1))
   end
 
 end
