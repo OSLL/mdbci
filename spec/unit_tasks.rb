909602c61696b6b407938612e1f171de2210e3e4
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-07-13 15:53:35 +0300

    #7187: added non-existing test

--- spec/unit_tasks.rb
+++ spec/unit_tasks.rb
@@ -16,6 +16,7 @@ task :run_unit do
   task :task_7185_helper_functions do |t| RakeTaskManager.new(t).run_unit end
   task :task_7045_default_packages_to_empty_machines do |t| RakeTaskManager.new(t).run_unit end
   task :task_7154_refer_old_node_to_new do |t| RakeTaskManager.new(t).run_unit end
+  task :task_zero do |t| RakeTaskManager.new(t).run_unit end
 
   Rake::Task[:task_generator].execute
   Rake::Task[:task_6819_show_box_info].execute
@@ -33,6 +34,7 @@ task :run_unit do
   Rake::Task[:task_7185_helper_functions].execute
   Rake::Task[:task_7045_default_packages_to_empty_machines].execute
   Rake::Task[:task_7154_refer_old_node_to_new].execute
+  Rake::Task[:task_zero].execute 
 
   RakeTaskManager.get_failed_tests_info
 
