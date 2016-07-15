8d8563e341f6052af76ddb409576b9b5af3c031b
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-07-15 16:19:28 +0300

    #7227: adds fix for first argument

--- scripts/build_parser/coredump_finder.sh
+++ scripts/build_parser/coredump_finder.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
 # Usage 
-# ./scripts/build_parser/coredump_finder.sh 200 url
+# ./scripts/build_parser/coredump_finder.sh run_test-200 url
 
 if [ $# -ne 2 ];
 then 
-	echo "Script require two arguments: run_test build number and output format (url or files)"
+	echo "Script require two arguments: run_test build name and output format (url or files)"
 	exit 1
 fi
 
 BASE_DIR="/home/vagrant/"
 LOGS_PATH="${BASE_DIR}LOGS"
 
-run_testNumber=${1}
+buildId=${1}
 showFileList=${2}
+
+# HACK for *matrix* jobs
+if [[ $buildId == *"matrix"* ]]
+then
+	buildNumber=`echo $buildId | grep -o '[0-9]*$'`
+	buildId="$buildId/*-$buildNumber"
+fi
+
+echo "$buildId"
+
 if [[ "$showFileList" == "url" ]]
 then
-	find $LOGS_PATH/run_test-${run_testNumber} | grep core | sed -e "s|${BASE_DIR}|http://max-tst-01.mariadb.com/|"
+	find $LOGS_PATH/${buildId} | grep core | sed -e "s|${BASE_DIR}|http://max-tst-01.mariadb.com/|"
 	exit 0
 fi
-find $LOGS_PATH/run_test-${run_testNumber} | grep core 
+find $LOGS_PATH/${buildId} | grep core 
