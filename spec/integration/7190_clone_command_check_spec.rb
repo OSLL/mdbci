93abe195d239868c724b9be4ca7f59517f696f71
Timofey Turenko <timofey.turenko@mariadb.com>
2016-07-19 10:38:25 +0200

    7190_implement_parametrized_integr_tests_for_clone_command

--- spec/integration/7190_clone_command_check_spec.rb
+++ spec/integration/7190_clone_command_check_spec.rb
@@ -3,30 +3,46 @@ require_relative '../spec_helper'
 
 TEMPLATE_LIBVIRT = "spec/configs/template/libvirt_lite.json"
 MACHINE_LIBVIRT = "7190_test_libvirt_machine"
-TEMPLATE_DOCKER = "../spec/configs/template/docker.json"
-MACHINE_DOCKER = "7190_test_docker_machine"
 NEW_PATH_LIBVIRT = "7190_new_test_libvirt_machine"
+TEMPLATE_DOCKER = "spec/configs/template/docker_lite.json"
+MACHINE_DOCKER = "7190_test_docker_machine"
+NEW_PATH_DOCKER = "7190_new_test_docker_machine"
 
 
 describe 'test_spec' do
 
   before :all do
-    puts Dir.pwd
-    puts Dir.entries('spec/configs/template')
-    `"./mdbci --template #{TEMPLATE_LIBVIRT} generate #{MACHINE_LIBVIRT}"`
-    `"ls"`
-    `"./mdbci up #{MACHINE_LIBVIRT}"`
-    `"cd #{MACHINE_LIBVIRT}"`
-    `vagrant halt`
-    `cd -`
+    setUp(TEMPLATE_LIBVIRT, MACHINE_LIBVIRT)
+    setUp(TEMPLATE_DOCKER, MACHINE_DOCKER)
   end
 
   executeShellCommandsAndTestExitCode ([
-    {'shell_command'=>"./mdbci clone #{MACHINE_LIBVIRT} #{NEW_PATH_LIBVIRT}", 'expectation'=>0}
+    {'shell_command'=>"./mdbci clone #{MACHINE_LIBVIRT} #{NEW_PATH_LIBVIRT}", 'expectation'=>0}, 
+    {'shell_command'=>"./mdbci clone #{MACHINE_DOCKER} #{NEW_PATH_DOCKER}", 'expectation'=>0}
   ])
 
-#  after :all do
-#      `vagrant destroy -f`
-#  end
+  after :all do
+    tearDown(MACHINE_LIBVIRT)
+    tearDown(MACHINE_DOCKER)    
+  end
 
+end
+
+
+def setUp(template, name_machine)
+  system("./mdbci --template #{template} generate #{name_machine}")
+  system("./mdbci up #{name_machine}")
+  system("cd #{name_machine}")
+  puts "shut down the running machine #{name_machine}"
+  system("vagrant halt")
+  system("cd -")
+end
+
+
+def tearDown(name_machine)
+  system("cd #{name_machine}")
+  system("vagrant destroy -f")
+  puts "destroy the machine #{name_machine}"
+  system("cd -")
+  system("rm -r #{name_machine}")
 end
