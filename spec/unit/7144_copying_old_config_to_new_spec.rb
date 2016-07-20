7682c80f6fcbb4091e97e45b907c075908e6916e
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-07-20 12:28:32 +0300

    #7264: renames copyOldConfigDirectoryToNew to copy_old_config_to_new at 7144_copying_old_config_to_new_spec.rb

--- spec/unit/7144_copying_old_config_to_new_spec.rb
+++ spec/unit/7144_copying_old_config_to_new_spec.rb
@@ -12,23 +12,23 @@ EXISTING_NEW_PATH = "spec/unit/test_empty_folder"
 describe 'Clone' do
 
   it '#clone should exit with non-zero code if path to old config is not existing' do
-    lambda{ copyOldConfigDirectoryToNew(OLD_NOT_EXISTING_PATH, VALID_NEW_PATH) }.should  raise_error(RuntimeError, "Old config directory pathToOldNotExistingConfig not found")
+    lambda{ copy_old_config_to_new(OLD_NOT_EXISTING_PATH, VALID_NEW_PATH) }.should  raise_error(RuntimeError, "Old config directory pathToOldNotExistingConfig not found")
   end
 
   it '#clone should exit with non-zero code if nodes in old config are not existing' do
     FileUtils.mkdir(EMPTY_FOLDER)
-    lambda{ copyOldConfigDirectoryToNew(EMPTY_FOLDER, VALID_NEW_PATH) }.should raise_error(RuntimeError, "In old config directory spec/unit/test_empty_folder nodes are not found")
+    lambda{ copy_old_config_to_new(EMPTY_FOLDER, VALID_NEW_PATH) }.should raise_error(RuntimeError, "In old config directory spec/unit/test_empty_folder nodes are not found")
     FileUtils.rm_rf(EMPTY_FOLDER)
   end
 
   it '#clone should exit with non-zero code if new folder is existing' do
     FileUtils.mkdir(EMPTY_FOLDER)
-    lambda{ copyOldConfigDirectoryToNew(VALID_OLD_PATH, EXISTING_NEW_PATH) }.should raise_error(RuntimeError, "New config directory spec/unit/test_empty_folder is existing")
+    lambda{ copy_old_config_to_new(VALID_OLD_PATH, EXISTING_NEW_PATH) }.should raise_error(RuntimeError, "New config directory spec/unit/test_empty_folder is existing")
     FileUtils.rm_rf(EMPTY_FOLDER)
   end
 
   it '#clone should copying all directories and subdirectories' do
-    copyOldConfigDirectoryToNew(VALID_OLD_PATH, VALID_NEW_PATH)
+    copy_old_config_to_new(VALID_OLD_PATH, VALID_NEW_PATH)
     FileUtils.rm_rf(VALID_NEW_PATH)
   end
 
