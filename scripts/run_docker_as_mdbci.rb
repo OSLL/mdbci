c65143eb7618d67694ee5fb211d9f4910860f59b
Alexander Kaluzhny <galiaf1995@gmail.com>
2016-07-25 13:46:57 +0300

    #7222 refactored scripts/run_docker_as_mdbci.rb

--- scripts/run_docker_as_mdbci.rb
+++ scripts/run_docker_as_mdbci.rb
@@ -12,18 +12,20 @@ class PpcFromDocker
   INFORMATION_TAG = 'INFO: '
   ERROR_TAG = 'ERROR:'
 
-  TEMPLATE_ARGUMENT_REQUIRED_ERROR = 'You must provide template_path'
+  TEMPLATE_ARGUMENT_REQUIRED_ERROR = 'You must provide config name'
 
   HELP_MESSAGE = <<-EOF
 Script creates test mdbci boxes, that are made from docker running instances or removes them
 Usage:
-    ./scripts/mdbci_from_docker.rb TEMPLATE
+1) Generate mdbci(ppc) config from docker
+    ./scripts/mdbci_from_docker.rb ORIGIN_DOCKER_CONFIG_NAME
+1) Remove generated mdbci(ppc) config
+    ./scripts/mdbci_from_docker.rb -r GENERATED_(MDBCI)PPC_CONFIG_NAME
+Options:
+    -r          remove generated mdbci config (and all leftovers)
 Arguments:
-    TEMPLATE    path to docker config template_path
+    CONFIG_NAME    path to docker or (mdbci)ppc config
   EOF
-
-  GLOBAL_PREFIX_DOCKER_MACHINE = 'mdbci_testing_config_docker'
-  GLOBAL_PREFIX_MDBCI_FROM_DOCKER_MACHINE = 'mdbci_testing_config_mdbci_from_docker'
 
   TEMPLATE_COOKBOOK_PATH = 'cookbook_path'
   TEMPLATE_AWS_CONFIG = 'aws_config'
@@ -39,13 +41,10 @@ Arguments:
 
   $is_for_removing = false
 
-  def initialize_mdbci_environment
-    $out = Out.new
-    $session = Session.new
-    $session.mdbciDir = Dir.pwd
-    $exception_handler = ExceptionHandler.new
-    $session.boxes = BoxesManager.new './BOXES'
-    $session.repos = RepoManager.new './repo.d'
+  attr_accessor :timestamp
+
+  def initialize
+    @timestamp = Time.now.strftime('%Y_%m_%d_%H_%M_%S')
   end
 
   def parse_options_and_args
@@ -69,64 +68,66 @@ Arguments:
     return template
   end
 
-  def get_nodes(template_path)
-    nodes = Array.new
-    template = JSON.parse(File.read("#{template_path}"))
-    template.each do |possible_node|
-      if possible_node[0] != TEMPLATE_AWS_CONFIG and possible_node[0] != TEMPLATE_COOKBOOK_PATH
-        nodes.push possible_node[0]
-      end
+  def generate_removing_script(config_name_ppc_from_docker, path_to_boxes_file, paths_to_keyfiles)
+    paths_to_keyfiles_array = Array.new
+    paths_to_keyfiles.each do |path_to_keyfile|
+      paths_to_keyfiles_array.push "\"#{path_to_keyfile}\""
     end
-    raise NODES_NOT_FOUND_ERROR if nodes.empty?
-    return nodes
-  end
-
-  def get_configs_names(template_path)
-    config_name = File.basename("#{Dir.pwd}/#{template_path}", File.extname("#{Dir.pwd}/#{template_path}"))
-    config_name_docker = "#{GLOBAL_PREFIX_DOCKER_MACHINE}_#{config_name}"
-    config_name_mdbci_from_docker = "#{GLOBAL_PREFIX_MDBCI_FROM_DOCKER_MACHINE}_#{config_name}"
-    return config_name_docker, config_name_mdbci_from_docker
+    paths_to_keyfiles_string = paths_to_keyfiles_array.join(', ')
+    removing_script = <<EOF
+require 'fileutils'
+def remove_config
+  [#{paths_to_keyfiles_string}].each { |path_to_keyfile| FileUtils.rm_rf(path_to_keyfile) }
+  FileUtils.rm_rf('#{path_to_boxes_file}')
+  FileUtils.rm_rf('#{config_name_ppc_from_docker}')
+end
+EOF
+    File.open("#{config_name_ppc_from_docker}/remove_config_completely.rb", 'w') do |file|
+      file.write removing_script
+    end
   end
 
   # return tuple: origin docker config name. ppc config name generated from origin docker config
-  def prepare_mdbci_environment(template_path)
-    config_name_docker, config_name_mdbci_from_docker = get_configs_names(template_path)
-    # Starting docker machines
-    execute_bash("./mdbci --template #{template_path} generate #{config_name_docker}")
-    execute_bash("./mdbci up #{config_name_docker}")
-    # Creating boxes config for running machines
+  def prepare_mdbci_environment(config_name_docker)
+    config_name_mdbci_from_docker = "#{config_name_docker}_#{@timestamp}"
+    paths_to_keyfiles = Array.new
     boxes_config = Hash.new
-    config_hash = JSON.parse(File.read(template_path))
-    nodes_names = get_nodes template_path
+    template_path = get_template_path(config_name_docker)
+    template_hash = JSON.parse(File.read(template_path))
+    nodes_names = get_nodes(config_name_docker)
     nodes_names.each do |node_name|
       box_config = BOX_CONFIG_TEMPLATE.clone
       # Getting ip of node
       private_ip_output = execute_bash("./mdbci show private_ip #{config_name_docker}/#{node_name} --silent")
       box_config[:IP] = private_ip_output.to_s.split("\n")[-1]
-      # Copying keyfile to KEYS directory and adding it to box config for current node
-      box_name = execute_bash("./mdbci show box #{config_name_docker}/#{node_name} --silent")
-      box_name = box_name.delete!("\n")
-      File.open("KEYS/#{GLOBAL_PREFIX_MDBCI_FROM_DOCKER_MACHINE}_#{box_name}", 'w') do |file|
+      # Copying keyfile to KEYS directory and adding it to box config for current node)
+      path_to_keyfile = "KEYS/#{node_name}_#{config_name_mdbci_from_docker}"
+      paths_to_keyfiles.push(path_to_keyfile)
+      File.open(path_to_keyfile, 'w') do |file|
         file.write(File.read("#{config_name_docker}/.vagrant/machines/#{node_name}/docker/private_key"))
       end
-      box_config[:keyfile] = "#{GLOBAL_PREFIX_MDBCI_FROM_DOCKER_MACHINE}_#{box_name}"
+      box_config[:keyfile] = config_name_mdbci_from_docker
       # Getting platform and platform version
+      box_name = execute_bash("./mdbci show box #{config_name_docker}/#{node_name} --silent")
+      box_name = box_name.delete!("\n")
       boxes_hash = JSON.parse(File.read('BOXES/boxes_docker.json'))
       box_config[:platform] = boxes_hash[box_name]['platform']
       box_config[:platform_version] = boxes_hash[box_name]['platform_version']
       # Adding combining boxes configs
-      boxes_config["#{GLOBAL_PREFIX_MDBCI_FROM_DOCKER_MACHINE}_#{box_name}"] = box_config
+      new_box_name = "#{config_name_mdbci_from_docker}_#{node_name}"
+      boxes_config[new_box_name] = box_config
       # Generating config for mdbci node
-      config_hash[node_name]['box'] = "#{GLOBAL_PREFIX_MDBCI_FROM_DOCKER_MACHINE}_#{box_name}"
+      template_hash[node_name]['box'] = new_box_name
     end
     # Saving boxes config to file
-    File.open("BOXES/#{GLOBAL_PREFIX_MDBCI_FROM_DOCKER_MACHINE}.json", 'w') do |file|
+    path_to_boxes_file = "BOXES/#{config_name_mdbci_from_docker}.json"
+    File.open(path_to_boxes_file, 'w') do |file|
       file.write(boxes_config.to_json)
     end
     # Creating configs for mdbci machines
     Dir.mkdir config_name_mdbci_from_docker
     File.open("#{config_name_mdbci_from_docker}/template.json", 'w') do |file|
-      file.write(config_hash.to_json)
+      file.write(template_hash.to_json)
     end
     File.open("#{config_name_mdbci_from_docker}/mdbci_template", 'w') do |file|
       file.write("#{config_name_mdbci_from_docker}/template.json")
@@ -134,39 +135,25 @@ Arguments:
     File.open("#{config_name_mdbci_from_docker}/provider", 'w') do |file|
       file.write('mdbci')
     end
-    return config_name_docker, config_name_mdbci_from_docker
+    generate_removing_script(config_name_mdbci_from_docker, path_to_boxes_file, paths_to_keyfiles)
+    return config_name_mdbci_from_docker
   end
 
-  def remove_mdbci_environment(template_path)
-    config_name = File.basename("#{Dir.pwd}/#{template_path}", File.extname("#{Dir.pwd}/#{template_path}"))
-    config_name_docker = "#{GLOBAL_PREFIX_DOCKER_MACHINE}_#{config_name}"
-    config_name_mdbci_from_docker = "#{GLOBAL_PREFIX_MDBCI_FROM_DOCKER_MACHINE}_#{config_name}"
-    # Removing mavhine (if exists)
-    mdbci_root = Dir.pwd
-    Dir.chdir config_name_docker
-    execute_bash('vagrant destroy -f')
-    Dir.chdir mdbci_root
-    # Removing docker config directory
-    FileUtils.rm_rf config_name_docker
-    # Removing, generated from docker, mdbci config directory
-    FileUtils.rm_rf config_name_mdbci_from_docker
-    # Removing generated boxes
-    FileUtils.rm_rf "BOXES/#{GLOBAL_PREFIX_MDBCI_FROM_DOCKER_MACHINE}.json"
-    # Removing generated private keys
-    FileUtils.rm_rf Dir.glob("KEYS/#{GLOBAL_PREFIX_MDBCI_FROM_DOCKER_MACHINE}_*")
+  def remove_mdbci_environment(config_name_ppc_from_docker)
+    require_relative "../#{config_name_ppc_from_docker}/remove_config_completely"
+    remove_config
   end
 
   def start
-    template_path = parse_options_and_args
+    config_name = parse_options_and_args
     unless $is_for_removing
-      prepare_mdbci_environment template_path
+      prepare_mdbci_environment config_name
     else
-      remove_mdbci_environment template_path
+      remove_mdbci_environment config_name
     end
   end
 
 end
-
 
 if File.identical?(__FILE__, $0)
   PpcFromDocker.new.start
