5fe60533c7dc1a10ab17befa20424d77c5603d65
Timofey Turenko <timofey.turenko@mariadb.com>
2016-08-17 17:38:51 +0200

    fix ci_url_suffix

--- maxscale-ci.sh
+++ maxscale-ci.sh
@@ -10,8 +10,8 @@ if [ -z $ci_url ]; then
 	export ci_url="http://maxscale-jenkins.mariadb.com/ci-repository/"
 fi
 
-if [ -z $3 ]; then
-	~/mdbci-repository-config/maxscale.sh $ci_url/$ci $dest
-else
+#if [ -z $3 ]; then
+#	~/mdbci-repository-config/maxscale.sh $ci_url/$ci $dest
+#else
 	~/mdbci-repository-config/maxscale.sh $ci_url/$ci/$3/ $dest
-fi
+#fi
