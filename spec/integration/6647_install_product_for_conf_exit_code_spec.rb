9253d45910cd24c2210ef8864bd7f5ad127c8ade
galiaf95 <galiaf1995@gmail.com>
2016-08-21 10:45:43 +0200

    #7430 tests integration refactored

---
+++ spec/integration/6647_install_product_for_conf_exit_code_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require_relative '../spec_helper'
+
+CONF_DOCKER = ENV['mdbci_param_conf_docker']
+CONF_PPC = ENV['mdbci_param_conf_docker']
+
+def test_command (product, product_version, config_path)
+  product_name_parameter = ''
+  product_version_parameter = ''
+  product_name_parameter = "--product #{product}" if product != nil
+  product_version_parameter = "--product-version #{product_version}" if product_version != nil
+  return "./mdbci install_product #{product_name_parameter} #{product_version_parameter} #{config_path}"
+end
+
+def pretest_command (product, product_version, config_path)
+  product_name_parameter = ''
+  product_version_parameter = ''
+  product_name_parameter = "--product #{product}" if product != nil
+  product_version_parameter = "--product-version #{product_version}" if product_version != nil
+  return "./mdbci setup_repo #{product_name_parameter} #{product_version_parameter} #{config_path}"
+end
+
+describe 'test_spec' do
+  executeShellCommandsAndTestExitCode ([
+    {'shell_command'=>pretest_command('mariadb', '10.0', "#{CONF_DOCKER}"), 'expectation'=>0},
+    {'shell_command'=>test_command('mariadb', nil, "#{CONF_DOCKER}"), 'expectation'=>0},
+    {'shell_command'=>pretest_command('mariadb', '10.0', "#{CONF_PPC}"), 'expectation'=>0},
+    {'shell_command'=>test_command('mariadb', nil, "#{CONF_PPC}"), 'expectation'=>0}
+  ])
+end
--- spec/integration_parametrized_tasks.rb
+++ spec/integration_parametrized_tasks.rb
@@ -1,10 +1,13 @@
-task :run_integration_parametrized do
+namespace :run_integration_parametrized do
 
-  task :task_6646_setup_repo_exit_code, [:pathToConfigToVBOXNode, :pathToConfigToMDBCINode, :pathToConfigToMDBCIFolder, :pathToConfigToMDBCINode] do |t, args| RakeTaskManager.new(t).run_integration_parametrized(args) end
-  task :task_6647_install_product_exit_code, [:pathToConfigToVBOXNode, :pathToConfigToMDBCINode, :pathToConfigToMDBCIFolder, :pathToConfigToMDBCINode] do |t, args| RakeTaskManager.new(t).run_integration_parametrized(args) end
-  task :task_6970_show_box_exit_code, [:pathToConfigNode, :pathToConfig] do |t, args| RakeTaskManager.new(t).run_integration_parametrized(args) end
-  task :task_7294_show_network_confi, [:pathToNetworkConfig] do |t, args| RakeTaskManager.new(t).run_integration_parametrized(args) end
-
+  task :task_6646_setup_repo_for_conf_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER, PPC]) end
+  task :task_6646_setup_repo_for_node_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER, PPC]) end
+  task :task_6646_setup_repo_handling_exceptions_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER, PPC]) end
+  task :task_6647_install_product_for_conf_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER, PPC]) end
+  task :task_6647_install_product_for_node_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER, PPC]) end
+  task :task_6647_install_product_handling_exceptions_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER, PPC]) end
+  task :task_6970_show_box_config_node_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER]) end
+  task :task_7294_show_network_config do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER]) end
   task :task_show_tests_info do RakeTaskManager.get_failed_tests_info end
 
 end
