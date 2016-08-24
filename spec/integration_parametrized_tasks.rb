3fc19b875a63ec4454f9b49f0ce3cfabeec2d643
galiaf95 <galiaf1995@gmail.com>
2016-08-24 14:55:48 +0200

    #6933 refactored test helpers

--- spec/integration_parametrized_tasks.rb
+++ spec/integration_parametrized_tasks.rb
@@ -8,14 +8,7 @@ namespace :run_integration_parametrized do
   task :task_6647_install_product_handling_exceptions_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER, PPC]) end
   task :task_6970_show_box_config_node_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER]) end
   task :task_7294_show_network_config do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER]) end
-  task :task_show_tests_info do RakeTaskManager.get_failed_tests_info end
 
 end
 
-task :run_integration_parametrized_all do
-  Rake.application.in_namespace(:run_integration_parametrized) do |x|
-    x.tasks.each do |t|
-      t.invoke
-    end
-  end
-end
+RakeTaskManager.rake_finalize(:run_integration_parametrized)
