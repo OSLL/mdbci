d40b7504c9ae2bbf28d0ed73552d054a1cb3ade7
galiaf95 <galiaf1995@gmail.com>
2016-08-24 22:01:26 +0200

    #7435 merged integration

--- spec/unit_parametrized_tasks.rb
+++ spec/unit_parametrized_tasks.rb
@@ -13,7 +13,7 @@ namespace :run_unit_parametrized do
   task :task_7110_collectConfigurationNetworkInfo do |t| RakeTaskManager.new(t).run_unit_parametrized([DOCKER])  end
   task :task_6821_show_box_config_node do |t| RakeTaskManager.new(t).run_unit_parametrized([DOCKER])  end
   task :task_7109_ssh do |t| RakeTaskManager.new(t).run_unit_parametrized([DOCKER, PPC])  end
-  task :task_7435_reactor_helper do |t| RakeTaskManager.new(t).run_unit_parametrized([LIBVIRT])  end
+  task :task_7435_refactor_helper do |t| RakeTaskManager.new(t).run_unit_parametrized([LIBVIRT])  end
   task :task_show_tests_info do RakeTaskManager.get_failed_tests_info end
 
 end
