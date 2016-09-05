ec1d3e49f1d1ba376582800f0de8dfbb736b8675
galiaf95 <galiaf1995@gmail.com>
2016-09-05 08:36:02 +0200

    #7448 fixes

--- spec/integration_parametrized_tasks.rb
+++ spec/integration_parametrized_tasks.rb
@@ -6,9 +6,9 @@ namespace :run_integration_parametrized do
   task :task_6647_install_product_for_conf_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER, PPC]) end
   task :task_6647_install_product_for_node_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER, PPC]) end
   task :task_6647_install_product_handling_exceptions_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER, PPC]) end
-  task :task_6970_show_box_config_node_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([]) end
-  task :task_7294_show_network_config do |t| RakeTaskManager.new(t).run_integration_parametrized([]) end
-  task :task_7448_install_sysbench do |t| RakeTaskManager.new(t).run_integration_parametrized([]) end
+  task :task_6970_show_box_config_node_exit_code do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER]) end
+  task :task_7294_show_network_config do |t| RakeTaskManager.new(t).run_integration_parametrized([DOCKER]) end
+  task :task_7448_install_sysbench do |t| RakeTaskManager.new(t).run_integration_parametrized([LIBVIRT]) end
 
 end
 
