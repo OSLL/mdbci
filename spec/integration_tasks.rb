12df0cdafa1f98e092c22fe8c11ee331953fead5
galiaf95 <galiaf1995@gmail.com>
2016-09-20 02:37:11 +0200

    #7425 added tests

--- spec/integration_tasks.rb
+++ spec/integration_tasks.rb
@@ -4,6 +4,7 @@ namespace :run_integration do
   task :task_6819_show_box_info do |t| RakeTaskManager.new(t).run_integration end
   task :task_6782_show_commands_exit_code do |t| RakeTaskManager.new(t).run_integration end
   task :task_6648_generate_exit_code do |t| RakeTaskManager.new(t).run_integration end
+  task :task_7425_sysbench_parser do |t| RakeTaskManager.new(t).run_integration end
 
   task :task_show_tests_info do RakeTaskManager.get_failed_tests_info end
 
