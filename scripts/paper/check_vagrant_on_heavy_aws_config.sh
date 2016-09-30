1dd919e3d0e3c4cb6704d7673069fab82a5ae582
zmm <mark.zaslavskiy@gmail.com>
2016-09-30 11:30:29 +0200

    #7392: fix for experiment

--- scripts/paper/check_vagrant_on_heavy_aws_config.sh
+++ scripts/paper/check_vagrant_on_heavy_aws_config.sh
@@ -4,19 +4,20 @@
 # Generate configuration from confs/aws.json
 
 configuration="aws_`date +%s`"
-N=10
-./mdbci --template confs/aws.json generate  --override
+N=4
+./mdbci --template confs/aws_special.json generate ${configuration}  --override
 
 echo "N=$N"
-# Execute N times with mdbci and vagrant
+ Execute N times with mdbci and vagrant
 vagrant_fails=0
 echo "Vagrant"
 for i in $(seq 1 $N)
 do
 	cd ${configuration}
-	vagrant up 2>/dev/null 1>&2
+	vagrant up 2>&1 | tee "../${configuration}_log_vagrant_${i}"
 	exit_code="$?"
-	echo "$exit_code ($i/$N)"
+	echo "$exit_code ($i/$N)" > "../${configuration}_vagrant_${i}"
+	vagrant status >> "../${configuration}_vagrant_${i}"
 	vagrant_fails=$(($vagrant_fails + $exit_code))
 	vagrant destroy -f
 	cd ..
@@ -27,11 +28,12 @@ mdbci_fails=0
 echo "MDBCI"
 for i in $(seq 1 $N)
 do
-	./mdbci up ${configuration}  2>/dev/null 1>&2
+	./mdbci up ${configuration} 2>&1 | tee "${configuration}_log_mdbci_${i}"
 	exit_code="$?"
-	echo "$exit_code ($i/$N)"
+	echo "$exit_code ($i/$N)" > "${configuration}_mdbci_${i}"
 	mdbci_fails=$(($mdbci_fails + $exit_code))
 	cd ${configuration}
+	vagrant status >> "../${configuration}_mdbci_${i}"
 	vagrant destroy -f
 	cd ..
 done
