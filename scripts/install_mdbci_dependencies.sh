8df0d400ea5a2104949732d9303bb94eb6a99626
galiaf95 <galiaf1995@gmail.com>
2016-10-25 23:41:19 +0300

    7668 added scripts

---
+++ scripts/install_mdbci_dependencies.sh
@@ -0,0 +1,40 @@
+ubuntu_codename=$(cat /etc/*release 2>/dev/null | grep "UBUNTU_CODENAME" | awk -F'=' '{print $2}')
+if [[ -z "$ubuntu_codename" ]]; then
+    ubuntu_codename=$(cat /etc/*release 2>/dev/null | grep "DISTRIB_CODENAME" | awk -F'=' '{print $2}')
+fi
+
+sudo apt-get install git build-essential -y
+
+# Vagrant and prerequisites
+sudo apt-get install ruby libxslt-dev libxml2-dev libvirt-dev zlib1g-dev -y
+sudo gem install ipaddress
+sudo gem install json-schema
+if [[ $(vagrant --version) != "Vagrant 1.8.1" ]]; then
+        wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
+        sudo dpkg -i vagrant_1.8.1_x86_64.deb
+        rm vagrant_1.8.1_x86_64.deb
+fi
+vagrant plugin install vagrant-vbguest
+vagrant plugin install vagrant-aws
+vagrant plugin install vagrant-libvirt
+vagrant plugin install vagrant-mutate
+vagrant plugin install vagrant-omnibus
+
+# Libvirt and tools(virsh, virt-clone)
+sudo apt install qemu-kvm libvirt-bin virtinst -y
+sudo adduser $USER libvirtd
+
+# Docker
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates -y
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-$ubuntu_codename main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+#sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install "docker-engine=1.11.0-0~$ubuntu_codename" -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+
+# After that user shoud logout and login
+# exec su $USER
---
+++ scripts/slave_setting/repo_setup.sh
@@ -0,0 +1,13 @@
+# Cloning repositories
+git clone git@github.com:OSLL/mdbci.git $HOME/mdbci
+git clone git@github.com:mariadb-corporation/mdbci-boxes.git $HOME/mdbci-boxes
+git clone git@github.com:mariadb-corporation/build-scripts-vagrant.git $HOME/build-scripts
+git clone git@github.com:mariadb-corporation/mdbci-repository-config.git $HOME/mdbci-repository-config
+
+# MDBCI boxes and keys linking
+ln -s $HOME/mdbci-boxes/BOXES $HOME/mdbci/BOXES
+ln -s $HOME/mdbci-boxes/KEYS $HOME/mdbci/KEYS
+
+# Credentials for AWS and PPC
+scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null vagrant@max-tst-01.mariadb.com:/home/vagrant/mdbci/aws-config.yml $HOME/mdbci
+scp -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null vagrant@max-tst-01.mariadb.com:/home/vagrant/mdbci/maxscale.pem $HOME/mdbci
