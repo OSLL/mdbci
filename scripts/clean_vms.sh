01ff04c120edc03d2b9c3ad03efdca473c4270ae
KILFaT <kinyaevilfat@gmail.com>
2016-11-17 10:36:50 +0300

    7727 modify script for one config and snapshots

--- scripts/clean_vms.sh
+++ scripts/clean_vms.sh
@@ -1,16 +1,26 @@
-#!/bin/bash
+# !/bin/bash
 
 # $1 is prefix of the machines to be deleted
-
+# $2
 if [ ! -z $1 ]; then
-
-    echo "Machine with prefixes ${1} will be cleaned"
-
+    only_one_config=0
+    if [[ (! -z $2) && ($2 == "-o")]]; then
+        let only_one_config=1
+        echo "First machine with prefix ${1} will be cleaned"
+    else
+        echo "Machine with prefixes ${1} will be cleaned"
+    fi
+#asd=(1 2 3 4 5)
     echo "Cleaning VirtuslBox machines"
+#    for i in "${asd[@]}"; do
     for i in $(VBoxManage list vms | grep ${1} | grep -o '"[^\"]*"' | tr -d '"'); do
         echo $i
         VBoxManage controlvm $i poweroff
         VBoxManage unregistervm $i -delete
+#        echo "$i"
+        if [ $only_one_config == 1 ]; then
+           break
+        fi
     done
 
     echo "Cleaning libvirt machines"
@@ -18,16 +28,28 @@ if [ ! -z $1 ]; then
       virsh shutdown $i
       virsh destroy $i
       virsh undefine $i
+      for k in $(virsh snapshot-list ${i} --tree); do
+          virsh snapshot-delete $i $k # i-domain; k-snapshot
+      done
+      if [ $only_one_config == 1 ]; then
+           break
+      fi
     done
 
     echo "Deleting libvirt machine's volumes"
     for i in $(virsh -q vol-list --pool default | grep $1 | awk '{print $1}'); do
       virsh vol-delete --pool default $i
+      if [ $only_one_config == 1 ]; then
+           break
+      fi
     done
 
     echo "Cleaning docker machines"
     for i in $(docker ps --all -f "name=${1}" --format "{{.Names}}"); do
       docker rm -fv $i
+      if [ $only_one_config == 1 ]; then
+           break
+      fi
     done
 
 else
