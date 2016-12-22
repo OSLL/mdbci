ffc367aea2133a5149fe91205b273c55ac52136e
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-12-22 17:24:10 +0300

    7857: wrong path in scripts/slave_setting/sshfs/check_resync_in_crone.sh

--- scripts/slave_setting/sshfs/check_resync_in_crone.sh
+++ scripts/slave_setting/sshfs/check_resync_in_crone.sh
@@ -4,7 +4,7 @@ script_name="resync_shared_dirs.sh"
 if ! crontab -l -u ${USER} | grep -q ${script_name}
 then
   echo "crontab is empty, adding line with ${script_name} call "
-  line="* * * * * $HOME/scripts/slave_setting/sshfs/${script_name}"
+  line="* * * * * $HOME/mdbci/scripts/slave_setting/sshfs/${script_name}"
   (crontab -u ${USER} -l; echo "$line" ) | crontab -u ${USER} -
   exit 0
 fi
