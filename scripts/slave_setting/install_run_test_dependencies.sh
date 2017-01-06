494db90092e5a669c513f8faa9b0793547101854
galiaf95 <galiaf1995@gmail.com>
2017-01-06 13:16:53 +0300

    added libmariadb-client-lgpl-dev for run test deps (libmariadbclient-dev is missing)

--- scripts/slave_setting/install_run_test_dependencies.sh
+++ scripts/slave_setting/install_run_test_dependencies.sh
@@ -2,7 +2,6 @@ sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
 sudo apt-get install libssl-dev \
-                     libmariadbclient-dev \
                      php5.5 \
                      perl \
                      coreutils \
@@ -11,4 +10,6 @@ sudo apt-get install libssl-dev \
                      openjdk-7-jdk \
                      python-pip \
                      cmake -y
+sudo apt-get install libmariadbclient-dev
+sudo apt-get install libmariadb-client-lgpl-dev
 sudo pip install JayDeBeApi
