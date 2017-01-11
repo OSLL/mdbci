c5ac3946b550dd1dbfa4a1e4f3666366ed083fa8
KILFaT <kinyaevilfat@gmail.com>
2017-01-12 01:26:45 +0300

    7691 add test with ntpdate

--- recipes/cookbooks/ntp/recipes/default.rb
+++ recipes/cookbooks/ntp/recipes/default.rb
@@ -21,4 +21,9 @@ system 'echo @@@ TEST DATE'
 system 'sudo date --set "12 Sep 2012 12:12:12"'
 system 'echo @@@ BEFORE `date`'
 system 'sudo sntp -s 0.europe.pool.ntp.org'
-system 'echo @@@ AFTER `date`'
+
+system 'echo @@@ AFTER_1 `date`'
+
+system 'sudo ntpdate 0.europe.pool.ntp.org'
+
+system 'echo @@@ AFTER_2 `date`'
