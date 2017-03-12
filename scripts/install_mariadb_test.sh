b253de157cc5f6eb5efe72738a8e0f2ff9d0ad0b
KILFaT <kinyaevlfat@gmail.com>
2017-03-12 14:09:44 +0300

    7926 add mariadb-test in separate script

---
+++ scripts/install_mariadb_test.sh
@@ -0,0 +1,19 @@
+version=`lsb_release -c`
+
+$ubuntu_codename=echo ${version#"Codename:"}
+
+if [[ "$ubuntu_codename" == "xenial" ]]; then
+        sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+elif [[ "$ubuntu_codename" == "trusty" ]]; then
+        sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+fi
+
+sudo add-apt-repository "deb [arch=amd64,i386,ppc64el] http://mirror.timeweb.ru/mariadb/repo/10.0/ubuntu $ubuntu_codename main"
+
+echo "deb [arch=amd64,i386] http://mirror.timeweb.ru/mariadb/repo/10.0/ubuntu $ubuntu_codename main" | sudo tee /etc/apt/sources.list.d/mariadb.list
+echo "deb-src http://mirror.timeweb.ru/mariadb/repo/10.0/ubuntu $ubuntu_codename main" | sudo tee --append /etc/apt/sources.list.d/mariadb.list
+
+sudo apt-get update
+sudo apt-get install software-properties-common  -y
+
+sudo apt-get install mariadb-test
