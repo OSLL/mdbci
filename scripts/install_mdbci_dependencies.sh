3b86c70046d38dcd65d21bfe4ce6a1fd691544f8
galiaf95 <galiaf1995@gmail.com>
2017-07-12 13:29:36 +0200

    #8340 added fixes and profile generation

--- scripts/install_mdbci_dependencies.sh
+++ scripts/install_mdbci_dependencies.sh
@@ -11,6 +11,11 @@ sudo apt-get install git build-essential -y
 
 # AWS CLI tool
 sudo pip install --upgrade awscli
+# Check AWS CLI credentials
+aws --profile mdbci iam get-user
+if [[ $? != 0 ]]; then
+  aws --profile mdbci configure
+fi
 # Create security group and update aws-config.yml file
 ./scripts/update_aws_config_security_group.sh hostname_$(date +%s)
 
--- scripts/update_aws_config_security_group.sh
+++ scripts/update_aws_config_security_group.sh
@@ -1,8 +1,14 @@
 group_name=$1
-aws --output json ec2 create-security-group --group-name ${group_name} --description 'MDBCI ${group_name} security group'
+aws  --profile mdbci --output json ec2 create-security-group --group-name ${group_name} --description "MDBCI ${group_name} security group"
 if [[ $? == 0 ]]; then
   sed -i "s|security_groups : \[\(.*\)\]|security_groups : [ 'default', '${group_name}' ]|g" aws-config.yml
   echo "Security group ${group_name} added to aws-config.yml!"
+  aws  --profile mdbci --output json ec2 authorize-security-group-ingress --group-name ${group_name} --ip-permissions '[{"PrefixListIds":[],"FromPort":0,"IpRanges":[{"CidrIp":"0.0.0.0/0"}],"ToPort":65535,"IpProtocol":"tcp","UserIdGroupPairs":[],"Ipv6Ranges":[]},{"PrefixListIds":[],"FromPort":0,"IpRanges":[{"CidrIp":"0.0.0.0/0"}],"ToPort":65535,"IpProtocol":"udp","UserIdGroupPairs":[],"Ipv6Ranges":[]},{"PrefixListIds":[],"FromPort":0,"IpRanges":[{"CidrIp":"0.0.0.0/0"}],"ToPort":-1,"IpProtocol":"icmp","UserIdGroupPairs":[],"Ipv6Ranges":[]}]'
+  if [[ $? == 0 ]]; then
+    echo "Security group ${group_name} configured!"
+  else
+    echo "Error occured while configuring security group ${group_name}!"
+  fi
 else
-  echo 'Error occured!'
+  echo echo "Error occured while creating security group ${group_name}!"
 fi
