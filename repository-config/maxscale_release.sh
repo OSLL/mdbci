71126d3203027bf6552b3e123cc385c497ee1e78
Ilfat Kinyaev <kinyaevilfat@gmail.com>
2017-09-29 19:05:32 +0300

    8706: add function for get releases by tag

--- repository-config/maxscale_release.sh
+++ repository-config/maxscale_release.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
+# $1 - url to release downloads
 # $2 - destanation
-# $1 - repo path
+# $3 - repo path
 
+get_release_versions () {
+  # $1 - repo path
+  # $2 - url to release downloads
+  repo=$3
+  base_url=$1
+  tags=`git ls-remote --tags $repo | grep -o -P '(?<=tags\/maxscale-).*(?=\^)'`
+  release_versions=()
+  for tag in $tags
+  do
+  	url="$base_url$tag/"
+  	if curl --output /dev/null --silent --head --fail "$url"; then
+      release_versions+=($tag)
+      url="$base_url$tag-debug/"
+      	if curl --output /dev/null --silent --head --fail "$url"; then
+          release_versions+=("$tag-debug")
+        fi
+    fi
+    tag=${tag::${#tag}-2}
+    url="$base_url$tag/"
+    if curl --output /dev/null --silent --head --fail "$url"; then
+      release_versions+=($tag)
+      url="$base_url$tag-debug/"
+      	if curl --output /dev/null --silent --head --fail "$url"; then
+          release_versions+=("$tag-debug")
+        fi
+    fi
+  done
+  echo ${release_versions[@]}
+}
+
+downloads_url=$1
 dest=$2
-repo=$1
+repo=$3
 
 c_dir=`pwd`
 
@@ -29,7 +61,7 @@ mkdir -p $dest/maxscale-release-templates-versions
 
 for f in $list
 do
-    sed "s|###repo_prefix###|$repo|g" $c_dir/$dest/maxscale-release-templates/$f  > $dest/maxscale-release-templates-versions/$f
+    sed "s|###repo_prefix###|$downloads_url|g" $c_dir/$dest/maxscale-release-templates/$f  > $dest/maxscale-release-templates-versions/$f
 done
 
 rm -rf $c_dir/$dest/maxscale-release-templates
@@ -39,11 +71,11 @@ list=`ls -1 *.json`
 echo $list
 cd $c_dir
 
-vers=`cat ~/mdbci/repository-config/maxscale_release/maxscale.version`
+vers=$(get_release_versions $repo $downloads_url)
 
 for f in $list
 do
-    for ver in $vers
+    for ver in ${vers[@]}
     do
 	echo $f $ver
         sed "s|###version###|$ver|g" $dest/maxscale-release-templates-versions/$f  > $dest/maxscale-release/${ver}_${f}
