b16ec1cb7397d0eab76717b0f24fbaed6c62dd9e
Andrey Vasilyev <andrey@crafted.su>
2017-11-17 17:50:41 +0300

    Specify shell interpeter, use script location as a basis for constructing relative links, add a bunch of comments to the script, introduce warnOnError function to execute commands that may fail during installation (refs #23371)

--- scripts/install_mdbci_dependencies.sh
+++ scripts/install_mdbci_dependencies.sh
@@ -1,5 +1,13 @@
-# Install MDBCI dependencies
+#!/bin/bash
+# This script installs MDBCI dependencies and sets them up
+# as required by the tool.
 
+# Get the location of the script and go into that directory.
+script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+cd $script_dir
+
+# Function executes the passed command. If it is not successfull, then
+# function shows the coman that have failed and exits from the script.
 function checkStatus {
   "$@"
   local status=$?
@@ -11,56 +19,72 @@ function checkStatus {
   return $status
 }
 
+# Function executes the passed command. If it is not successfull, then
+# function shows that the command was not fully executed.
+function warnOnError {
+  "$@"
+  local status=$?
+  if [ $status -ne 0 ]; then
+    echo "[ISTALLATION WARNING]: $@"
+  fi
+  return $status
+}
+
+# Try to find out the code name of the installed ubuntu release
 ubuntu_codename=$(cat /etc/*release 2>/dev/null | grep "UBUNTU_CODENAME" | awk -F'=' '{print $2}')
 if [[ -z "$ubuntu_codename" ]]; then
     ubuntu_codename=$(cat /etc/*release 2>/dev/null | grep "DISTRIB_CODENAME" | awk -F'=' '{print $2}')
 fi
 
+# Update the packages of the system and install git and build-essential packages
 checkStatus sudo apt-get update
+checkStatus sudo apt-get install -y git build-essential
 
-checkStatus sudo apt-get install git build-essential -y
+# Install AWS, configure user and security group.
+checkStatus ./setup_aws.sh
 
-checkStatus ./scripts/setup_aws.sh
-
-# Vagrant and prerequisites
-checkStatus sudo apt-get install ruby libxslt-dev \
-                                      libxml2-dev \
-                                      libvirt-dev \
-                                      zlib1g-dev -y
+# Install dependencies required for mdbci and Vagrant to run
+checkStatus sudo apt-get install -y ruby \
+                                    libxslt-dev \
+                                    libxml2-dev \
+                                    libvirt-dev \
+                                    zlib1g-dev
+# Install ruby gems used by the mdbci
 checkStatus sudo gem install ipaddress
 checkStatus sudo gem install json-schema -v 2.6.2
+# Install the Vagrant and plugins used by the mdbci
 if [[ $(vagrant --version) != "Vagrant 1.8.1" ]]; then
         checkStatus wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
         checkStatus sudo dpkg -i vagrant_1.8.1_x86_64.deb
         checkStatus rm vagrant_1.8.1_x86_64.deb
 fi
-
 checkStatus vagrant plugin install vagrant-aws --plugin-version 0.7.2
 checkStatus vagrant plugin install vagrant-libvirt --plugin-version 0.0.33
 checkStatus vagrant plugin install vagrant-mutate --plugin-version 1.2.0
 checkStatus vagrant plugin install vagrant-omnibus --plugin-version 1.5.0
 checkStatus vagrant box add --force dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
 
-# Libvirt and tools(virsh, virt-clone)
-checkStatus sudo apt install qemu-kvm \
-                             libvirt-bin \
-                             virtinst -y
+# Install libvirt and tools to controll it including virsh, virt-clone
+checkStatus sudo apt install -y qemu-kvm \
+                                libvirt-bin \
+                                virtinst
+# Allow user to manage libvirt daemon
 checkStatus sudo adduser $USER libvirtd
 
-# Configuration of libvirt images path
-checkStatus sudo virsh pool-destroy default
-checkStatus sudo virsh pool-undefine default
-checkStatus mkdir -p $HOME/libvirt-images
-checkStatus cp ./scripts/slave_setting/libvirt/default.xml ./scripts/slave_setting/libvirt/default_tmp.xml
-checkStatus sed -i "s|#REPLACE_ME#|$HOME/libvirt-images|g" ./scripts/slave_setting/libvirt/default_tmp.xml
-checkStatus sudo virsh pool-create ./scripts/slave_setting/libvirt/default_tmp.xml
-checkStatus sudo virsh pool-dumpxml --pool default > ./scripts/slave_setting/libvirt/default_tmp.xml
-checkStatus sudo virsh pool-define ./scripts/slave_setting/libvirt/default_tmp.xml
+# Configure libvirt daemon images path
+warnOnError sudo virsh pool-destroy default
+warnOnError sudo virsh pool-undefine default
+libvirt_images_directory="$HOME/libvirt-images"
+checkStatus mkdir -p $libvirt_images_directory
+checkStatus cp slave_setting/libvirt/default.xml slave_setting/libvirt/default_tmp.xml
+checkStatus sed -i "s|#REPLACE_ME#|$libvirt_images_directory|g" slave_setting/libvirt/default_tmp.xml
+checkStatus sudo virsh pool-create slave_setting/libvirt/default_tmp.xml
+checkStatus sudo virsh pool-dumpxml --pool default > slave_setting/libvirt/default_tmp.xml
+checkStatus sudo virsh pool-define slave_setting/libvirt/default_tmp.xml
 checkStatus sudo virsh pool-autostart default
-checkStatus rm ./scripts/slave_setting/libvirt/default_tmp.xml
+checkStatus rm slave_setting/libvirt/default_tmp.xml
 
-
-# Docker
+# Install and setup docker
 checkStatus sudo apt-get update
 checkStatus sudo apt-get install apt-transport-https \
                                  ca-certificates -y
