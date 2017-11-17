56b03905a927fb45a5f4bab27bacb1123a25262f
Andrey Vasilyev <andrey@crafted.su>
2017-11-17 17:52:27 +0300

    Rename function checkStatus into failOnError (refs #23371)

--- scripts/install_mdbci_dependencies.sh
+++ scripts/install_mdbci_dependencies.sh
@@ -37,64 +37,64 @@ if [[ -z "$ubuntu_codename" ]]; then
 fi
 
 # Update the packages of the system and install git and build-essential packages
-checkStatus sudo apt-get update
-checkStatus sudo apt-get install -y git build-essential
+failOnError sudo apt-get update
+failOnError sudo apt-get install -y git build-essential
 
 # Install AWS, configure user and security group.
-checkStatus ./setup_aws.sh
+failOnError ./setup_aws.sh
 
 # Install dependencies required for mdbci and Vagrant to run
-checkStatus sudo apt-get install -y ruby \
+failOnError sudo apt-get install -y ruby \
                                     libxslt-dev \
                                     libxml2-dev \
                                     libvirt-dev \
                                     zlib1g-dev
 # Install ruby gems used by the mdbci
-checkStatus sudo gem install ipaddress
-checkStatus sudo gem install json-schema -v 2.6.2
+failOnError sudo gem install ipaddress
+failOnError sudo gem install json-schema -v 2.6.2
 # Install the Vagrant and plugins used by the mdbci
 if [[ $(vagrant --version) != "Vagrant 1.8.1" ]]; then
-        checkStatus wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
-        checkStatus sudo dpkg -i vagrant_1.8.1_x86_64.deb
-        checkStatus rm vagrant_1.8.1_x86_64.deb
+        failOnError wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
+        failOnError sudo dpkg -i vagrant_1.8.1_x86_64.deb
+        failOnError rm vagrant_1.8.1_x86_64.deb
 fi
-checkStatus vagrant plugin install vagrant-aws --plugin-version 0.7.2
-checkStatus vagrant plugin install vagrant-libvirt --plugin-version 0.0.33
-checkStatus vagrant plugin install vagrant-mutate --plugin-version 1.2.0
-checkStatus vagrant plugin install vagrant-omnibus --plugin-version 1.5.0
-checkStatus vagrant box add --force dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
+failOnError vagrant plugin install vagrant-aws --plugin-version 0.7.2
+failOnError vagrant plugin install vagrant-libvirt --plugin-version 0.0.33
+failOnError vagrant plugin install vagrant-mutate --plugin-version 1.2.0
+failOnError vagrant plugin install vagrant-omnibus --plugin-version 1.5.0
+failOnError vagrant box add --force dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
 
 # Install libvirt and tools to controll it including virsh, virt-clone
-checkStatus sudo apt install -y qemu-kvm \
+failOnError sudo apt install -y qemu-kvm \
                                 libvirt-bin \
                                 virtinst
 # Allow user to manage libvirt daemon
-checkStatus sudo adduser $USER libvirtd
+failOnError sudo adduser $USER libvirtd
 
 # Configure libvirt daemon images path
 warnOnError sudo virsh pool-destroy default
 warnOnError sudo virsh pool-undefine default
 libvirt_images_directory="$HOME/libvirt-images"
-checkStatus mkdir -p $libvirt_images_directory
-checkStatus cp slave_setting/libvirt/default.xml slave_setting/libvirt/default_tmp.xml
-checkStatus sed -i "s|#REPLACE_ME#|$libvirt_images_directory|g" slave_setting/libvirt/default_tmp.xml
-checkStatus sudo virsh pool-create slave_setting/libvirt/default_tmp.xml
-checkStatus sudo virsh pool-dumpxml --pool default > slave_setting/libvirt/default_tmp.xml
-checkStatus sudo virsh pool-define slave_setting/libvirt/default_tmp.xml
-checkStatus sudo virsh pool-autostart default
-checkStatus rm slave_setting/libvirt/default_tmp.xml
+failOnError mkdir -p $libvirt_images_directory
+failOnError cp slave_setting/libvirt/default.xml slave_setting/libvirt/default_tmp.xml
+failOnError sed -i "s|#REPLACE_ME#|$libvirt_images_directory|g" slave_setting/libvirt/default_tmp.xml
+failOnError sudo virsh pool-create slave_setting/libvirt/default_tmp.xml
+failOnError sudo virsh pool-dumpxml --pool default > slave_setting/libvirt/default_tmp.xml
+failOnError sudo virsh pool-define slave_setting/libvirt/default_tmp.xml
+failOnError sudo virsh pool-autostart default
+failOnError rm slave_setting/libvirt/default_tmp.xml
 
 # Install and setup docker
-checkStatus sudo apt-get update
-checkStatus sudo apt-get install apt-transport-https \
+failOnError sudo apt-get update
+failOnError sudo apt-get install apt-transport-https \
                                  ca-certificates -y
-checkStatus sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-checkStatus echo "deb https://apt.dockerproject.org/repo ubuntu-$ubuntu_codename main" | sudo tee /etc/apt/sources.list.d/docker.list
-checkStatus sudo apt-get update
+failOnError sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+failOnError echo "deb https://apt.dockerproject.org/repo ubuntu-$ubuntu_codename main" | sudo tee /etc/apt/sources.list.d/docker.list
+failOnError sudo apt-get update
 #sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
-checkStatus sudo apt-get install "docker-engine=1.11.0-0~$ubuntu_codename" -y --allow-downgrades
-checkStatus sudo groupadd -f docker
-checkStatus sudo usermod -aG docker $USER
+failOnError sudo apt-get install "docker-engine=1.11.0-0~$ubuntu_codename" -y --allow-downgrades
+failOnError sudo groupadd -f docker
+failOnError sudo usermod -aG docker $USER
 
 
 # After that user shoud logout and login
