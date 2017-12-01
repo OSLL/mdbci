45d833ede6630a636f499925b7c689a0ab83180d
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2017-12-01 17:29:42 +0300

    Remove else clause that covered the whole body of the up command

--- core/commands/up_command.rb
+++ core/commands/up_command.rb
@@ -91,98 +91,137 @@ class UpCommand < BaseCommand
     if @nodesProvider == 'mdbci'
       @ui.warning 'You are using mdbci nodes template. ./mdbci up command doesn\'t supported for this boxes!'
       return ERROR_RESULT
-    else
-      # Generating docker images (so it will not be loaded for similar nodes repeatedly)
-      generateDockerImages(template, '.') if @nodesProvider == 'docker'
+    end
 
-      no_parallel_flag = ''
-      if (@nodesProvider == 'aws') || (@nodesProvider == 'docker')
-        no_parallel_flag = " #{VAGRANT_NO_PARALLEL} "
-      end
+    # Generating docker images (so it will not be loaded for similar nodes repeatedly)
+    generateDockerImages(template, '.') if @nodesProvider == 'docker'
 
-      @ui.info "Bringing up #{(node.empty? ? 'configuration ' : 'node ')} #{@configuration}"
+    no_parallel_flag = ''
+    if (@nodesProvider == 'aws') || (@nodesProvider == 'docker')
+      no_parallel_flag = " #{VAGRANT_NO_PARALLEL} "
+    end
 
-      @ui.info 'Destroying everything'
-      exec_cmd_destr = `vagrant destroy --force #{node}`
-      @ui.info exec_cmd_destr
+    @ui.info "Bringing up #{(node.empty? ? 'configuration ' : 'node ')} #{@configuration}"
 
-      cmd_up = "vagrant up #{no_parallel_flag} --provider=#{@nodesProvider} #{node}"
-      @ui.info "Actual command: #{cmd_up}"
+    @ui.info 'Destroying everything'
+    exec_cmd_destr = `vagrant destroy --force #{node}`
+    @ui.info exec_cmd_destr
+
+    cmd_up = "vagrant up #{no_parallel_flag} --provider=#{@nodesProvider} #{node}"
+    @ui.info "Actual command: #{cmd_up}"
+    chef_not_found_node = nil
+    status = nil
+    begin
       chef_not_found_node = nil
-      status = nil
-      begin
-        chef_not_found_node = nil
-        status = Open3.popen3(cmd_up) do |stdin, stdout, stderr, wthr|
+      status = Open3.popen3(cmd_up) do |stdin, stdout, stderr, wthr|
+        stdin.close
+        stdout.each_line do |line|
+          @ui.info line
+          chef_not_found_node = line if @nodesProvider == 'aws'
+        end
+        stdout.close
+        error = stderr.read
+        stderr.close
+        if (@nodesProvider == 'aws') && error.to_s.include?(CHEF_NOT_FOUND_ERROR)
+          chef_not_found_node = chef_not_found_node.to_s.match(OUTPUT_NODE_NAME_REGEX).captures[0]
+        else
+          error.each_line { |line| @ui.error line }
+          chef_not_found_node = nil
+        end
+        wthr.value
+      end
+      if chef_not_found_node
+        @ui.warning "Chef not is found on aws node: #{chef_not_found_node}, applying quick fix..."
+        cmd_provision = "vagrant provision #{chef_not_found_node}"
+        status = Open3.popen3(cmd_provision) do |stdin, stdout, stderr, wthr|
           stdin.close
-          stdout.each_line do |line|
-            @ui.info line
-            chef_not_found_node = line if @nodesProvider == 'aws'
-          end
+          stdout.each_line { |line| @ui.info line }
           stdout.close
-          error = stderr.read
+          stderr.each_line { |line| @ui.error line }
           stderr.close
-          if (@nodesProvider == 'aws') && error.to_s.include?(CHEF_NOT_FOUND_ERROR)
-            chef_not_found_node = chef_not_found_node.to_s.match(OUTPUT_NODE_NAME_REGEX).captures[0]
-          else
-            error.each_line { |line| @ui.error line }
-            chef_not_found_node = nil
-          end
           wthr.value
         end
-        if chef_not_found_node
-          @ui.warning "Chef not is found on aws node: #{chef_not_found_node}, applying quick fix..."
-          cmd_provision = "vagrant provision #{chef_not_found_node}"
-          status = Open3.popen3(cmd_provision) do |stdin, stdout, stderr, wthr|
-            stdin.close
+      end
+    end while !chef_not_found_node.nil?
+    unless status.success?
+      @ui.error 'Bringing up failed'
+      exit_code = status.exitstatus
+      @ui.error "exit code #{exit_code}"
+
+      dead_machines = []
+      machines_with_broken_chef = []
+
+      vagrant_status = `vagrant status`.split("\n\n")[1].split("\n")
+      nodes = []
+      vagrant_status.each { |stat| nodes.push(stat.split(/\s+/)[0]) }
+
+      @ui.warning 'Checking for dead machines and checking Chef runs on machines'
+      nodes.each do |machine_name|
+        status = `vagrant status #{machine_name}`.split("\n")[2]
+        @ui.info status
+        unless status.include? 'running'
+          dead_machines.push(machine_name)
+          next
+        end
+
+        chef_log_cmd = "vagrant ssh #{machine_name} -c \"test -e /var/chef/cache/chef-stacktrace.out && printf 'FOUND' || printf 'NOT_FOUND'\""
+        chef_log_out = `#{chef_log_cmd}`
+        machines_with_broken_chef.push machine_name if chef_log_out == 'FOUND'
+      end
+
+      unless dead_machines.empty?
+        @ui.error 'Some machines are dead:'
+        dead_machines.each { |machine| @ui.error "\t#{machine}" }
+      end
+
+      unless machines_with_broken_chef.empty?
+        @ui.error 'Some machines have broken Chef run:'
+        machines_with_broken_chef.each { |machine| @ui.error "\t#{machine}" }
+      end
+
+      unless dead_machines.empty?
+        (1..@attempts).each do |i|
+          @ui.info 'Trying to force restart broken machines'
+          @ui.info "Attempt: #{i}"
+          dead_machines.delete_if do |machine|
+            puts `vagrant destroy -f #{machine}`
+            cmd_up = "vagrant up #{no_parallel_flag} --provider=#{@nodesProvider} #{machine}"
+            success = Open3.popen3(cmd_up) do |_stdin, stdout, stderr, wthr|
+              stdout.each_line { |line| @ui.info line }
+              stderr.each_line { |line| @ui.error line }
+              wthr.value.success?
+            end
+            success
+          end
+          if !dead_machines.empty?
+            @ui.error 'Some machines are still dead:'
+            dead_machines.each { |machine| @ui.error "\t#{machine}" }
+          else
+            @ui.info 'All dead machines successfuly resurrected'
+            break
+          end
+        end
+        raise 'Bringing up failed (error description is above)' unless dead_machines.empty?
+      end
+
+      unless machines_with_broken_chef.empty?
+        @ui.info 'Trying to re-provision machines'
+        machines_with_broken_chef.delete_if do |machine|
+          cmd_up = "vagrant provision #{machine}"
+          success = Open3.popen3(cmd_up) do |_stdin, stdout, stderr, wthr|
             stdout.each_line { |line| @ui.info line }
-            stdout.close
             stderr.each_line { |line| @ui.error line }
-            stderr.close
-            wthr.value
+            wthr.value.success?
           end
+          success
         end
-      end while !chef_not_found_node.nil?
-      unless status.success?
-        @ui.error 'Bringing up failed'
-        exit_code = status.exitstatus
-        @ui.error "exit code #{exit_code}"
-
-        dead_machines = []
-        machines_with_broken_chef = []
-
-        vagrant_status = `vagrant status`.split("\n\n")[1].split("\n")
-        nodes = []
-        vagrant_status.each { |stat| nodes.push(stat.split(/\s+/)[0]) }
-
-        @ui.warning 'Checking for dead machines and checking Chef runs on machines'
-        nodes.each do |machine_name|
-          status = `vagrant status #{machine_name}`.split("\n")[2]
-          @ui.info status
-          unless status.include? 'running'
-            dead_machines.push(machine_name)
-            next
-          end
-
-          chef_log_cmd = "vagrant ssh #{machine_name} -c \"test -e /var/chef/cache/chef-stacktrace.out && printf 'FOUND' || printf 'NOT_FOUND'\""
-          chef_log_out = `#{chef_log_cmd}`
-          machines_with_broken_chef.push machine_name if chef_log_out == 'FOUND'
-        end
-
-        unless dead_machines.empty?
-          @ui.error 'Some machines are dead:'
-          dead_machines.each { |machine| @ui.error "\t#{machine}" }
-        end
-
         unless machines_with_broken_chef.empty?
-          @ui.error 'Some machines have broken Chef run:'
+          @ui.error 'Some machines are still have broken Chef run:'
           machines_with_broken_chef.each { |machine| @ui.error "\t#{machine}" }
-        end
-
-        unless dead_machines.empty?
           (1..@attempts).each do |i|
-            @ui.info 'Trying to force restart broken machines'
+            @ui.info 'Trying to force restart machines'
             @ui.info "Attempt: #{i}"
-            dead_machines.delete_if do |machine|
+            machines_with_broken_chef.delete_if do |machine|
               puts `vagrant destroy -f #{machine}`
               cmd_up = "vagrant up #{no_parallel_flag} --provider=#{@nodesProvider} #{machine}"
               success = Open3.popen3(cmd_up) do |_stdin, stdout, stderr, wthr|
@@ -192,57 +231,19 @@ class UpCommand < BaseCommand
               end
               success
             end
-            if !dead_machines.empty?
-              @ui.error 'Some machines are still dead:'
-              dead_machines.each { |machine| @ui.error "\t#{machine}" }
+            if !machines_with_broken_chef.empty?
+              @ui.error 'Some machines are still have broken Chef run:'
+              machines_with_broken_chef.each { |machine| @ui.error "\t#{machine}" }
             else
-              @ui.info 'All dead machines successfuly resurrected'
+              @ui.info 'All broken_chef machines successfuly reprovisioned.'
               break
             end
           end
-          raise 'Bringing up failed (error description is above)' unless dead_machines.empty?
-        end
-
-        unless machines_with_broken_chef.empty?
-          @ui.info 'Trying to re-provision machines'
-          machines_with_broken_chef.delete_if do |machine|
-            cmd_up = "vagrant provision #{machine}"
-            success = Open3.popen3(cmd_up) do |_stdin, stdout, stderr, wthr|
-              stdout.each_line { |line| @ui.info line }
-              stderr.each_line { |line| @ui.error line }
-              wthr.value.success?
-            end
-            success
-          end
-          unless machines_with_broken_chef.empty?
-            @ui.error 'Some machines are still have broken Chef run:'
-            machines_with_broken_chef.each { |machine| @ui.error "\t#{machine}" }
-            (1..@attempts).each do |i|
-              @ui.info 'Trying to force restart machines'
-              @ui.info "Attempt: #{i}"
-              machines_with_broken_chef.delete_if do |machine|
-                puts `vagrant destroy -f #{machine}`
-                cmd_up = "vagrant up #{no_parallel_flag} --provider=#{@nodesProvider} #{machine}"
-                success = Open3.popen3(cmd_up) do |_stdin, stdout, stderr, wthr|
-                  stdout.each_line { |line| @ui.info line }
-                  stderr.each_line { |line| @ui.error line }
-                  wthr.value.success?
-                end
-                success
-              end
-              if !machines_with_broken_chef.empty?
-                @ui.error 'Some machines are still have broken Chef run:'
-                machines_with_broken_chef.each { |machine| @ui.error "\t#{machine}" }
-              else
-                @ui.info 'All broken_chef machines successfuly reprovisioned.'
-                break
-              end
-            end
-            raise 'Bringing up failed (error description is above)' unless machines_with_broken_chef.empty?
-          end
+          raise 'Bringing up failed (error description is above)' unless machines_with_broken_chef.empty?
         end
       end
     end
+
     @ui.info 'All nodes successfully up!'
     @ui.info "DIR_PWD=#{pwd}"
     @ui.info "CONF_PATH=#{config_path}"
