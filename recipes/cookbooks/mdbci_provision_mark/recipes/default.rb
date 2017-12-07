27179514d639297d902d8e21ba1e05ba797bc8a7
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2017-12-07 09:52:33 +0300

    Create /var/mdbci/provisioned file during the provision of the VM (refs #23392)

--- recipes/cookbooks/mdbci_provision_mark/recipes/default.rb
+++ recipes/cookbooks/mdbci_provision_mark/recipes/default.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
+
 #
 # Cookbook:: mdbci_provision_mark
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Create a directory for mdbci files
+directory '/var/mdbci' do
+  mode '0755'
+  owner 'root'
+  group 'root'
+end
+
+# Put the mark that machine has been provisioned
+template '/var/mdbci/provisioned' do
+  source 'provisioned.erb'
+  mode '0444'
+  owner 'root'
+  group 'root'
+  variables provisioned_time: node['ohai_time']
+end
