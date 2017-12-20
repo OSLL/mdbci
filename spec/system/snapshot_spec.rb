86e5ae6d53a7cb1df0d3d5fecb27a85dd31a7e1e
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2017-12-20 17:55:24 +0300

    Add a check for mdbci snapshot revert for destroyed configurations (refs #23455)

--- spec/system/snapshot_spec.rb
+++ spec/system/snapshot_spec.rb
@@ -35,5 +35,16 @@ describe 'snaphot command', :system do
         expect(mdbci_run_command("snapshot revert --path-to-nodes #{config} --snapshot-name test")).not_to be_success
       end
     end
+
+    context 'when trying to revert destroyed configuration' do
+      it 'should return an error code' do
+        config = mdbci_create_configuration(@test_dir, 'centos_7_libvirt_plain')
+        @configs_to_destroy.push(config)
+        run_command_in_dir('vagrant up', config)
+        mdbci_check_command("snapshot take --path-to-nodes #{config} --snapshot-name test")
+        run_command_in_dir('vagrant destroy -f', config)
+        expect(mdbci_run_command("snapshot revert --path-to-nodes #{config} --snapshot-name test")).not_to be_success
+      end
+    end
   end
 end
