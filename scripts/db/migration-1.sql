af971233c1bea766a670887b8422189975e7d8ca
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2018-01-25 22:38:13 +0300

    Rename migration files, update the final schema of the database, create the db_version table in the migration-1 (refs #23610)

--- scripts/db/migration-1.sql
+++ scripts/db/migration-1.sql
@@ -2,3 +2,9 @@ ALTER TABLE test_run ADD cmake_flags TEXT DEFAULT "NOT FOUND", ADD maxscale_sour
 
 UPDATE test_run SET cmake_flags="NOT FOUND" WHERE cmake_flags IS NULL;
 UPDATE test_run SET maxscale_source="NOT FOUND" WHERE maxscale_source IS NULL;
+
+CREATE TABLE IF NOT EXISTS db_version (
+  version INT
+);
+TRUNCATE TABLE db_version;
+INSERT INTO db_version (version) VALUES (1);
