30781e75e3711b305c3950ba22cad83f131788c1
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2018-04-10 17:51:52 +0300

    Unify repository parsing for RHEL-based and Debian-based distributives (refs #23897)

--- scripts/repository_generator/parse_columnstore_repo.rb
+++ scripts/repository_generator/parse_columnstore_repo.rb
@@ -19,6 +19,13 @@ class ColumnstoreRepoParser
     puts "Creating configuration in #{@directory}"
   end
 
+  # Get links on the specified page
+  def get_links(path = '/')
+    uri = "#{REPO_PAGE}/#{path}"
+    doc = Nokogiri::HTML(open(uri))
+    doc.css('#main a')
+  end
+
   # This method goes throug the main page and finds releases that sholud be added to
   # the repository
   def find_vaiable_releases
@@ -33,43 +40,33 @@ class ColumnstoreRepoParser
     end
   end
 
-  # Get links on the specified page
-  def get_links(path = '/')
-    uri = "#{REPO_PAGE}/#{path}"
-    doc = Nokogiri::HTML(open(uri))
-    doc.css('#main a')
-  end
-
-  # Create repository for debian-based distribive
-  # @param release [String] name of the release
-  # @param system [String] name of the repository
-  def create_debian_repo(release, system)
-    repos = find_repos(release, 'repo', system).each_with_object([]) do |link, repositories|
-      repo_link = "#{release}/repo/#{link[:href]}".gsub(/\/\//, '/')
-      get_links("#{repo_link}/dists").each do |release_link|
+  SYSTEMS = {
+    debian: {
+      path: 'repo',
+      key: DEBIAN_REPO_KEY,
+      release_path: -> (repo_link) { "#{repo_link}/dists" },
+      repo_path: -> (repo_link, release_name) { "#{REPO_PAGE}#{repo_link} #{release_name} main" }
+    },
+    rhel: {
+      path: 'yum',
+      key: RHEL_REPO_KEY,
+      release_path: -> (repo_link) { repo_link },
+      repo_path: -> (repo_link, release_name) { "#{REPO_PAGE}#{repo_link}/#{release_name}/x86_64" }
+    }
+  }
+  def create_repo(release, system, type)
+    system_type = SYSTEMS[type]
+    subpath = system_type[:path]
+    repos = get_links("#{release}/#{subpath}").select do |link|
+      link.content.include?(system)
+    end.each_with_object([]) do |link, repositories|
+      repo_link = "#{release}/#{subpath}/#{link[:href]}".gsub(/\/\//, '/')
+      get_links(system_type[:release_path].call(repo_link)).each do |release_link|
         release_name = release_link.content.delete('/')
+        repo_path = system_type[:repo_path].call(repo_link, release_name)
         repositories << {
-          repo: "#{REPO_PAGE}#{repo_link} #{release_name} main",
-          repo_key: DEBIAN_REPO_KEY,
-          platform_version: release_name,
-          platform: system,
-          product: 'columnstore',
-          version: release,
-        }
-      end
-    end
-    FileUtils.mkdir_p("#{@directory}/#{system}/")
-    File.write("#{directory}/#{system}/#{release}.json", JSON.pretty_generate(repos))
-  end
-
-  def create_rhel_repo(release, system)
-    repos = find_repos(release, 'yum', system).each_with_object([]) do |link, repositories|
-      repo_link = "#{release}/yum/#{link[:href]}".gsub(/\/\//, '/')
-      get_links("#{repo_link}").each do |release_link|
-        release_name = release_link.content.delete('/')
-        repositories << {
-          repo: "#{REPO_PAGE}#{repo_link}/#{release_name}/x86_64",
-          repo_key: RHEL_REPO_KEY,
+          repo: repo_path,
+          repo_key: system_type[:key],
           platform_version: release_name,
           platform: system,
           product: 'columnstore',
@@ -81,19 +78,13 @@ class ColumnstoreRepoParser
     File.write("#{directory}/#{system}/#{release}.json", JSON.pretty_generate(repos))
   end
 
-  # Finds repositories by the type
-  def find_repos(release, type, system)
-    get_links("#{release}/#{type}").select do |link|
-      link.content.include?(system)
-    end
-  end
-
   def parse
     find_vaiable_releases.each do |release|
       puts "Configuring release #{release}"
-      create_rhel_repo(release, 'centos')
-#      create_debian_repo(release, 'debian')
-#      create_debian_repo(release, 'ubuntu')
+      create_repo(release, 'centos', :rhel)
+      create_repo(release, 'sles', :rhel)
+      create_repo(release, 'debian', :debian)
+      create_repo(release, 'ubuntu', :debian)
     end
   end
 end
