c4faf0471df9f32dfcbe0c9016a9e0449d555ee4
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2018-04-10 17:21:03 +0300

    Add method to extract rhel-based information from repository

--- scripts/repository_generator/parse_columnstore_repo.rb
+++ scripts/repository_generator/parse_columnstore_repo.rb
@@ -12,6 +12,7 @@ class ColumnstoreRepoParser
   attr_reader :directory
   REPO_PAGE = 'https://downloads.mariadb.com/ColumnStore/'
   DEBIAN_REPO_KEY = 'AD0DEAFDA41F5C14'
+  RHEL_REPO_KEY = "#{REPO_PAGE}MariaDB-ColumnStore.gpg.key"
 
   def initialize
     @directory = Dir.mktmpdir
@@ -43,10 +44,7 @@ class ColumnstoreRepoParser
   # @param release [String] name of the release
   # @param system [String] name of the repository
   def create_debian_repo(release, system)
-    FileUtils.mkdir_p("#{@directory}/#{system}/")
-    repos = get_links("#{release}/repo").select do |link|
-      link.content.include?(system)
-    end.each_with_object([]) do |link, repositories|
+    repos = find_repos(release, 'repo', system).each_with_object([]) do |link, repositories|
       repo_link = "#{release}/repo/#{link[:href]}".gsub(/\/\//, '/')
       get_links("#{repo_link}/dists").each do |release_link|
         release_name = release_link.content.delete('/')
@@ -60,16 +58,42 @@ class ColumnstoreRepoParser
         }
       end
     end
+    FileUtils.mkdir_p("#{@directory}/#{system}/")
     File.write("#{directory}/#{system}/#{release}.json", JSON.pretty_generate(repos))
   end
 
+  def create_rhel_repo(release, system)
+    repos = find_repos(release, 'yum', system).each_with_object([]) do |link, repositories|
+      repo_link = "#{release}/yum/#{link[:href]}".gsub(/\/\//, '/')
+      get_links("#{repo_link}").each do |release_link|
+        release_name = release_link.content.delete('/')
+        repositories << {
+          repo: "#{REPO_PAGE}#{repo_link}/#{release_name}/x86_64",
+          repo_key: RHEL_REPO_KEY,
+          platform_version: release_name,
+          platform: system,
+          product: 'columnstore',
+          version: release
+        }
+      end
+    end
+    FileUtils.mkdir_p("#{directory}/#{system}")
+    File.write("#{directory}/#{system}/#{release}.json", JSON.pretty_generate(repos))
+  end
 
+  # Finds repositories by the type
+  def find_repos(release, type, system)
+    get_links("#{release}/#{type}").select do |link|
+      link.content.include?(system)
+    end
+  end
 
   def parse
     find_vaiable_releases.each do |release|
       puts "Configuring release #{release}"
-      create_debian_repo(release, 'debian')
-      create_debian_repo(release, 'ubuntu')
+      create_rhel_repo(release, 'centos')
+#      create_debian_repo(release, 'debian')
+#      create_debian_repo(release, 'ubuntu')
     end
   end
 end
