f155fa1f3196f603c9c226a2fd034b97cb4666de
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2018-05-11 01:46:52 +0300

    Automate creation of repo.d directory for MDBCI (refs #23932)

---
+++ scripts/repository_generator/generate_repo_d.rb
@@ -0,0 +1,506 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+require 'nokogiri'
+require 'pp'
+require 'tmpdir'
+require 'json'
+require 'fileutils'
+
+require_relative 'config'
+
+class GenerateRepoD
+  attr_reader :directory
+  DEFAULT_PRODUCTS = %w[columnstore community galera maxscale_ci mdbe mysql]
+  PRODUCTS_DIR_NAMES = {
+    'columnstore': 'columnstore',
+    'community': 'community',
+    'galera': 'galera',
+    'maxscale_ci': 'maxscale',
+    'maxscale_release': 'maxscale',
+    'mdbe': 'mdbe',
+    'mysql': 'mysql'
+  }
+
+  def initialize
+    @directory = Dir.mktmpdir
+    puts "Creating configuration in #{@directory}"
+  end
+
+  # Get links on the specified page
+  def get_links(repo_page, path = '/')
+    uri = "#{repo_page}/#{path}"
+    doc = Nokogiri::HTML(open(uri))
+    doc.css('a')
+  end
+
+  # This method goes through the main page and finds releases that should be added to
+  # the repository
+  def find_viable_releases(repo_page)
+    get_links(repo_page, '/').select do |link|
+      dir_link?(link)
+    end.select do |link|
+      links = get_links(repo_page, link[:href])
+                .map { |sublink| sublink.content }
+      yield(link, links)
+    end.map(&:content).map do |text|
+      text.delete('/')
+    end
+  end
+
+  def dir_link?(link)
+    link.content =~ /\/$/ || link[:href] =~ /^(?!((http|https):\/\/|\.{2}|\/|\?)).*\/$/
+  end
+
+  def create_repo(repo_page, systems, release_info, system, type, product, lambdas = {})
+    puts "Creating repository configuration for #{system} and #{product} #{release_info[:name]} release"
+    system_type = systems[type]
+    subpath = system_type[:path]
+    repos = get_links(repo_page, "#{release_info[:path]}/#{subpath}").select do |link|
+      if !lambdas[:repo_link_detector].nil?
+        lambdas[:repo_link_detector].call(link)
+      else
+        link.content =~ /^#{system}(\/?)$/
+      end
+    end.each_with_object([]) do |link, repositories|
+      repo_link = "#{release_info[:path]}/#{subpath}/#{link[:href]}".gsub(/\/\//, '/')
+      add_repo_from_platform_dir(product, release_info[:name], repo_link, repo_page, repositories, system, system_type)
+    end
+
+    lambdas[:result_handler].call(repos) unless lambdas[:result_handler].nil?
+
+    return if repos.empty? || !lambdas[:result_handler].nil?
+    FileUtils.mkdir_p("#{directory}/#{system}")
+    File.write("#{directory}/#{system}/#{release_info[:name]}.json", JSON.pretty_generate(repos))
+  end
+
+  def add_repo_from_platform_dir(product, release, repo_link, repo_page, repositories, system, system_type)
+    get_links(repo_page, system_type[:release_path].call(repo_link)).select do |link|
+      dir_link?(link)
+    end.each do |release_link|
+      release_name = release_link.content.delete('/')
+      repo_path = system_type[:repo_path].call(repo_link, release_name)
+      repositories << {
+        repo: repo_path,
+        repo_key: system_type[:key].call(release),
+        platform_version: release_name,
+        platform: system,
+        product: product,
+        version: release
+      }
+    end
+    repositories
+  end
+
+  def parse_product(config, product, lambdas = {}, systems_info = {}, release_info = {})
+    systems = {
+      debian: {
+        path: '',
+        repo_page: config['repo']['deb']['path'],
+        key: ->(_version) { config['repo']['deb']['key'] },
+        release_path: ->(repo_link) { "#{repo_link}/dists" },
+        release_name: ->(release) { release },
+        repo_path: ->(repo_link, release_name) {
+          "#{config['repo']['deb']['path']}#{repo_link} #{release_name} main"
+        }
+      },
+      rhel: {
+        path: '',
+        repo_page: config['repo']['rpm']['path'],
+        key: ->(_version) { config['repo']['rpm']['key'] },
+        release_path: ->(repo_link) { repo_link },
+        release_name: ->(release) { release },
+        repo_path: ->(repo_link, release_name) {
+          "#{config['repo']['rpm']['path']}#{repo_link}/#{release_name}/x86_64"
+        }
+      }
+    }
+    systems.each_key do |system_type|
+      systems[system_type].merge!(systems_info[system_type]) unless systems_info[system_type].nil?
+    end
+
+    systems.each do |system_type, system_info|
+      if !release_info.nil? && !release_info[:path].nil? && !release_info[:name].nil?
+        create_repo_for_platforms(config['platforms'], product, release_info, system_info, system_type, systems)
+        next
+      end
+
+      find_viable_releases(system_info[:repo_page]) do |link, links|
+        if !lambdas[:viable_release_detector].nil?
+          lambdas[:viable_release_detector].call(system_type, system_info, link, links)
+        else
+          config['platforms'].map do |system_name|
+            links.grep(/^#{system_name}(\/?)$/).any?
+          end.any?
+        end
+      end.each do |release|
+        current_release_info = { path: release, name: system_info[:release_name].call(release) }
+        create_repo_for_platforms(config['platforms'], product, current_release_info, system_info, system_type, systems)
+      end
+    end
+  end
+
+  def create_repo_for_platforms(platforms, product, release_info, system_info, system_type, systems)
+    puts "Configuring #{product} release #{release_info[:name]}"
+    platforms.each do |system_name|
+      next if system_type != system_type_by_system_name(system_name)
+      send("create_repo_#{product}".to_sym,
+           system_info[:repo_page],
+           systems,
+           release_info,
+           system_name,
+           system_type)
+    end
+  end
+
+  def create_repo_community(repo_page, systems, release, system, type)
+    create_repo(repo_page, systems, release, system, type, 'mariadb')
+  end
+
+  def create_repo_columnstore(repo_page, systems, release, system, type)
+    create_repo(
+      repo_page,
+      systems,
+      release,
+      system,
+      type,
+      'columnstore',
+      repo_link_detector: ->(link) {
+        link.content.include?(system) && !link.content.include?('.')
+      }
+    )
+  end
+
+  def create_repo_galera(repo_page, systems, release, system, type)
+    create_repo(repo_page, systems, release, system, type, 'galera')
+  end
+
+  def create_repo_mdbe(repo_page, systems, release, system, type)
+    create_repo(repo_page, systems, release, system, type, 'mdbe')
+  end
+
+  def create_repo_maxscale_ci(repo_page, systems, release, system, type)
+    create_repo(
+      repo_page,
+      systems,
+      release,
+      system,
+      type,
+      'maxscale',
+      result_handler: ->(repos) {
+        repos.each do |repo|
+          platform = repo[:platform]
+          platform_version = repo[:platform_version]
+          File.write("#{directory}/#{platform}-#{platform_version}.json", JSON.pretty_generate(repo))
+        end
+      }
+    )
+  end
+
+  def create_repo_maxscale_release(repo_page, systems, release, system, type)
+    create_repo(
+      repo_page,
+      systems,
+      release,
+      system,
+      type,
+      'maxscale',
+      result_handler: ->(repos) {
+        repos.each do |repo|
+          platform = repo[:platform]
+          platform_version = repo[:platform_version]
+          version = repo[:version]
+
+          repo[:version] = 'default'
+          repo[:key] =
+          File.write("#{directory}/#{platform}_#{platform_version}_#{version}.json", JSON.pretty_generate(repo))
+        end
+      }
+    )
+  end
+
+  def create_repo_mysql(repo_page, systems, release, system, type)
+    create_repo(
+      repo_page,
+      systems,
+      release,
+      system,
+      type,
+      'mysql',
+      repo_link_detector: ->(link) {
+        (%w[centos rhel].include?(system) && !(link.content =~ /^el(\/?)$/).nil?) ||
+          (system == 'sles' && !(link.content =~ /^sles(\/?)$/).nil?)
+      }
+    )
+  end
+
+  def parse_community(config)
+    parse_product(config, 'community')
+  end
+
+  def parse_columnstore(config)
+    systems = {
+      debian: {
+        path: 'repo',
+        repo_path: -> (repo_link, _release_name) { "#{config['repo']['deb']['path']}#{repo_link}" }
+      },
+      rhel: {
+        path: 'yum'
+      }
+    }
+
+    parse_product(
+      config,
+      'columnstore',
+      {
+        viable_release_detector: ->(_system_type, _system_info, _link, links) {
+          links.grep(/^yum(\/?)$/).any? && links.grep(/^repo(\/?)$/).any?
+        }
+      },
+      systems
+    )
+  end
+
+  def parse_galera(config)
+    parse_product(config, 'galera')
+  end
+
+  def parse_mdbe(config)
+    systems = {
+      debian: {
+        path: 'repo',
+        repo_path: ->(repo_link, release_name) {
+          "#{config['repo']['deb']['path']}#{repo_link} #{release_name} main"
+        }
+      },
+      rhel: {
+        path: 'yum',
+        repo_path: ->(repo_link, release_name) {
+          "#{config['repo']['rpm']['path']}#{repo_link}/#{release_name}"
+        }
+      }
+    }
+
+    parse_product(
+      config,
+      'mdbe',
+      {
+        viable_release_detector: ->(_system_type, _system_info, _link, links) {
+          links.grep(/^yum(\/?)$/).any? && links.grep(/^repo(\/?)$/).any?
+        }
+      },
+      systems
+    )
+  end
+
+  def parse_maxscale_ci(config)
+    ci = @maxscale_ci || config['repo']['deb']['ci_default']
+    deb_repo_page = config['repo']['deb']['path'].sub('##ci##', ci)
+    rpm_repo_page = config['repo']['rpm']['path'].sub('##ci##', ci)
+
+    systems = {
+      debian: {
+        path: '',
+        repo_page: deb_repo_page,
+        repo_path: ->(repo_link, release_name) {
+          "#{deb_repo_page}#{repo_link} #{release_name} main"
+        }
+      },
+      rhel: {
+        path: '',
+        repo_page: rpm_repo_page,
+        repo_path: ->(repo_link, release_name) {
+          "#{rpm_repo_page}#{repo_link}/#{release_name}/$basearch"
+        }
+      }
+    }
+
+    parse_product(
+      config,
+      'maxscale_ci',
+      {},
+      systems,
+      {
+        name: 'default',
+        path: ''
+      }
+    )
+  end
+
+  def parse_maxscale_release(config)
+    systems = {
+      debian: {
+        path: '',
+        key: ->(version) {
+          if config['repo']['deb']['old_key_versions'].include?(version)
+            config['repo']['deb']['old_key']
+          else
+            config['repo']['deb']['key']
+          end
+        }
+      },
+      rhel: {
+        path: '',
+        key: ->(version) {
+          if config['repo']['rpm']['old_key_versions'].include?(version)
+            config['repo']['rpm']['old_key']
+          else
+            config['repo']['rpm']['key']
+          end
+        },
+        repo_path: ->(repo_link, release_name) {
+          "#{config['repo']['rpm']['path']}#{repo_link}/#{release_name}/$basearch"
+        }
+      }
+    }
+
+    parse_product(
+      config,
+      'maxscale_release',
+      {},
+      systems
+    )
+  end
+
+  def parse_mysql_debian_systems(config)
+    release_regexp = /^mysql-(\d+\.?\d+(-[^\/]*)?)(\/?)$/
+    debian = {
+      path: '',
+      repo_page: config['repo']['deb']['path'],
+      key: ->(_version) { config['repo']['deb']['key'] },
+      release_path: ->(repo_link) { "#{repo_link}/dists" },
+      release_name: ->(release) { release },
+      repo_path: ->(system, release_name, version) {
+        "deb #{config['repo']['deb']['path']}#{system} #{release_name} mysql-#{version}"
+      }
+    }
+
+    repo_page = debian[:repo_page]
+    subpath = debian[:path]
+    release_path = debian[:release_path]
+    product = 'mysql'
+
+    puts 'Configuring mysql for debian systems'
+    config['platforms'].each do |system_name|
+      next if system_type_by_system_name(system_name) != :debian
+
+      puts "Creating repository configuration for #{system_name}"
+      repos = get_links(repo_page).select do |link|
+        link.content =~ /^#{system_name}(\/?)$/
+      end.each_with_object([]) do |link, repositories|
+        repo_link = "#{subpath}/#{link[:href]}".gsub(/\/\//, '/')
+
+        get_links(repo_page, release_path.call(repo_link)).select do |repo_inner_link|
+          dir_link?(repo_inner_link)
+        end.each do |release_link|
+          release_name = release_link.content.delete('/')
+          get_links(repo_page, "#{release_path.call(repo_link)}/#{release_name}").each do |release_inner_link|
+            next if (release_inner_link.content =~ release_regexp).nil?
+            version = release_inner_link.content.match(release_regexp).captures.first
+            repo_path = debian[:repo_path].call(system_name, release_name, version)
+            repositories << {
+              repo: repo_path,
+              repo_key: debian[:key].call(release_name),
+              platform_version: release_name,
+              platform: system_name,
+              product: product,
+              version: version
+            }
+          end
+        end
+        repositories
+      end
+
+      break if repos.empty?
+      repos.map { |repo| repo[:version] }.uniq.each do |version|
+        FileUtils.mkdir_p("#{directory}/#{system_name}")
+        File.write(
+          "#{directory}/#{system_name}/#{version}.json",
+          JSON.pretty_generate(repos.select { |repo| repo[:version] == version })
+        )
+      end
+    end
+  end
+
+  def parse_mysql(config)
+    rpm_release_regexp = /^mysql-(\d+\.?\d+)-community(\/?)$/
+
+    systems = {
+      rhel: {
+        path: '',
+        release_name: ->(release) { release.match(rpm_release_regexp).captures.first },
+        repo_path: ->(repo_link, release_name) {
+          "#{config['repo']['rpm']['path']}#{repo_link}/#{release_name}/x86_64"
+        }
+      }
+    }
+
+    parse_product(
+      config,
+      'mysql',
+      {
+        viable_release_detector: ->(system_type, _system_info, link, _links) {
+          system_type != :debian && !(link.content =~ rpm_release_regexp).nil?
+        }
+      },
+      systems
+    )
+    parse_mysql_debian_systems(config)
+  end
+
+  def system_type_by_system_name(system_name)
+    case system_name
+      when 'centos', 'sles', 'rhel', 'opensuse'
+        :rhel
+      when 'debian', 'ubuntu'
+        :debian
+      else
+        nil
+    end
+  end
+
+  def generate(config_file, products, dest, maxscale_ci = nil)
+    @maxscale_ci = maxscale_ci
+    @config = Config::parse(config_file)
+    products.each do |product|
+      FileUtils.rm_rf("#{@directory}/.", secure: true)
+      begin
+        send("parse_#{product}".to_sym, @config[product])
+      rescue Exception => e
+        puts "ERROR: #{product} was not generated"
+        puts e
+        next
+      end
+      product_name = PRODUCTS_DIR_NAMES[product] || PRODUCTS_DIR_NAMES[product.to_sym]
+      next if product_name.nil?
+      FileUtils.mkdir_p("#{dest}/#{product_name}")
+      FileUtils.rm_rf("#{dest}/#{product_name}/.", secure: true)
+      FileUtils.cp_r("#{@directory}/.", "#{dest}/#{product_name}")
+    end
+  end
+end
+
+if ARGV.length < 2
+  puts <<-EOF
+Usage:
+  generate_repo_d CONFIG_FILE DESTINATION_PATH PRODUCT PRODUCT_ARG
+
+  CONFIG_FILE: The config file path.
+  DESTINATION_PATH: The destination path.
+  PRODUCT (optional): Generate the repository for the specified product.
+  PRODUCT_ARG (optional): Only for maxscale_ci - $ci (example: develop).
+
+Examples:
+  Generate repo.d for all products: `generate_repo_d ./config_template.yaml ~/mdbci/repo.d`
+  Generate repod.d for maxscale_ci and default $ci: `generate_repo_d ./config_template.yaml ~/mdbci/repo.d maxscale_ci`
+  Generate repod.d for maxscale_ci and specified $ci: `generate_repo_d ./config_template.yaml ~/mdbci/repo.d maxscale_ci develop`
+  EOF
+  exit 0
+end
+
+CONFIG_FILE = ARGV.shift
+DESTINATION_PATH = ARGV.shift
+PRODUCTS = ARGV.first.nil? ? GenerateRepoD::DEFAULT_PRODUCTS : [ARGV.shift]
+PRODUCT_ARG = ARGV.shift
+
+GenerateRepoD.new.generate(CONFIG_FILE, PRODUCTS, DESTINATION_PATH, PRODUCT_ARG) if $0 == __FILE__
