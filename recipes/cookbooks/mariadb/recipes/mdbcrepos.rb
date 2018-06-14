a86cdf05c27734fafb2f79e3e268fe0f266557d5
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2018-06-14 10:46:25 +0300

    Use repository key provided in the repository configuration instead of hard-coded value during MariaDB installation (refs #24125)

--- recipes/cookbooks/mariadb/recipes/mdbcrepos.rb
+++ recipes/cookbooks/mariadb/recipes/mdbcrepos.rb
@@ -14,17 +14,19 @@ end
 #
 case node[:platform_family]
   when "debian", "ubuntu", "mint"
+  release_name = '$(lsb_release -cs)'
+  system "echo MariaDB version: #{node['mariadb']['version']}"
+  system "echo MariaDB repo: #{node['mariadb']['repo']}"
+  system "echo MariaDB repo key: #{node['mariadb']['repo_key']}"
+  system 'echo MDBCI plain repo recipe'
+
   # Add repo key
   execute "Key add" do
-    command "apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db"
+    command "apt-key adv --recv-keys --keyserver keyserver.ubuntu.com #{node['mariadb']['repo_key']}"
   end
-  release_name = '$(lsb_release -cs)'
-  system 'echo MariaDB version: ' + node['mariadb']['version']
-  system 'echo MariaDB repo: ' + node['mariadb']['repo']
-  system 'echo MariaDB repo key: ' + node['mariadb']['repo_key']
-  system 'echo MDBCI plain repo recipe'
+
   #6373 to be removed command 'echo "deb ' + node['mariadb']['repo'] + '/' + node['mariadb']['version'] + '/' + node[:platform] + ' ' + release_name + ' main" > /etc/apt/sources.list.d/mariadb.list'
-  addrepocmd = 'echo "deb '+ node['mariadb']['repo']+' ">/etc/apt/sources.list.d/mariadb.list'
+  addrepocmd = "echo 'deb #{node['mariadb']['repo']}' > /etc/apt/sources.list.d/mariadb.list"
 
   # Add repo
   execute "Repository add" do
@@ -48,7 +50,6 @@ case node[:platform_family]
 
   release_name = "if cat /etc/SuSE-release | grep Enterprise &>/dev/null; then echo sles; else echo opensuse; fi"
   execute "Change suse on sles repository" do
-    command "cat /etc/zypp/repos.d/mariadb.repo | sed s/suse/$(" + release_name + ")/g > /etc/zypp/repos.d/mariadb.repo"
+    command "cat /etc/zypp/repos.d/mariadb.repo | sed s/suse/$(#{release_name})/g > /etc/zypp/repos.d/mariadb.repo"
   end
-
 end
