01a9e26a7460bd4eb14a898ca46e8e3a6640a4c0
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2018-06-19 17:56:57 +0300

    Automatically fix issues in MariaDB and MySQL repository configuration scripts according to Rubocop suggestions (refs #24125)

--- recipes/cookbooks/mariadb/recipes/mdbcrepos.rb
+++ recipes/cookbooks/mariadb/recipes/mdbcrepos.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 include_recipe 'packages::configure_apt'
 
 # Install default packages
-%w( net-tools psmisc ).each do |pkg|
+%w[net-tools psmisc].each do |pkg|
   package pkg do
     retries 2
     retry_delay 10
@@ -10,9 +12,9 @@ end
 
 # Configure repository
 case node[:platform_family]
-when "debian", "ubuntu", "mint"
+when 'debian', 'ubuntu', 'mint'
   # Add repo key
-  execute "Key add" do
+  execute 'Key add' do
     command "apt-key adv --recv-keys --keyserver keyserver.ubuntu.com #{node['mariadb']['repo_key']}"
   end
 
@@ -24,24 +26,24 @@ when "debian", "ubuntu", "mint"
     action :create
   end
 
-  execute "Update repository cache" do
-    command "apt-get update"
+  execute 'Update repository cache' do
+    command 'apt-get update'
   end
 
-when "rhel", "fedora", "centos"
-  template "/etc/yum.repos.d/mariadb.repo" do
-    source "mdbci.mariadb.rhel.erb"
+when 'rhel', 'fedora', 'centos'
+  template '/etc/yum.repos.d/mariadb.repo' do
+    source 'mdbci.mariadb.rhel.erb'
     action :create
   end
 
-when "suse"
-  template "/etc/zypp/repos.d/mariadb.repo" do
-    source "mdbci.mariadb.suse.erb"
+when 'suse'
+  template '/etc/zypp/repos.d/mariadb.repo' do
+    source 'mdbci.mariadb.suse.erb'
     action :create
   end
 
-  release_name = "if cat /etc/SuSE-release | grep Enterprise &>/dev/null; then echo sles; else echo opensuse; fi"
-  execute "Change suse on sles repository" do
+  release_name = 'if cat /etc/SuSE-release | grep Enterprise &>/dev/null; then echo sles; else echo opensuse; fi'
+  execute 'Change suse on sles repository' do
     command "cat /etc/zypp/repos.d/mariadb.repo | sed s/suse/$(#{release_name})/g > /etc/zypp/repos.d/mariadb.repo"
   end
 end
--- recipes/cookbooks/mysql/recipes/mdbcrepos.rb
+++ recipes/cookbooks/mysql/recipes/mdbcrepos.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 include_recipe 'packages::configure_apt'
 
 # Install default packages
-%w( net-tools psmisc ).each do |pkg|
+%w[net-tools psmisc].each do |pkg|
   package pkg do
     retries 2
     retry_delay 10
@@ -10,10 +12,10 @@ end
 
 # Configure repository
 case node[:platform_family]
-when "debian", "ubuntu", "mint"
+when 'debian', 'ubuntu', 'mint'
   # Add repo key
-  execute "Key add" do
-    command "apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 5072E1F5"
+  execute 'Key add' do
+    command 'apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 5072E1F5'
   end
 
   file '/etc/apt/sources.list.d/mysql.list' do
@@ -24,24 +26,24 @@ when "debian", "ubuntu", "mint"
     action :create
   end
 
-  execute "Update repository cache" do
-    command "apt-get update"
+  execute 'Update repository cache' do
+    command 'apt-get update'
   end
 
-when "rhel", "fedora", "centos"
-  template "/etc/yum.repos.d/mysql.repo" do
-    source "mdbci.mysql.rhel.erb"
+when 'rhel', 'fedora', 'centos'
+  template '/etc/yum.repos.d/mysql.repo' do
+    source 'mdbci.mysql.rhel.erb'
     action :create
   end
 
-when "suse"
-  template "/etc/zypp/repos.d/mysql.repo" do
-    source "mdbci.mysql.suse.erb"
+when 'suse'
+  template '/etc/zypp/repos.d/mysql.repo' do
+    source 'mdbci.mysql.suse.erb'
     action :create
   end
 
-  release_name = "if cat /etc/SuSE-release | grep Enterprise &>/dev/null; then echo sles; else echo opensuse; fi"
-  execute "Change suse on sles repository" do
-    command "cat /etc/zypp/repos.d/mysql.repo | sed s/suse/$(" + release_name + ")/g > /etc/zypp/repos.d/mysql.repo"
+  release_name = 'if cat /etc/SuSE-release | grep Enterprise &>/dev/null; then echo sles; else echo opensuse; fi'
+  execute 'Change suse on sles repository' do
+    command 'cat /etc/zypp/repos.d/mysql.repo | sed s/suse/$(' + release_name + ')/g > /etc/zypp/repos.d/mysql.repo'
   end
 end
