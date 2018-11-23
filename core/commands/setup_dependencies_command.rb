d83f57a46cd02d04b8a7108f8ecbc834b4a17bb0
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-11-23 15:46:40 +0300

    Extract dependency management of linux distribution into a separate class (refs #24572)

--- core/commands/setup_dependencies_command.rb
+++ core/commands/setup_dependencies_command.rb
@@ -3,6 +3,7 @@
 require_relative 'base_command'
 require_relative '../services/shell_commands'
 
+# Command installs reqired dependencies for running mdbci
 class SetupDependenciesCommand < BaseCommand
   include ShellCommands
 
@@ -10,12 +11,21 @@ class SetupDependenciesCommand < BaseCommand
     'Installs vagrant and its dependencies'
   end
 
-  def execute
-    distro = get_linux_distro.downcase
-    if @env.reinstall
-      return SUCCESS_RESULT unless delete_dependencies(distro)
+  def initialize(arg, env, logger)
+    super(arg, env, logger)
+    case get_linux_distro.downcase
+    when 'centos'
+      @dependency_manager = CentosDependencyManager.new(arg, env, logger)
+    when 'debian', 'ubuntu'
+      @dependency_manager = DebianDependencyManager.new(arg, env, logger)
     end
-    result = install_dependencies(distro)
+  end
+
+  def execute
+    if @env.reinstall
+      return SUCCESS_RESULT unless delete_packages
+    end
+    result = @dependency_manager.install_dependencies
     result = prepare_environment if result.success?
     result.success? ? SUCCESS_RESULT : ERROR_RESULT
   end
@@ -30,32 +40,11 @@ class SetupDependenciesCommand < BaseCommand
     end
   end
 
-  # Installs dependencies for supported platforms
-  def install_dependencies(distro)
-    vagrant_package = 'vagrant_2.2.0_x86_64'
-    vagrant_url = "https://releases.hashicorp.com/vagrant/2.2.0/#{vagrant_package}"
-    case distro
-    when 'centos'
-      result = run_command('sudo yum -y install libvirt-client qemu git')
-      result = run_command("sudo yum -y install #{vagrant_url}.rpm") if result[:value].success?
-    when 'debian', 'ubuntu'
-      run_command('sudo apt-get update')
-      result = run_command('sudo apt-get -y install build-essential libxslt-dev '\
-                           'libxml2-dev libvirt-dev wget git cmake')
-      result = run_command("wget #{vagrant_url}.deb") if result[:value].success?
-      result = run_command("sudo dpkg -i #{vagrant_package}.deb") if result[:value].success?
-      run_command("rm #{vagrant_package}.deb")
-    else
-      raise 'Unknown platform'
-    end
-    result[:value]
-  end
-
   # Install vagrant plugins and prepares mdbci environment
   def prepare_environment
     result = run_command('vagrant plugin install vagrant-libvirt --plugin-version 0.0.43')
     result = run_command('vagrant plugin install vagrant-aws --plugin-version 0.7.2') if result[:value].success?
-    result = run_command('sudo mkdir -p /var/lib/libvirt/libvirt-images')  if result[:value].success?
+    result = run_command('sudo mkdir -p /var/lib/libvirt/libvirt-images') if result[:value].success?
     result = run_command('sudo virsh pool-create-as default dir '\
                          '--target /var/lib/libvirt/libvirt-images') if result[:value].success?
     result = run_command('sudo usermod -a -G libvirt $(whoami)') if result[:value].success?
@@ -64,10 +53,11 @@ class SetupDependenciesCommand < BaseCommand
     result[:value]
   end
 
-  def delete_dependencies(distro)
+  def delete_packages
     $stdout.print("This operation will uninstall following packages:
   vagrant,
-  #{distro == 'centos' ? 'libvirt-client' : 'libvirt-dev'},
+  libvirt-client,
+  libvirt-dev,
 as well as all installed vagrant plugins and 'default' libvirt pool.
 Are you sure you want to continue? [y/N]: ")
     while input = gets.strip
@@ -81,7 +71,7 @@ Are you sure you want to continue? [y/N]: ")
     end
     delete_libvirt_pool
     delete_vagrant_plugins
-    delete_packages(distro)
+    @dependency_manager.delete_dependencies
     SUCCESS_RESULT
   end
 
@@ -103,15 +93,61 @@ Are you sure you want to continue? [y/N]: ")
       run_command("vagrant plugin uninstall #{plugins.join(' ')}")
     end
   end
+end
 
-  def delete_packages(distro)
-    case distro
-    when 'centos'
-      run_command('sudo yum -y remove vagrant')
-      run_command('sudo yum -y remove libvirt-client')
-    when 'debian', 'ubuntu'
-      run_command('sudo dpkg -P vagrant')
-      run_command('sudo dpkg -P libvirt-dev')
-    end
+# Base class for a dependency manager for a specific linux distribution
+class DependencyManager
+  include ShellCommands
+
+  VAGRANT_VERSION = '2.2.0'
+  VAGRANT_PACKAGE = "vagrant_#{VAGRANT_VERSION}_x86_64"
+  VAGNRAT_URL = "https://releases.hashicorp.com/vagrant/#{VAGRANT_VERSION}/#{VAGRANT_PACKAGE}"
+
+  def initialize(args, env, logger)
+    @args = args
+    @env = env
+    @ui = logger
+  end
+
+  # Installs dependencies for supported platforms
+  def install_dependencies
+    raise 'Not implemented'
+  end
+
+  # Deletes dependencies on supported platform
+  def delete_dependencies
+    raise 'Not implemented'
+  end
+end
+
+# Class that manages CentOS specific packages
+class CentosDependencyManager < DependencyManager
+  def install_dependencies
+    result = run_command('sudo yum -y install libvirt-client qemu git')
+    result = run_command("sudo yum -y install #{VAGNRAT_URL}.rpm") if result[:value].success?
+    result[:value]
+  end
+
+  def delete_dependencies
+    run_command('sudo yum -y remove vagrant')
+    run_command('sudo yum -y remove libvirt-client')
+  end
+end
+
+# Class that manages Debian specific packages, also suit for Ubuntu
+class DebianDependencyManager < DependencyManager
+  def install_dependencies
+    run_command('sudo apt-get update')
+    result = run_command('sudo apt-get -y install build-essential libxslt-dev '\
+                         'libxml2-dev libvirt-dev wget git cmake')
+    result = run_command("wget #{VAGNRAT_URL}.deb") if result[:value].success?
+    result = run_command("sudo dpkg -i #{VAGRANT_PACKAGE}.deb") if result[:value].success?
+    run_command("rm #{VAGRANT_PACKAGE}.deb")
+    result[:value]
+  end
+
+  def delete_dependencies
+    run_command('sudo dpkg -P vagrant')
+    run_command('sudo dpkg -P libvirt-dev')
   end
 end
