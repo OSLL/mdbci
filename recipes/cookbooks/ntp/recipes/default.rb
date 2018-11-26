e2d66c26f63be8cb5db4c4722d82d6af6d572709
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2018-11-26 16:10:36 +0300

    Remove script that changes date during the NTP configuration (refs #24618)

--- recipes/cookbooks/ntp/recipes/default.rb
+++ recipes/cookbooks/ntp/recipes/default.rb
@@ -30,24 +30,3 @@ template "/etc/ntp.conf" do
   mode 0644
   notifies :restart, resources(:service => node[:ntp][:service])
 end
-
-script "test_date" do
-  interpreter "bash"
-  user "root"
-  environment 'platform' => node['platform']
-  code <<-EOH
-    sudo date --set "12 Sep 2012 12:12:12"
-    echo @@@ TEST DATE: `date`
-    case $platform in
-    ubuntu|debian)
-        sudo sntp -s 0.europe.pool.ntp.org
-        ;;
-    *)
-        sudo service ntpd stop
-        sudo ntpdate 0.europe.pool.ntp.org
-        sudo service ntpd start
-        ;;
-    esac
-    echo @@@ SYNC DATE: `date`
-  EOH
-end
