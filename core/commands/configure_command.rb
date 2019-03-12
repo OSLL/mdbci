8bf5ce4da3470a3b0907dc009aa3433fca23abaf
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2019-03-12 19:36:07 +0300

    Add the configuration for RHEL credentials (refs #25042)

--- core/commands/configure_command.rb
+++ core/commands/configure_command.rb
@@ -16,32 +16,69 @@ class ConfigureCommand < BaseCommand
 
   def show_help
     info = <<-HELP
-'configure' command creates configuration for MDBCI to use with AWS.
+'configure' command creates configuration for MDBCI to use AWS and RHEL subscription.
+
+You can configure all products (AWS and RHEL):
+  mdbci configure
+
+Or you can configure specified product (for example, AWS):
+  mdbci configure --product aws
     HELP
     @ui.info(info)
   end
 
+  # rubocop:disable Metrics/CyclomaticComplexity
   def execute
     if @env.show_help
       show_help
       return SUCCESS_RESULT
     end
-    credentials = input_aws_credentials
-    return ERROR_RESULT if credentials.nil?
-    security_group = input_or_create_security_group(credentials)
-    credentials['security_group'] = security_group
-    @configuration['aws'] = credentials
+    configure_results = []
+
+    configure_results << configure_aws if @env.nodeProduct.casecmp('aws').zero? || @env.nodeProduct.nil?
+    configure_results << configure_rhel if @env.nodeProduct.casecmp('rhel').zero? || @env.nodeProduct.nil?
+
+    return ERROR_RESULT if configure_results.include?(ERROR_RESULT)
+
     @configuration.save
     SUCCESS_RESULT
   end
+  # rubocop:enable Metrics/CyclomaticComplexity
 
   private
 
+  def configure_aws
+    aws_credentials = input_aws_credentials
+    return ERROR_RESULT if aws_credentials.nil?
+
+    aws_security_group = input_or_create_security_group(aws_credentials)
+    aws_credentials['security_group'] = aws_security_group
+    @configuration['aws'] = aws_credentials
+    SUCCESS_RESULT
+  end
+
+  def configure_rhel
+    rhel_credentials = input_rhel_subscription_credentials
+    return ERROR_RESULT if rhel_credentials.nil?
+
+    @configuration['rhel'] = rhel_credentials
+    SUCCESS_RESULT
+  end
+
+  def input_rhel_subscription_credentials
+    {
+      login: read_topic('Please input login for Red Hat Subscription-Manager'),
+      password: read_topic('Please input password for Red Hat Subscription-Manager'),
+      pool_id: read_topic('Please input pool id for Red Hat Subscription-Manager')
+    }
+  end
+
   def input_or_create_security_group(credentials)
-    if read_topic('Create new AWS security group?', 'y').downcase == 'y'
+    if read_topic('Create new AWS security group?', 'y').casecmp('y').zero?
       aws_service = AwsService.new(credentials, @ui)
       security_group = aws_service.create_security_group
       return ERROR_RESULT if security_group.nil?
+
       @ui.info("Created new security group: #{security_group}")
       security_group
     else
@@ -59,10 +96,11 @@ class ConfigureCommand < BaseCommand
       region = read_topic('Please input AWS region', region)
       check_complete = AwsService.check_credentials(@ui, key_id, secret_key, region)
       break if check_complete
+
       @ui.error('You have provided inappropriate information.')
-      break nil unless read_topic('Try again?', 'y').downcase == 'y'
+      return nil unless read_topic('Try again?', 'y').casecmp('y').zero?
     end
-    { 'access_key_id' => key_id, 'secret_access_key' => secret_key, 'region' => region }
+    { access_key_id: key_id, secret_access_key: secret_key, region: region }
   end
 
   # Ask user to input non-empty string as value
@@ -72,6 +110,7 @@ class ConfigureCommand < BaseCommand
       user_input = $stdin.gets.strip
       user_input = default_value if user_input.empty?
       break user_input unless user_input.empty?
+
       $stdout.puts("Please provide the #{topic}.")
     end
   end
