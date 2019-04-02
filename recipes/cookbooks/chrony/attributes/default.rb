63f6fd4ae8e3d01dedd7cd63d23f2d1bd6c9d6c4
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2019-04-02 20:26:30 +0300

    Refactor the chrony recipe and remove ntp package before chrony installation (refs #25111)

--- recipes/cookbooks/chrony/attributes/default.rb
+++ recipes/cookbooks/chrony/attributes/default.rb
@@ -1,29 +1,22 @@
 # frozen_string_literal: true
 
 # Chrony configuration file
-default['chrony']['config_file'] = {
-  'centos' => '/etc/chrony.conf',
-  'redhat' => '/etc/chrony.conf',
-  'debian' => '/etc/chrony/chrony.conf',
-  'ubuntu' => '/etc/chrony/chrony.conf',
-  'linux' => '/etc/chrony.conf',
-  'suse' => '/etc/chrony.conf'
-}
+default[:chrony][:config_file] = case node[:platform]
+                                 when 'ubuntu', 'debian'
+                                   '/etc/chrony/chrony.conf'
+                                 else
+                                   '/etc/chrony.conf'
+                                 end
 
 # Default configuration arrays
-default['chrony']['servers'] = {
-  '0.europe.pool.ntp.org' => 'offline minpoll 8'
+default[:chrony][:servers] = {
+  '0.europe.pool.ntp.org': 'offline minpoll 8'
 }
 
 # Chrony service
-default['chrony']['service'] = {
-  'centos' => 'chronyd',
-  'redhat' => 'chronyd',
-  'debian' => 'chrony',
-  'ubuntu' => 'chrony',
-  'linux' => 'chronyd',
-  'suse' => 'chronyd'
-}
-
-# If chrony service is restarted when config file changes
-default['chrony']['auto_restart'] = true
+default[:chrony][:service] = case node[:platform]
+                             when 'ubuntu', 'debian'
+                               'chrony'
+                             else
+                               'chronyd'
+                             end
--- recipes/cookbooks/chrony/recipes/default.rb
+++ recipes/cookbooks/chrony/recipes/default.rb
@@ -1,22 +1,26 @@
 if node[:platform] == 'linux'
+  zypper_package 'ntp' do
+    action :remove
+  end
   zypper_package 'chrony'
 else
-  package 'chrony' do
-    action [:install]
+  package 'ntp' do
+    action :remove
   end
+  package 'chrony'
 end
 
-service node['chrony']['service'][node['platform']] do
+service node[:chrony][:service] do
   supports restart: true, status: true, reload: true
   action %i[enable start]
 end
 
-template node['chrony']['config_file'][node['platform']] do
+template node[:chrony][:config_file] do
   owner 'root'
   group 'root'
   mode '0644'
   source 'chrony.conf.erb'
-  notifies :restart, resources(service: node['chrony']['service'][node['platform']])
+  notifies :restart, resources(service: node[:chrony][:service])
 end
 
 template '/usr/local/bin/synchronize_time.sh' do
