83a2779b03b2a663bc13c882e64b1ba9aa9fcedb
Alexey Kallistov <alexey.kallistov@fruct.org>
2019-04-18 16:49:51 +0300

    Added exception handling (refs #25017)

--- core/commands/configure_network_command.rb
+++ core/commands/configure_network_command.rb
@@ -7,6 +7,7 @@ require 'net/ssh'
 # rubocop:disable Metrics/MethodLength
 class ConfigureNetworkCommand
   def self.publicKeysSsh(args, keyFile, labels, out)
+    exit_code = 0
     vm = MachineConfigurator.new(out)
     raise 'Configuration name is required' if args.nil? 
     args = args.split('/')
@@ -25,42 +26,50 @@ class ConfigureNetworkCommand
     if args[1].nil? # No node argument, copy keys to all nodes
       if labels.nil? # No label, copy keys to all nodes
         network.nodes.each do |node|
-          parse_node(node, keyFile, vm, configurationDir)
+          exit_code = parse_node(node, keyFile, vm, configurationDir)
         end
       else # Copy keys to nodes with select label
         network.nodes.each do |node|
           unless node.config.template[node.name]["labels"].nil?
             if node.config.template[node.name]["labels"].include? labels
-              parse_node(node, keyFile, vm, configurationDir)
+               exit_code = parse_node(node, keyFile, vm, configurationDir)
             end
           end
         end
       end
     else # Copy keys to select node
       node = network.nodes.find { |elem| elem.name == args[1] }
-      parse_node(node, keyFile, vm, configurationDir)
+      exit_code = parse_node(node, keyFile, vm, configurationDir)
     end   
-    exit_code = 0
+    exit_code
   end
     
   def self.upload_ssh_file(machine, keyFile, vm)
+    exit_code = 0
     options = Net::SSH.configuration_for(machine['network'], true)
     options[:auth_methods] = %w[publickey none]
     options[:verify_host_key] = false
     options[:keys] = [machine['keyfile']]
-    Net::SSH.start(machine['network'], machine['whoami'], options) do |ssh|  
-      vm.upload_file(ssh, keyFile, ".ssh/authorized_keys", false)
-    end 
+    begin
+      Net::SSH.start(machine['network'], machine['whoami'], options) do |ssh|  
+        vm.upload_file(ssh, keyFile, ".ssh/authorized_keys", false)
+      end
+    rescue
+      print "Could not connaction to machine #{machine['network']}\n"
+      exit_code = 1
+    end
+    exit_code
   end
   
   def self.parse_node(node, keyFile, vm, configurationDir)
+    exit_code = 0
     if node.nil?
       raise "No such node with name #{args[1]} in #{args[0]}"
     end
     config = NetworkConfigFile.new(configurationDir.path + '/' + node.name + '_network_config')
     machine = {"whoami" => config.configs[node.name]["whoami"], "network" => config.configs[node.name]["network"],
                "keyfile" => config.configs[node.name]["keyfile"] }
-    upload_ssh_file(machine, keyFile, vm)
+    exit_code = upload_ssh_file(machine, keyFile, vm)
   end
   
   def self.load_nodes_from_dir(configurationDir)
