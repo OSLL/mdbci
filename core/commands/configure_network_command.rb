92e3a03a3a4855035273a5ef6fcb8390b1085501
Alexey Kallistov <alexey.kallistov@fruct.org>
2019-04-19 16:07:38 +0300

    Fix code style (refs #25017)

--- core/commands/configure_network_command.rb
+++ core/commands/configure_network_command.rb
@@ -1,80 +1,86 @@
+# frozen_string_literal: true
+
 require_relative '../models/network_config_file'
 require_relative '../services/machine_configurator'
-require_relative '../network'
 require 'net/ssh'
 
+# This class loads ssh keys to configuration or selected nodes.
 class ConfigureNetworkCommand
-
-  def initialize(args, keyFile, labels, out)
+  # Create the command instance.
+  # @param args [String] string of arguments splits / for the current command.
+  # @param keyFile [String] path for ssh key on local machine
+  # @param labels [String] label of nodes for the current command
+  # @param out [Out] the object that should be used to log information.
+  def initialize(args, keyfile, labels, out)
     raise 'Configuration name is required' if args.nil?
+
     @args = args.split('/')
-    raise "Directory with nodes does not exists: #{@args[0]}" unless Dir.exists? @args[0]
-    @keyFile = keyFile
+    raise "Directory with nodes does not exists: #{@args[0]}" unless Dir.exist? @args[0]
+
+    @keyfile = keyfile
     @labels = labels
-    @out = out
-    @mc = MachineConfigurator.new(@out)
+    @mc = MachineConfigurator.new(out)
     @config = Configuration.new(@args[0])
   end
-  
-  # copy ssh keys to config/node
+
+  # This method is called whenever the command is executed
   def execute
     if @args[1].nil? # No node argument, copy keys to all nodes
       if @labels.nil?
-        public_keys_for_configuration
+        public_keys_to_configuration
       else
-        public_keys_for_nodes_with_label
+        public_keys_to_nodes_with_label
       end
     else
-      public_keys_for_node
+      public_keys_to_node
     end
   end
-  
+
   private
-  
+
   # copy ssh keys to config
-  def public_keys_for_configuration
+  def public_keys_to_configuration
     exit_code = 0
     nodes = @config.node_configurations
     raise "No aws, vbox, libvirt, docker nodes found in #{@args[0]}" if nodes.empty?
-      nodes.each do |node|
-        machine = parse_node(node)
-        code = upload_ssh_file(machine)
-        exit_code = 1 if code == 1
-      end
+
+    nodes.each do |node|
+      machine = parse_node(node)
+      code = upload_ssh_file(machine)
+      exit_code = 1 if code == 1
+    end
     exit_code
   end
-  
+
   # copy ssh keys to nodes with select label
-  def public_keys_for_nodes_with_label
+  def public_keys_to_nodes_with_label
     exit_code = 0
     count = 0
     nodes = @config.node_configurations
     raise "No aws, vbox, libvirt, docker nodes found in #{@args[0]}" if nodes.empty?
+
     nodes.each do |node|
-      unless node[1]["labels"].nil?
-        if node[1]["labels"].include? @labels
-          count = 1
-          machine = parse_node(node)
-          code = upload_ssh_file(machine)
-          exit_code = 1 if code == 1
-        end
-      end
+      next if node[1]['labels'].nil?
+
+      next if node[1]['labels'].include? @labels
+
+      count = 1
+      machine = parse_node(node)
+      code = upload_ssh_file(machine)
+      exit_code = 1 if code == 1
     end
-    print "No such node with label #{@label}\n" if count == 0
+    print "No such node with label #{@label}\n" if count.zero?
+
     exit_code
   end
-  
+
   # copy ssh keys to select node
-  def public_keys_for_node
-    exit_code = 0
-    node = @config.node_configurations.find{ |node| node[1]["hostname"] == @args[1]}
-    unless node.nil?
-      machine = parse_node(node)
-      exit_code = upload_ssh_file(machine)
-    else
-      raise "No such node with name #{@args[1]} in #{@args[0]}\n"
-    end
-    exit_code
+  def public_keys_to_node
+    node = @config.node_configurations.find { |nodeinfo| nodeinfo[1]['hostname'] == @args[1] }
+    raise "No such node with name #{@args[1]} in #{@args[0]}\n" if node.nil?
+
+    machine = parse_node(node)
+    upload_ssh_file(machine)
   end
 
   # Connect to the specified machine and upload ssh keyfile
@@ -86,10 +92,10 @@ class ConfigureNetworkCommand
     options[:verify_host_key] = false
     options[:keys] = [machine['keyfile']]
     begin
-      Net::SSH.start(machine['network'], machine['whoami'], options) do |ssh|  
-        @mc.upload_file(ssh, @keyFile, ".ssh/authorized_keys", false)
+      Net::SSH.start(machine['network'], machine['whoami'], options) do |ssh|
+        @mc.upload_file(ssh, @keyfile, '.ssh/authorized_keys', false)
       end
-    rescue
+    rescue StandardError
       print "Could not connaction to machine with name #{machine['name']}\n"
       exit_code = 1
     end
@@ -99,13 +105,12 @@ class ConfigureNetworkCommand
   # Parse information about machine
   # @param node [Node] node object
   def parse_node(node)
-    if node.nil?
-      raise "No such node with name #{args[1]} in #{args[0]}"
-    end
+    raise "No such node with name #{args[1]} in #{args[0]}" if node.nil?
+
     config = NetworkConfigFile.new(Dir.pwd + '/' + @args[0] + '_network_config')
-    {"whoami" => config.configs[node[1]["hostname"]]["whoami"],
-     "network" => config.configs[node[1]["hostname"]]["network"],
-     "keyfile" => config.configs[node[1]["hostname"]]["keyfile"],
-     "name" => node[1]["hostname"]}
+    { 'whoami' => config.configs[node[1]['hostname']]['whoami'],
+      'network' => config.configs[node[1]['hostname']]['network'],
+      'keyfile' => config.configs[node[1]['hostname']]['keyfile'],
+      'name' => node[1]['hostname'] }
   end
 end
