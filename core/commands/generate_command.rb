2c7ac523c97b3b8706350abb0d510c96b0c90225
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2019-04-25 15:33:47 +0300

    Introduce GenerateCommand as a proxy to the Vagrant configuration generation command (refs #25176)

---
+++ core/commands/generate_command.rb
@@ -0,0 +1,14 @@
+require_relative 'base_command'
+
+# Command acs as the gatekeeper for two generators: Vagrant-based configurator
+# and Docker-based configurator
+class GenerateCommand < BaseCommand
+  def self.synopsis
+    'Generate a configuration based on the template.'
+  end
+
+  def execute
+    command = GenerateVagrantConfigurationCommand.new(@args, @env, @ui)
+    command.execute(@args.shift, @env.isOverride)
+  end
+end
