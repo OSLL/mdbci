c44409c448f14966c8fa477bbf1599f641c77748
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2019-04-30 10:43:49 +0300

    Fix logic for copying configuration files and update checks for file absense (refs #25176)

--- core/services/docker_configuration_generator.rb
+++ core/services/docker_configuration_generator.rb
@@ -35,19 +35,32 @@ class DockerConfigurationGenerator
     service_config_path = File.join(@configuration_path, 'configs')
     FileUtils.mkdir_p(service_config_path)
     @template.each_node do |node_name, node|
-      next unless node.key?('product')
-      cnf_data = node['product'].values_at('cnf_template_path', 'cnf_template')
-      if cnf_data.size != 0 || cnf_data.size != 2
-        @ui.error("Error in #{node_name} configuration.")
-        @ui.error("You must provide both 'cnf_template' and 'cnf_template_path' when configuring products.")
-        return ERROR_RESULT
-      end
-      FileUtils.cp(File.join(*cnf_data), File.join(service_config_path, "#{node_name}.cnf"), verbose: true)
+      @ui.info("Copying configuration file for the node '#{node_name}'")
+      result = copy_product_config_file(node, File.join(service_config_path, "#{node_name}.cnf"))
+      return result if result != SUCCESS_RESULT
     end
     SUCCESS_RESULT
   rescue SystemCallError => error
     @ui.error("Error while copying configuration files into '#{service_config_path}'")
     @ui.error("Error message: #{error.message}")
     ERROR_RESULT
+  end
+
+  def copy_product_config_file(node, result_file)
+    unless node.key?('product')
+      @ui.error("The node '#{node_name}' does not specify the product to be installed")
+      return ERROR_RESULT
+    end
+    product = node['product']
+    if product.key?('cnf_template') && product.key?('cnf_template_path')
+      FileUtils.cp(File.join(File.expand_path(product['cnf_template_path'], File.dirname(@template_file)),
+                             product['cnf_template']), result_file)
+    elsif product.key?('cnf_template')
+      FileUtils.cp(File.expand_path(product['cnf_template'], File.dirname(@template_file)), result_file)
+    else
+      @ui.error("You must provide path to configuration file in 'cnf_template' and 'cnf_template_path'.")
+      return ERROR_RESULT
+    end
+    SUCCESS_RESULT
   end
 end
