5d4acabc18cdeabff8a9b3b5cad31b18406db160
Alexey Kallistov <alexey.kallistov@fruct.org>
2019-06-19 16:46:43 +0300

    Add a congigurationgenerator class to generate a role-file (refs #25198)

--- core/commands/install_product_command.rb
+++ core/commands/install_product_command.rb
@@ -2,6 +2,7 @@
 
 require_relative '../services/machine_configurator'
 require_relative '../../core/models/configuration'
+require_relative '../../core/services/configuration_generator'
 
 # This class installs the product on selected node.
 class InstallProduct < BaseCommand
@@ -93,58 +94,9 @@ class InstallProduct < BaseCommand
     else
       product < { 'name' => @product, 'version' => @product_version.to_s }
     end
-    product_config = generate_product_config(@product, product, box)
-    role_json_file = generate_json_format(name, product_config, recipe_name, box)
+    product_config = ConfigurationGenerator.generate_product_config(@env, @product, product, box, nil)
+    role_json_file = ConfigurationGenerator.generate_json_format(@env, name, product_config, recipe_name, box)
     IO.write(role_file_path, role_json_file)
     role_file_path
-  end
-
-  # Generate a list of role parameters in JSON format
-  # @param name [String] node name
-  # @param product_config [Hash] list of the product parameters
-  # @param recipe_name [String] name of the recipe
-  # @param box [String] name of the box
-  def generate_json_format(name, product_config, recipe_name, box)
-    run_list = ['recipe[mdbci_provision_mark::remove_mark]',
-                "recipe[#{recipe_name}]",
-                'recipe[mdbci_provision_mark::default]']
-    if check_subscription_manager(box)
-      run_list.insert(1, 'recipe[subscription-manager]')
-      product_config = product_config.merge('subscription-manager': retrieve_subscription_credentials)
-    end
-    role = { name: name,
-             default_attributes: {},
-             override_attributes: product_config,
-             json_class: 'Chef::Role',
-             description: '',
-             chef_type: 'role',
-             run_list: run_list }
-    JSON.pretty_generate(role)
-  end
-
-  # Check whether box needs to be subscribed or not
-  # @param box [String] name of the box
-  def check_subscription_manager(box)
-    @env.box_definitions.get_box(box)['configure_subscription_manager'] == 'true'
-  end
-
-  # Generate the list of the product parameters
-  # @param product_name [String] name of the product for install
-  # @param product [Hash] parameters of the product to configure from configuration file
-  # @param box [String] name of the box
-  def generate_product_config(product_name, product, box)
-    repo = @env.repos.find_repository(product_name, product, box)
-    raise "Repo for product #{product['name']} #{product['version']} for #{box} not found" if repo.nil?
-
-    config = { 'version': repo['version'], 'repo': repo['repo'], 'repo_key': repo['repo_key'] }
-    if !product['cnf_template'].nil? && !product['cnf_template_path'].nil?
-      config['cnf_template'] = product['cnf_template']
-      config['cnf_template_path'] = product['cnf_template_path']
-    end
-    repo_file_name = @env.repos.repo_file_name(product_name)
-    config['repo_file_name'] = repo_file_name unless repo_file_name.nil?
-    config['node_name'] = product['node_name'] unless product['node_name'].nil?
-    attribute_name = @env.repos.attribute_name(product_name)
-    { "#{attribute_name}": config }
   end
 end
