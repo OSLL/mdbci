6859c96c30a603fd6480b80cdc3282d4a79fa461
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2019-06-19 18:19:07 +0300

    Use custom resolv.conf file for ubuntu bionic (refs #25298)

--- assets/chef-recipes/cookbooks/packages/recipes/setup_resolved.rb
+++ assets/chef-recipes/cookbooks/packages/recipes/setup_resolved.rb
@@ -3,8 +3,19 @@
 #
 
 RESOLVED_FILE = '/etc/systemd/resolved.conf'
+platform_is_bionic = node['platform'] == 'ubuntu' && node['platform_version'].to_i == 18
 
-if File.exist?(RESOLVED_FILE)
+if platform_is_bionic
+  cookbook_file '/etc/resolv.conf' do
+    owner 'root'
+    group 'root'
+    mode '0644'
+    source 'resolv.conf'
+    action :create
+  end
+end
+
+if File.exist?(RESOLVED_FILE) && !platform_is_bionic
   cookbook_file RESOLVED_FILE do
     source 'resolved.conf'
     mode '0644'
