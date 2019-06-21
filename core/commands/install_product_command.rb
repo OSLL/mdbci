593e7ff33498085172964864d43ab5fdb926174b
Alexey Kallistov <alexey.kallistov@fruct.org>
2019-06-21 15:56:12 +0300

    Fix product installation method for installproduct class (refs #25198)

--- core/commands/install_product_command.rb
+++ core/commands/install_product_command.rb
@@ -47,12 +47,13 @@ class InstallProduct < BaseCommand
       return ARGUMENT_ERROR_RESULT
     end
     @mdbci_config = Configuration.new(@args.first, @env.labels)
+    @network_config = NetworkConfig.new(@mdbci_config, @ui)
 
     @product = @env.nodeProduct
     @product_version = @env.productVersion
 
     begin
-      @network_config = NetworkSettings.from_file(@mdbci_config.network_settings_file)
+      @network_settings = NetworkSettings.from_file(@mdbci_config.network_settings_file)
     rescue StandardError
       @ui.error('Network settings file is not found for the configuration')
       return ARGUMENT_ERROR_RESULT
@@ -65,29 +66,31 @@ class InstallProduct < BaseCommand
   # Setup ssh key data
   # @param node_name [String] name of the node
   def setup_ssh_key(node_name)
-    network_settings = @network_config.node_settings(node_name)
-    { 'whoami' => network_settings['whoami'],
-      'network' => network_settings['network'],
-      'keyfile' => network_settings['keyfile'],
+    node_settings = @network_settings.node_settings(node_name)
+    { 'whoami' => node_settings['whoami'],
+      'network' => node_settings['network'],
+      'keyfile' => node_settings['keyfile'],
       'name' => node_name }
   end
 
   # Install product on server
   # @param machine [Hash] information about machine to connect
   def install_product(machine)
-    role_file_path = generate_role_file(machine['name'])
-    target_path = "configs/#{machine['name']}.json"
-    @machine_configurator.configure(machine, "#{machine['name']}.json", @ui,
-                                    [[role_file_path, target_path]],
-                                    '', '14.7.17')
+    name = machine['name'].to_s
+    role_file_path = generate_role_file(name)
+    target_path = "configs/#{name}-config.json"
+    @network_config.add_nodes([name])
+    @machine_configurator.configure(@network_config[name], "#{name}-config.json",
+                                    @ui, [[role_file_path, target_path]])
   end
 
   # Create a role file to install the product from the chef
   # @param name [String] node name
   def generate_role_file(name)
-    box = @mdbci_config.node_configurations[name]['box']
+    node = @mdbci_config.node_configurations[name]
+    box = node['box'].to_s
     recipe_name = @env.repos.recipe_name(@product)
-    product = @mdbci_config.node_configurations[name]['product']
+    product = node['product']
     role_file_path = "#{@mdbci_config.path}/#{name}.json"
     if product.nil?
       product = { 'name' => @product, 'version' => @product_version.to_s }
@@ -98,5 +101,23 @@ class InstallProduct < BaseCommand
     role_json_file = ConfigurationGenerator.generate_json_format(@env, name, product_config, recipe_name, box)
     IO.write(role_file_path, role_json_file)
     role_file_path
+  end
+
+
+  # Make a hash list of node parameters by a node configuration and
+  # information of the box parameters.
+  #
+  # @param node [Array] information of the node from configuration file
+  # @param box_params [Hash] information of the box parameters
+  # @return [Hash] list of the node parameters.
+  def make_node_params(node, box_params, name)
+    p node
+    symbolic_box_params = Hash[box_params.map { |k, v| [k.to_sym, v] }]
+    {
+      name: name.to_s,
+      host: node['hostname'].to_s,
+      vm_mem: node['memory_size'].nil? ? '1024' : node['memory_size'].to_s,
+      vm_cpu: (@env.cpu_count || node['cpu_count'] || '1').to_s
+    }.merge(symbolic_box_params)
   end
 end
