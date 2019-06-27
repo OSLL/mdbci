2f26b5d2c3828506a832fd13fbb4bee69d6ce172
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2019-06-27 12:51:38 +0300

    Remove the modification of vagrant configurator

--- core/commands/partials/vagrant_configurator.rb
+++ core/commands/partials/vagrant_configurator.rb
@@ -238,15 +238,9 @@ class VagrantConfigurator
     nodes = @config.node_names
     run_in_directory(@config.path) do
       store_network_config
-      if nodes.size != 1
-        up_results = Workers.map(nodes) { |node| up_node(node) }
-        up_results.each { |up_result| up_result[1].print_to_stdout } if @env.threads_count > 1
-        return ERROR_RESULT unless up_results.detect { |up_result| !up_result[0] }.nil?
-      else
-        up_result = up_node(nodes[0])
-        up_result[1].print_to_stdout
-        return ERROR_RESULT unless up_result[0].nil?
-      end
+      up_results = Workers.map(nodes) { |node| up_node(node) }
+      up_results.each { |up_result| up_result[1].print_to_stdout } if @env.threads_count > 1
+      return ERROR_RESULT unless up_results.detect { |up_result| !up_result[0] }.nil?
     end
     generate_config_information(Dir.pwd)
     generate_label_information_file
